479bbd81a6f11f8725d39ec9b1b3e662c29344b0
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-03-26 11:41:37 -0600

    Generated initial entry components

--- src/app/entry/entry.module.ts
+++ src/app/entry/entry.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignInComponent, ForgotPasswordComponent, AccountCreateComponent, PasswordResetComponent],
   imports: [
     CommonModule
   ]
