78f7b67d40217d8e5261308c3375b273dde0fa8b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-06 18:09:48 -0600

    Created model for dialog data

---
+++ src/models/dialog-data.model.ts
@@ -0,0 +1,18 @@
+/**
+ * Represents all information that a dialog might display.
+ */
+export interface DialogData {
+
+  /** The title to be displayed on the dialog. */
+  title: string;
+
+  /** The message text to be displayed on the dialog. */
+  message: string;
+
+  /** The text to display for the okay/confirm/dismiss button. */
+  okButtonText?: string;
+
+  /** The text to display for the cancel button. */
+  cancelButtonText?: string;
+
+}
