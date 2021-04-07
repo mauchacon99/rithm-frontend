8dcc98610b02abd890274cf2711420322aad5829
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-07 13:31:37 -0600

    Lint fixes

--- src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * Reusable component used for displaying a confirmation dialog.
+ */
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss']
 })
-export class ConfirmDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ConfirmDialogComponent {
 
 }
