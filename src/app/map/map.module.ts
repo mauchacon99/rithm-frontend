aa11cbf33e092fc62bd95ccbd91b0290d21ca09b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-08 14:21:22 -0600

    Basic map routing module

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MapRoutingModule } from './map-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MapRoutingModule
   ]
 })
 export class MapModule { }
