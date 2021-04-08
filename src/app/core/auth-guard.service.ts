ea68c8e73818f472c8f120707a0a85e91730ab16
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-08 17:24:28 -0600

    Creating can activate auth check

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { DialogService } from './dialog.service';
+import { UserService } from './user.service';
 
 /**
  * Service for preventing a user from accessing pages if not authenticated.
@@ -6,6 +9,23 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
+  constructor(private router: Router,
+              private userService: UserService,
+              private dialogService: DialogService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    // If logged in, return true
+    if (this.userService.isSignedIn()) {
+      return true;
+    } else {
+      this.router.navigateByUrl('');
+      this.dialogService.alert({
+        title: 'Action Not Permitted',
+        message: 'You need to sign in before you can view that page.'
+      });
+      return false;
+    }
+  }
 }
