deb6cc10872783081ed5eeee1a95a82b9ace2893
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-08 14:19:19 -0600

    Setting up entry routing

--- src/app/entry/entry.module.ts
+++ src/app/entry/entry.module.ts
@@ -4,13 +4,15 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { AccountCreateComponent } from './account-create/account-create.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { EntryRoutingModule } from './entry-routing.module';
 
 
 
 @NgModule({
   declarations: [SignInComponent, ForgotPasswordComponent, AccountCreateComponent, PasswordResetComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    EntryRoutingModule
   ]
 })
 export class EntryModule { }
