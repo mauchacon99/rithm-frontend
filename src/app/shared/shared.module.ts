76e44dcf5892f81f880b8d4828560384d8ef28e3
Austin Bagley <aust.bagley@gmail.com>
2021-04-08 10:49:10 -0600

    added full screen loader component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FullScreenLoaderComponent } from './full-screen-loader/full-screen-loader.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FullScreenLoaderComponent],
   imports: [
     CommonModule
   ]
