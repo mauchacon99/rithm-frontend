02098a959cee77754c4eba8746e974df086dccbb
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-08 17:07:22 -0600

    Removing test code from sign in

--- src/app/entry/sign-in/sign-in.component.ts
+++ src/app/entry/sign-in/sign-in.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DialogService } from 'src/app/core/dialog.service';
 
 /**
  * Component for signing into the system.
@@ -10,18 +9,6 @@ import { DialogService } from 'src/app/core/dialog.service';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
-
-  constructor(private dialogService: DialogService) {
-    this.dialogService.confirm({
-      title: 'Alert',
-      message: 'You are not allowed to view this page.',
-      okButtonText: 'That is fine',
-      cancelButtonText: 'Abort',
-      promptInput: 'Kore wa pen desu.',
-      promptLabel: 'Nihongo'
-    });
-  }
-
 
   /**
    * Attempts to sign the user in using the provided credentials.
