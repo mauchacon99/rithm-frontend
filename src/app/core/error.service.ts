104814de4219e7782ec82f935a6e0eaac6d7abcc
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-12 09:28:17 -0600

    chore: take error object instead of error message

--- src/app/core/error.service.ts
+++ src/app/core/error.service.ts
@@ -11,9 +11,9 @@ export class ErrorService {
   /**
    * Logs an error to the console.
    *
-   * @param error The error message that was encountered.
+   * @param error The error that was encountered.
    */
-  logError(error: string): void {
+  logError(error: Error): void {
     console.error(error);
     // TODO: Add error log reporting using some third-party service
   }
@@ -22,9 +22,9 @@ export class ErrorService {
    * Logs an error and displays a separate message to the user.
    *
    * @param displayMessage The error message to display to the user.
-   * @param error The error message that was encountered.
+   * @param error The error that was encountered.
    */
-  displayError(displayMessage: string, error: string): void {
+  displayError(displayMessage: string, error: Error): void {
     this.logError(error);
 
     // TODO: Display error
