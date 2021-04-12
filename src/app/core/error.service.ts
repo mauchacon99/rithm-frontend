b59356e09459f2d0486ab6acd7b8db9742a38e25
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-12 09:21:28 -0600

    chore: adding initial log error service function

--- src/app/core/error.service.ts
+++ src/app/core/error.service.ts
@@ -8,11 +8,17 @@ import { Injectable } from '@angular/core';
 })
 export class ErrorService {
 
-  logError(): void {
-
+  /**
+   * Logs an error to the console.
+   *
+   * @param error The error message that was encountered.
+   */
+  logError(error: string): void {
+    console.error(error);
+    // TODO: Add error log reporting using some third-party service
   }
 
-  displayError(): void {
+  displayError(error: string): void {
 
   }
 
