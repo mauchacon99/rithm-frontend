5a2d3c0e9980f66287050fab0a88762737722948
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-14 17:53:13 -0600

    chore: initial sign in method

--- src/app/core/user.service.ts
+++ src/app/core/user.service.ts
@@ -9,6 +9,17 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   /**
+   * Signs the user in to the system.
+   *
+   * @param email The email address for the user.
+   * @param password The entered password for the user.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  signIn(email: string, password: string): void {
+    // RIT-144
+  }
+
+  /**
    * Checks if the user is currently signed in.
    *
    * @returns True if currently authenticated, false otherwise.
