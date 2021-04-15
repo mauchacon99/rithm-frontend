d2ad3318198b0f3c07104bb951564046348bf9f0
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-14 19:06:41 -0600

    chore: add base api url to env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
 
+  /** The base URL for the Rithm API. */
+  baseApiUrl: 'api.rithm.tech', // TODO: Point to actual URL
+
   /** The lifetime for which an access token is valid in milliseconds. */
   accessTokenLifetime: 900000
 };
