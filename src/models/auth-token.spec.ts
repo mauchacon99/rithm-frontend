00566d66cdfad53466506266da34592ba1dc63b8
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-14 18:47:04 -0600

    test: fix broken instance test

--- src/models/auth-token.spec.ts
+++ src/models/auth-token.spec.ts
@@ -1,7 +1,9 @@
 import { AuthToken } from './auth-token';
 
+const SAMPLE_AUTH_TOKEN = 'dj0yJmk9N2pIazlsZk1iTzIxJmQ9WVdrOWVEUmpVMFpWTXpRbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD00NA--';
+
 describe('AuthToken', () => {
   it('should create an instance', () => {
-    expect(new AuthToken()).toBeTruthy();
+    expect(new AuthToken(SAMPLE_AUTH_TOKEN)).toBeTruthy();
   });
 });
