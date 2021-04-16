46439a37ece469f37add1b7e1ae3fa4cc14dc0c1
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-16 14:58:27 -0600

    test: unit tests for sign out

--- src/app/core/user.service.spec.ts
+++ src/app/core/user.service.spec.ts
@@ -1,16 +1,71 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserService } from './user.service';
+
+// const routerSpy =  jasmine.createSpyObj('Router', ['navigateByUrl']);
+
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      providers: [
+        // { provide: Router, useClass: routerSpy }
+      ]
+    });
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should allow sign in', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should clear local storage on sign out', () => {
+    localStorage.setItem('test', 'test');
+    service.signOut();
+    expect(localStorage.getItem('test')).toBeNull();
+  });
+
+  it('should clear local storage on sign out', () => {
+    sessionStorage.setItem('test', 'test');
+    service.signOut();
+    expect(sessionStorage.getItem('test')).toBeNull();
+  });
+
+  it('should clear cookies on sign out', () => {
+    document.cookie = 'test=test';
+    service.signOut();
+    const cookies = document.cookie;
+    expect(cookies).toBeFalsy();
+  });
+
+  it('should return to sign in page on sign out', () => {
+    // service.signOut();
+
+    // const spy = routerSpy.navigateByUrl as jasmine.Spy;
+    // const navArgs = spy.calls.first().args[0];
+
+
+    // expect(navArgs).toBe(['']);
+  });
+
+  it('should report sign in status', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh expired access tokens', () => {
     expect(service).toBeTruthy();
   });
 });
