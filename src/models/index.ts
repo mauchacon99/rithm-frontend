8f17390d941fcffaa110b3e6b0264176e0727f41
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-20 14:57:17 -0600

    chore: create basic models for user and group

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,6 @@
 export * from './dialog-data';
 export * from './global-permission';
+export * from './group';
 export * from './object-permission';
 export * from './permission';
+export * from './user';
---
+++ src/models/user.ts
@@ -0,0 +1,29 @@
+import { GlobalPermission } from './global-permission';
+import { ObjectPermission } from './object-permission';
+
+/**
+ * Represents all information about a Rithm user.
+ */
+export interface User {
+
+  /** The user's first name. */
+  firstName: string;
+
+  /** The user's last name. */
+  lastName: string;
+
+  /** The email address registered to the user. */
+  email: string;
+
+  /** The list of flow-level permissions that the user has. */
+  objectPermissions: ObjectPermission[];
+
+  /** The global permissions that the user has. */
+  globalPermission: GlobalPermission;
+
+  /** The groups that the user is a member of. */
+  groups: [];
+
+  /** The date that this user was created. */
+  createdDate: string;
+}
