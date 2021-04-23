5ca5e4c083d5ac3f5c4352ecc59f2f17b91d53fd
Austin Bagley <aust.bagley@gmail.com>
2021-04-23 11:01:53 -0600

    init the sign in form group

--- src/app/entry/sign-in/sign-in.component.ts
+++ src/app/entry/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Component for signing into the system.
@@ -9,6 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
+  signInForm: FormGroup;
+
+  constructor ( ) {
+    this.signInForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
 
   /**
    * Attempts to sign the user in using the provided credentials.
