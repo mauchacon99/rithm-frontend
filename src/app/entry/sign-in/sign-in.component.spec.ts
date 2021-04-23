eb6561faf1529b332859ffcf11f6d9efadedd75a
Austin Bagley <aust.bagley@gmail.com>
2021-04-23 12:00:08 -0600

    unit test sign in form

--- src/app/entry/sign-in/sign-in.component.spec.ts
+++ src/app/entry/sign-in/sign-in.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './sign-in.component';
 
@@ -8,7 +9,10 @@ describe('SignInComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      declarations: [ SignInComponent ],
+      imports: [
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +25,28 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.signInForm.valid).toBeFalsy();
+  });
+
+  it('should check the validation of the email field', () => {
+    const email = component.signInForm.controls['email'];
+    expect(email.valid).toBeFalsy();
+
+    email.setValue('');
+    expect(email.hasError('required')).toBeTruthy();
+
+    email.setValue('test.com');
+    expect(email.hasError('email')).toBeTruthy();
+  });
+
+  it('should check the validation of the password field', () => {
+    const password = component.signInForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+
+    password.setValue('');
+    expect(password.hasError('required')).toBeTruthy();
   });
 });
