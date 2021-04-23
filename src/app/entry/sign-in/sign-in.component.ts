818f03a7a7e2158a7d8c7cc2eb177a60b52c3549
Austin Bagley <aust.bagley@gmail.com>
2021-04-23 11:13:27 -0600

    remove unused import

--- src/app/entry/sign-in/sign-in.component.ts
+++ src/app/entry/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Component for signing into the system.
