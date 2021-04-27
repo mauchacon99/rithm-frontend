a108baf90dab7343a1e860a7eeecee06d5ba641e
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-27 15:33:58 -0600

    chore: updating Angular environments

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  /** The base URL for the Rithm API. */
+  baseApiUrl: 'https://api.rithm.tech', // TODO: Point to actual URL
+
+  /** The lifetime for which an access token is valid in milliseconds. */
+  accessTokenLifetime: 900000
+};
