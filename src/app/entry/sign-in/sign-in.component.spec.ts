6fdf32efb5f1b0b08e38c172eff54ecf71539f1f
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-30 16:28:51 -0600

    test: write additional unit tests for sign in

--- src/app/entry/sign-in/sign-in.component.spec.ts
+++ src/app/entry/sign-in/sign-in.component.spec.ts
@@ -1,23 +1,34 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of, throwError } from 'rxjs';
 import { CoreModule } from 'src/app/core/core.module';
+import { UserService } from 'src/app/core/user.service';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SignInResponse } from 'src/models';
 
 import { SignInComponent } from './sign-in.component';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
+  let loader: HarnessLoader;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ],
+      declarations: [SignInComponent],
       imports: [
         BrowserAnimationsModule,
         ReactiveFormsModule,
@@ -28,6 +39,9 @@ describe('SignInComponent', () => {
         MatCardModule,
         MatInputModule,
         MatButtonModule
+      ],
+      providers: [
+        { provide: UserService, useClass: MockUserService }
       ]
     })
       .compileComponents();
@@ -36,6 +50,8 @@ describe('SignInComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
@@ -43,34 +59,23 @@ describe('SignInComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('form invalid when empty', () => {
-    expect(component.signInForm.valid).toBeFalsy();
-  });
-
-  it('should check the validation of the email field', () => {
-    const email = component.signInForm.controls['email'];
-    expect(email.valid).toBeFalsy();
-
-    email.setValue('');
-    expect(email.hasError('required')).toBeTruthy();
-
-    email.setValue('test.com');
-    expect(email.hasError('email')).toBeTruthy();
-  });
-
-  it('should check the validation of the password field', () => {
-    const password = component.signInForm.controls['password'];
-    expect(password.valid).toBeFalsy();
-
-    password.setValue('');
-    expect(password.hasError('required')).toBeTruthy();
-  });
-
-  it('should navigate to forgot password on text click', () => {
+  it('should display loading indicator during request', () => {
     // TODO: add unit test
   });
 
-  it('should navigate to account create on text click', () => {
+  it('should display invalid credentials message', () => {
+    const message = fixture.debugElement.query(By.css('.invalid-message'));
+    expect(message.classes['transparent']).toBeFalsy();
+
+    component.signInForm.controls['email'].setValue('incorrect@email.com');
+    component.signInForm.controls['password'].setValue('password1234');
+    component.signIn();
+    expect(component.invalidCredentials).toBeTrue;
+
+    expect(message).toBeTruthy();
+  });
+
+  it('should display message to verify email if not validated', () => {
     // TODO: add unit test
   });
 
@@ -78,53 +83,149 @@ describe('SignInComponent', () => {
     // TODO: add unit test
   });
 
+  it('should navigate to dashboard upon successful sign in', () => {
+    const routerSpy = spyOn(router, 'navigateByUrl');
+    component.signInForm.controls['email'].setValue('someone@email.com');
+    component.signInForm.controls['password'].setValue('password1234');
+    component.signIn();
+    expect(routerSpy).toHaveBeenCalledOnceWith('dashboard');
+
+    // TODO: add unit test
+  });
+
   // sign in form
   describe('sign in form', () => {
+    let formGroup: FormGroup;
+    let emailControl: AbstractControl;
+    let passwordControl: AbstractControl;
+
+    beforeEach(() => {
+      formGroup = component.signInForm;
+      emailControl = formGroup.controls['email'];
+      passwordControl = component.signInForm.controls['password'];
+    });
+
     it('should exist', () => {
-      // TODO: add unit test
+      expect(formGroup).toBeTruthy();
     });
 
-    it('should display invalid credentials message', () => {
-      // TODO: add unit test
-    });
-
-    it('should navigate to dashboard upon successful sign in', () => {
-      // TODO: add unit test
+    it('should be invalid when empty', () => {
+      expect(formGroup.valid).toBeFalse();
     });
 
     // email field
     describe('email field', () => {
-      it('should exist', () => {
-        // TODO: add unit test
+      let emailInputHarness: MatInputHarness;
+
+      beforeEach(async () => {
+        emailInputHarness = await loader.getHarness(MatInputHarness.with({ selector: '#email' }));
       });
 
-      it('should show errors on invalid email', () => {
-        // TODO: add unit test
+      it('should exist', () => {
+        expect(emailInputHarness).toBeTruthy();
+      });
+
+      it('should require a value', () => {
+        expect(emailControl.valid).toBeFalsy();
+        expect(emailControl.hasError('required')).toBeTrue();
+      });
+
+      it('should check for a valid email', () => {
+        emailControl.setValue('test.com');
+        expect(emailControl.hasError('email')).toBeTrue();
+        emailControl.setValue('test@email.com');
+        expect(emailControl.hasError('email')).toBeFalse();
       });
     });
 
     // password field
     describe('password field', () => {
+      let passwordInputHarness: MatInputHarness;
+
+      beforeEach(async () => {
+        passwordInputHarness = await loader.getHarness(MatInputHarness.with({ selector: '#password' }));
+      });
+
       it('should exist', () => {
-        // TODO: add unit test
+        expect(passwordInputHarness).toBeTruthy();
+      });
+
+      it('should require a value', () => {
+        expect(passwordControl.valid).toBeFalse();
+        expect(passwordControl.hasError('required')).toBeTrue();
       });
     });
 
     // sign in button
     describe('sign in button', () => {
+      let buttonHarness: MatButtonHarness;
+
+      beforeEach(async () => {
+        buttonHarness = await loader.getHarness(MatButtonHarness);
+        spyOn(component, 'signIn');
+      });
+
       it('should exist', () => {
-        // TODO: add unit test
+        expect(buttonHarness).toBeTruthy();
       });
 
-      it('should be disabled without input', () => {
-        // TODO: add unit test
+      it('should be disabled when form is empty', async () => {
+        expect(await buttonHarness.isDisabled()).toBeTrue();
       });
 
-      it('should be enabled when form is filled out', () => {
-        // TODO: add unit test
+      it('should be disabled when form is invalid', async () => {
+        formGroup.controls['email'].setValue('test@email....com');
+        formGroup.controls['password'].setValue('password1234');
+        expect(await buttonHarness.isDisabled()).toBeTrue();
+      });
+
+      it('should be enabled when form is filled out', async () => {
+        formGroup.controls['email'].setValue('test@email.com');
+        formGroup.controls['password'].setValue('password1234');
+        expect(await buttonHarness.isDisabled()).toBeFalse();
+      });
+
+      it('should sign in when clicked', async () => {
+        formGroup.controls['email'].setValue('test@email.com');
+        formGroup.controls['password'].setValue('password1234');
+        expect(await buttonHarness.isDisabled()).toBeFalse(); // This needs to be present for some reason...
+
+        await buttonHarness.click();
+        expect(component.signIn).toHaveBeenCalled();
       });
     });
 
   });
 
 });
+
+// eslint-disable-next-line
+class MockUserService {
+
+  // eslint-disable-next-line
+  signIn(email: string, password: string): Observable<SignInResponse> {
+    let response;
+
+    if (email.includes('unverified')) {
+      response = new HttpErrorResponse({
+        error: {
+          error: 'Unable to login before email has been verified.'
+        }
+      });
+    } else if (email.includes('incorrect')) {
+      response = new HttpErrorResponse({
+        error: {
+          error: 'Invalid username or password.'
+        }
+      });
+    } else {
+      return of({
+        accessToken: 'wowowowo',
+        user: undefined
+      });
+    }
+
+    return throwError(response);
+
+  }
+}
