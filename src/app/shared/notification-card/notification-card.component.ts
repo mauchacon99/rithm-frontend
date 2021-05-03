b1691d3cd87e07e2e3cb2907692f2af109d3ad3f
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-03 16:29:00 -0600

    docs: better docs and function stubs

--- src/app/shared/notification-card/notification-card.component.ts
+++ src/app/shared/notification-card/notification-card.component.ts
@@ -10,8 +10,25 @@ import { Component } from '@angular/core';
 })
 export class NotificationCardComponent {
 
+  /** The title for the notification. */
   title = 'Steven Rogers tagged you in a comment';
 
+  /** The notification message. */
   message = '@Tony Stark please double check the SKU on this new product';
+
+  /**
+   * Dismisses this specific notification.
+   */
+  dismiss(): void {
+    // TODO: dismiss this notification
+  }
+
+  /**
+   * Navigates the user to the specific context for the notification. For example,
+   * getting tagged in a comment will bring them to that specific document/station/etc.
+   */
+  goToContext(): void {
+    // TODO: bring the user to the specific context
+  }
 
 }
