2142145cdb7ad1c0846ea2c2a9d886b874d8c44b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-04 10:49:11 -0600

    test: simplify module imports for account create component

--- src/app/entry/account-create/account-create.component.spec.ts
+++ src/app/entry/account-create/account-create.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import { CoreModule } from 'src/app/core/core.module';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { MockUserService } from 'src/app/core/user-service-mock';
+import { UserService } from 'src/app/core/user.service';
 
 import { AccountCreateComponent } from './account-create.component';
 
@@ -13,9 +15,12 @@ describe('AccountCreateComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ AccountCreateComponent ],
       imports: [
-        CoreModule,
-        SharedModule,
-        RouterTestingModule
+        RouterTestingModule,
+        MatSnackBarModule,
+        MatDialogModule
+      ],
+      providers: [
+        { provide: UserService, useClass: MockUserService }
       ]
     })
     .compileComponents();
