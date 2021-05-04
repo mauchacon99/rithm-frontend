2299a0b6c15de33bd07213ae4bb37ed781a9b60b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-04 09:57:40 -0600

    call register endpoint

--- src/app/entry/account-create/account-create.component.ts
+++ src/app/entry/account-create/account-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { ErrorService } from 'src/app/core/error.service';
+import { UserService } from 'src/app/core/user.service';
 
 /**
  * Component used for creating an account.
@@ -9,5 +12,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-create.component.scss']
 })
 export class AccountCreateComponent {
+
+  constructor(
+    private userService: UserService,
+    private errorService: ErrorService
+  ) {}
+
+  createAccount(): void {
+    this.userService.register('first', 'last', 'email@test.com', 'Qwer5234')
+      .pipe(first())
+      .subscribe((test) => {
+        // RIT-174
+        console.log(test);
+      }, (error) => {
+        this.errorService.displayError(
+          'Something went wrong on our end and we\'re looking into it. Please try again in a little while.',
+          error,
+          true
+        );
+      });
+  }
 
 }
