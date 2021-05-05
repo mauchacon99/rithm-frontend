348bd50ee293746f5b4a2ab1db10eb1fb6cd6038
Austin Bagley <aust.bagley@gmail.com>
2021-05-05 14:56:57 -0600

    docs

--- src/app/shared/notification-toasts-container/notification-toasts-container.component.ts
+++ src/app/shared/notification-toasts-container/notification-toasts-container.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+/**
+ * Container for the toast notifications.
+ */
 @Component({
   selector: 'app-notification-toasts-container',
   templateUrl: './notification-toasts-container.component.html',
   styleUrls: ['./notification-toasts-container.component.scss']
 })
-export class NotificationToastsContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class NotificationToastsContainerComponent {
 
 }
