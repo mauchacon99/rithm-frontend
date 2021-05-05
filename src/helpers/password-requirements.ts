7c7b987657fa4429f036b9079d095d251d965b9e
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-05 08:47:00 -0600

    refactor: moving code into helper function

--- src/helpers/password-requirements.ts
+++ src/helpers/password-requirements.ts
@@ -1,6 +1,7 @@
-import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
 /**
- * Use for displaying and validating passwords.
+ * Helper for displaying and validating passwords.
  */
 export class PasswordRequirements {
   /** Regex to check for 8 chars. */
@@ -14,74 +15,70 @@ export class PasswordRequirements {
   /** Regex to check for one special char (!#$%& etc). */
   private at_least_one_special_char = new RegExp(/^(?=.*?[" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"])/);
 
-  constructor() {
-    // constructor
-  }
   /**
    * Check password to see if it has 8 chars or more.
    *
-   * @returns Object when there are errors.
+   * @returns A validator function.
    */
   isGreaterThanEightChars(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const _value = control.value;
-      return this.at_least_eight_chars.test(_value) ? null : {missingPassLength: true};
-    };
+    return this.testRegExp(this.at_least_eight_chars, 'missingPassLength');
   }
   /**
    * Check password to see if it has a lowercase char.
    *
-   * @returns Object when there are errors.
+   * @returns A validator function.
    */
   hasOneLowerCaseChar(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const _value = control.value;
-      return this.at_least_one_lower_case_char.test(_value) ? null : {missingLowerChar: true};
-    };
+    return this.testRegExp(this.at_least_one_lower_case_char, 'missingLowerChar');
   }
   /**
    * Check password to see if it has an uppercase char.
    *
-   * @returns Object when there are errors.
+   * @returns A validator function.
    */
   hasOneUpperCaseChar(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const _value = control.value;
-      return this.at_least_one_upper_case_char.test(_value) ? null : {missingUpperChar: true};
-    };
+    return this.testRegExp(this.at_least_one_upper_case_char, 'missingUpperChar');
   }
   /**
    * Check password to see if it has a number.
    *
-   * @returns Object when there are errors.
+   * @returns A validator function.
    */
   hasOneDigitChar(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const _value = control.value;
-      return this.at_least_one_digit_char.test(_value) ? null : {missingDigitChar: true};
-    };
+    return this.testRegExp(this.at_least_one_digit_char, 'missingDigitChar');
   }
   /**
    * Check password to see if it has a special char.
    *
-   * @returns Object when there are errors.
+   * @returns A validator function.
    */
   hasOneSpecialChar(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const _value = control.value;
-      return this.at_least_one_special_char.test(_value) ? null : {missingSpecialChar: true};
-    };
+    return this.testRegExp(this.at_least_one_special_char, 'missingSpecialChar');
   }
   /**
    * Check to see if password and confirm password match.
    *
-   * @returns Object when there are errors.
+   * @returns A validator function.
    */
   passwordsMatch(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const password = control.parent?.get('password')?.value;
       const confirmPass = control.parent?.get('confirmPassword')?.value;
-      return (password !== confirmPass) ? {mismatchingPasswords: true} : null;
+      return (password !== confirmPass) ? { mismatchingPasswords: true } : null;
+    };
+  }
+
+  /**
+   * Creates validator function to test control value against a regular expression.
+   *
+   * @param regExp The regular expression to test.
+   * @param errorName The validator error name if the test does not pass.
+   * @returns A validator function.
+   */
+  private testRegExp(regExp: RegExp, errorName: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const _value = control.value;
+      return regExp.test(_value) ? null : { [errorName]: true };
     };
   }
 
