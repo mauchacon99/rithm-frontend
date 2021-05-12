bd8d3314512bffbd7b20f39f9cfa0c23d7389641
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-12 16:03:36 -0600

    fix: re-add path for microservice

--- src/app/core/user.service.ts
+++ src/app/core/user.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 import { AccessToken } from 'src/helpers';
 import { SignInResponse, TokenResponse, User } from 'src/models';
 
-const MICROSERVICE_PATH = '';
+const MICROSERVICE_PATH = '/userservice';
 
 /**
  * Service for all interactions involving a user.
