2e0883a069c1b3a5f3ffc5ea759ea94911c63a92
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-19 09:08:03 -0600

    test: fix unit test import error

--- src/app/core/reset-password.guard.spec.ts
+++ src/app/core/reset-password.guard.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ResetPasswordGuard } from './reset-password.guard';
 
@@ -6,7 +7,11 @@ describe('ResetPasswordGuard', () => {
   let guard: ResetPasswordGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ]
+    });
     guard = TestBed.inject(ResetPasswordGuard);
   });
 
