67205fae013ac58850791a605cdec2e36a544d52
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-05-20 15:56:34 -0600

    refactor: initial map component

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapRoutingModule } from './map-routing.module';
+import { MapComponent } from './map/map.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MapComponent
+  ],
   imports: [
     CommonModule,
     MapRoutingModule
