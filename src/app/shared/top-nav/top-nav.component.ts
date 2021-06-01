7eb9ce770fba9972f70bf9578d9894d28c2ac329
Austin Bagley <aust.bagley@gmail.com>
2021-06-01 15:59:27 -0600

    add mat-drawer and basic styles

--- src/app/shared/top-nav/top-nav.component.ts
+++ src/app/shared/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 /**
  * Component for the top site navigation.
@@ -9,10 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-nav.component.scss']
 })
 export class TopNavComponent {
+  @Input() mobileNav: any;
+
   /** List of navigation items. */
   navItems = ['Dashboard', 'Map'];
 
   /** Monogram for user profile icon. */
   monogram = 'AB';
+
+  toggle(): void {
+    this.mobileNav.toggle();
+  }
 
 }
