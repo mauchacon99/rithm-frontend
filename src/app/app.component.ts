90cc67d6ebbd836c2e0a318075998b0619f69ac1
Austin Bagley <aust.bagley@gmail.com>
2021-06-02 10:27:45 -0600

    removed an import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { SidenavService } from './core/sidenav.service';
 
