4dc360e0f1bd569d48048705f8a468c28a5943c9
Austin Bagley <aust.bagley@gmail.com>
2021-06-04 16:29:29 -0600

    saving work...Change UI based on # of roster

--- src/app/dashboard/station-card/station-card.component.ts
+++ src/app/dashboard/station-card/station-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 /**
  * Component for displaying a card with station information.
@@ -8,6 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './station-card.component.html',
   styleUrls: ['./station-card.component.scss']
 })
-export class StationCardComponent {
+export class StationCardComponent implements OnInit{
+  @Input() stationName = 'Station name';
+
+  @Input() totalDocs = 5;
+
+  @Input() roster = [
+    {
+      initials: 'AB'
+    },
+    {
+      initials: 'SR'
+    },
+    {
+      initials: 'TS'
+    }
+  ];
+
+  showAll = false;
+
+  constructor() {
+    console.log(this.roster);
+  }
+
+  ngOnInit(): void {
+    if(this.roster.length <= 3) {
+      this.showAll = true;
+    }
+  }
 
 }
