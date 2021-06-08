9ec148c8894f02d12f7c612b3dc265c7de0d2e06
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-08 09:30:14 -0600

    chore: service for station

---
+++ src/app/core/station.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Service for all station behavior and business logic.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class StationService {
+
+}
