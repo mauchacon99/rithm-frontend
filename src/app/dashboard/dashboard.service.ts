aa97822417d82f4214903808533b9501bed82946
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-08 09:36:02 -0600

    chore: use dashboard service only in module

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { DashboardModule } from './dashboard.module';
 
 /**
  * Service for all business logic involving the dashboard.
  */
 @Injectable({
-  providedIn: 'root'
+  providedIn: DashboardModule
 })
 export class DashboardService {
 
