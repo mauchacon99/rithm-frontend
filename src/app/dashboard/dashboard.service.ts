a8bdd755d98adacda80d3abddd99278acf287f7e
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-08 09:28:43 -0600

    chore: initial service for dashboard behavior

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Service for all business logic involving the dashboard.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+}
