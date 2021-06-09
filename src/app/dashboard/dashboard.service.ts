06ee98356dfcfa81244ccc9020dd04038e478909
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-09 13:18:24 -0600

    chore: mock getWorkerStations method

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Station, User } from 'src/models';
 
 /**
  * Service for all business logic involving the dashboard.
@@ -8,4 +10,35 @@ import { Injectable } from '@angular/core';
 })
 export class DashboardService {
 
+  /**
+   * Gets a list of stations where the signed-in user is on the work roster.
+   *
+   * @returns A list of stations.
+   */
+  getWorkerStations(): Observable<Station[]> {
+    const mockUser: User = {
+      rithmId: '1234',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@doe.com',
+      objectPermissions: [],
+      groups: [],
+      createdDate: new Date().toISOString()
+    };
+
+    const mockStations: Station[] = [
+      {
+        name: 'Station 1',
+        instructions: 'Some instructions',
+        documents: 5,
+        supervisors: [
+          mockUser
+        ],
+        rosterUsers: [
+          mockUser
+        ]
+      }
+    ];
+    return of(mockStations);
+  }
 }
