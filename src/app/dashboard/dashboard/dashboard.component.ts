e53f5131f18624beac8b5800994fd2101af36d5b
Austin Bagley <aust.bagley@gmail.com>
2021-06-09 09:49:08 -0600

    reorganize to own component

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { User } from 'src/models';
 
 /**
  * Component for the dashboard screens.
@@ -10,25 +9,6 @@ import { User } from 'src/models';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  /** A temp user. */
-  user: User;
 
-  /** Number of previously started docs. */
-  numPrev = 9;
-
-  /** Number of stations user is a member of. */
-  numStations = 12;
-
-  constructor() {
-    this.user = {
-      rithmId: '1',
-      firstName: 'Steve',
-      lastName: 'Rogers',
-      email: 'steve@rogers.com',
-      objectPermissions: [],
-      groups: [],
-      createdDate: ''
-    };
-  }
 
 }
--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models';
 
 /**
  * Component for the dashboard overview header.
@@ -9,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  /** A temp user. */
+  user: User;
 
+  /** Number of previously started docs. */
+  numPrev = 9;
+
+  /** Number of stations user is a member of. */
+  numStations = 12;
+
+  constructor() {
+    this.user = {
+      rithmId: '1',
+      firstName: 'Steve',
+      lastName: 'Rogers',
+      email: 'steve@rogers.com',
+      objectPermissions: [],
+      groups: [],
+      createdDate: ''
+    };
+  }
 }
