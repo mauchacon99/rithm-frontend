90c33832fb98551c343218a2e766be7523337541
Adarsh <adarsh.achar@inpivota.com>
2021-06-10 23:49:48 +0530

    RIT-303 Unit test case for dashboard header data

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { AccessToken } from 'src/helpers';
 import { DashboardHeaderResponse, Station, User } from 'src/models';
 
 const MICROSERVICE_PATH = '/dashboardservice';
@@ -13,19 +12,16 @@ const MICROSERVICE_PATH = '/dashboardservice';
 @Injectable({
   providedIn: 'root'
 })
+
 export class DashboardService {
   constructor(
     private http: HttpClient) { }
 
-  /** The access token to be used to authenticate for every request. */
-  accessToken: AccessToken | undefined;
-
-
-   /**
-    *Getting Dashboard header info.
-    *
-    * @returns Dashboard header observable.
-    */
+  /**
+   *Getting Dashboard header info.
+   *
+   * @returns Dashboard header observable.
+   */
   getDashboardHeader(): Observable<DashboardHeaderResponse> {
     return this.http.get<DashboardHeaderResponse>(`${environment.baseApiUrl}${MICROSERVICE_PATH}/api/Dashboard/Header`);
   }
