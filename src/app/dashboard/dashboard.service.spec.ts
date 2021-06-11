86facb3c97af4dbf1f1e573b0f1a8722b1ce488b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-11 16:12:32 -0600

    test: write unit test for dasbhoard header request

--- src/app/dashboard/dashboard.service.spec.ts
+++ src/app/dashboard/dashboard.service.spec.ts
@@ -1,10 +1,16 @@
+/* eslint-disable rxjs/no-ignored-error */
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { DashboardService } from './dashboard.service';
+import { DashboardHeaderResponse } from 'src/models';
+import { environment } from 'src/environments/environment';
+
+const MICROSERVICE_PATH = '/dashboardservice/api/dashboard';
 
 describe('DashboardService', () => {
   let service: DashboardService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +19,31 @@ describe('DashboardService', () => {
       ]
     });
     service = TestBed.inject(DashboardService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should successfully get dashboard header data', () => {
+    const expectedResponse: DashboardHeaderResponse = {
+      userRithmId: '1234',
+      id: 1,
+      startedDocuments: 5,
+      rosterStations: 4
+    };
+
+    service.getDashboardHeader()
+      .subscribe((response) => {
+        expect(response).toEqual(expectedResponse);
+      });
+
+    // outgoing request
+    const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/header`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expectedResponse);
+    httpTestingController.verify();
   });
 });
