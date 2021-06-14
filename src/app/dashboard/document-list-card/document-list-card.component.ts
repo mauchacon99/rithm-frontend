86fe2beb21ebdc8ea830872b7fb3ee1b91f60a9b
Austin Bagley <aust.bagley@gmail.com>
2021-06-14 16:37:35 -0600

    start process of making responsive

--- src/app/dashboard/document-list-card/document-list-card.component.ts
+++ src/app/dashboard/document-list-card/document-list-card.component.ts
@@ -12,34 +12,39 @@ export class DocumentListCardComponent {
   /** List of documents. */
   @Input() docList = [
     {
-      docName: 'New Doc 1',
-      stationName: 'Station name',
+      docName: 'Really long document name',
+      stationName: 'really long Station name',
       timeInStation: '2 hours',
-      assigned: false
+      assigned: false,
+      priority: 1
     },
     {
       docName: 'New Doc 2',
       stationName: 'Station name',
       timeInStation: '4 hours',
-      assigned: false
+      assigned: true,
+      priority: 2
     },
     {
       docName: 'New Doc 3',
       stationName: 'Station name',
       timeInStation: '5 hours',
-      assigned: false
+      assigned: false,
+      priority: 1
     },
     {
       docName: 'New Doc 4',
       stationName: 'Station name',
       timeInStation: '7 hours',
-      assigned: false
+      assigned: false,
+      priority: 3
     },
     {
       docName: 'New Doc 5',
       stationName: 'Station name',
-      timeInStation: '1 hours',
-      assigned: false
+      timeInStation: '1 hour',
+      assigned: false,
+      priority: 7
     }
   ];
 
