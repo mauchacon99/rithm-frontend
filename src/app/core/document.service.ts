dd80008bfd56979ec43b2c82f3b3f5d1400d5519
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-14 13:27:22 -0600

    style: fix lint errors

--- src/app/core/document.service.ts
+++ src/app/core/document.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+// const MICROSERVICE_PATH = '/documentservice';
+
+/**
+ * Service for all document behavior and business logic.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
 
-  constructor() { }
 }
