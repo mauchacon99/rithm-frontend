8c756d6715c8520a3dad02b1c2ba87609e3b52d8
Austin Bagley <aust.bagley@gmail.com>
2021-06-14 15:07:48 -0600

    change list order, add dummy docs

--- src/app/dashboard/document-list-card/document-list-card.component.ts
+++ src/app/dashboard/document-list-card/document-list-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Component for displaying a list of documents on the dashboard.
@@ -9,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list-card.component.scss']
 })
 export class DocumentListCardComponent {
+  /** List of documents. */
+  @Input() docList = [
+    {
+      docName: 'New Doc 1',
+      stationName: 'Station name',
+      timeInStation: '2 hours',
+      assigned: false
+    },
+    {
+      docName: 'New Doc 2',
+      stationName: 'Station name',
+      timeInStation: '4 hours',
+      assigned: false
+    },
+    {
+      docName: 'New Doc 3',
+      stationName: 'Station name',
+      timeInStation: '5 hours',
+      assigned: false
+    },
+    {
+      docName: 'New Doc 4',
+      stationName: 'Station name',
+      timeInStation: '7 hours',
+      assigned: false
+    },
+    {
+      docName: 'New Doc 5',
+      stationName: 'Station name',
+      timeInStation: '1 hours',
+      assigned: false
+    }
+  ];
+
+  /** Is the data being loaded. */
+  @Input() isLoading = false;
 
 }
