c40096f4cec011320e1e004a5179848ba1d93f39
Austin Bagley <aust.bagley@gmail.com>
2021-06-15 09:17:12 -0600

    fully responsive

--- src/app/dashboard/previously-started-documents/previously-started-documents.component.ts
+++ src/app/dashboard/previously-started-documents/previously-started-documents.component.ts
@@ -9,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./previously-started-documents.component.scss']
 })
 export class PreviouslyStartedDocumentsComponent {
+  /** Temp list of documents. */
+  docsList = [
+    {
+      docName: 'Really long document name',
+      stationName: 'really long Station name',
+      timeInStation: '2 hours',
+      assigned: true,
+      priority: 1
+    },
+    {
+      docName: 'New Doc 2',
+      stationName: 'Station name',
+      timeInStation: '4 hours',
+      assigned: true,
+      priority: 2
+    },
+    {
+      docName: 'New Doc 3',
+      stationName: 'Station name',
+      timeInStation: '5 hours',
+      assigned: true,
+      priority: 1
+    },
+    {
+      docName: 'New Doc 4',
+      stationName: 'Station name',
+      timeInStation: '7 hours',
+      assigned: true,
+      priority: 3
+    },
+    {
+      docName: 'New Doc 5',
+      stationName: 'Station name',
+      timeInStation: '1 hour',
+      assigned: true,
+      priority: 7
+    }
+  ];
 
 }
--- src/app/dashboard/priority-queue/priority-queue.component.ts
+++ src/app/dashboard/priority-queue/priority-queue.component.ts
@@ -9,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./priority-queue.component.scss']
 })
 export class PriorityQueueComponent {
+  /** Temp list of documents. */
+  docsList = [
+    {
+      docName: 'Really long document name',
+      stationName: 'really long Station name',
+      timeInStation: '2 hours',
+      assigned: false,
+      priority: 1
+    },
+    {
+      docName: 'New Doc 2',
+      stationName: 'Station name',
+      timeInStation: '4 hours',
+      assigned: false,
+      priority: 2
+    },
+    {
+      docName: 'New Doc 3',
+      stationName: 'Station name',
+      timeInStation: '5 hours',
+      assigned: false,
+      priority: 1
+    },
+    {
+      docName: 'New Doc 4',
+      stationName: 'Station name',
+      timeInStation: '7 hours',
+      assigned: false,
+      priority: 3
+    },
+    {
+      docName: 'New Doc 5',
+      stationName: 'Station name',
+      timeInStation: '1 hour',
+      assigned: false,
+      priority: 7
+    }
+  ];
 
 }
