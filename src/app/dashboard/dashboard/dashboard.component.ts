6e7da85e8e24e60bf2bcb36c7672dd247b189da6
Austin Bagley <aust.bagley@gmail.com>
2021-06-16 11:58:11 -0600

    ui of list of users

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { RosterModalComponent } from 'src/app/shared/roster-modal/roster-modal.component';
 
 /**
  * Main component for the dashboard screens.
@@ -8,7 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  ngOnInit(): void {
+    this.dialog.open(RosterModalComponent, {
+      minWidth: '325px'
+    });
+  }
 
 }
--- src/app/shared/roster-modal/roster-modal.component.ts
+++ src/app/shared/roster-modal/roster-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Reusable component for displaying the worker roster for a station.
@@ -9,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./roster-modal.component.scss']
 })
 export class RosterModalComponent {
+  /** Roster type. */
+  @Input() isWorker = true;
 
+  /** Station id. */
+  @Input() stationId = 0;
+
+  /** Station name. */
+  @Input() stationName = 'Station name';
+
+  /** Temp users list. */
+  users = [
+    {
+      firstName: 'Maggie',
+      lastName: 'Rhee',
+      email: 'maggie.rhee@email.com',
+      initials: 'MR'
+    },
+    {
+      firstName: 'Tyreese',
+      lastName: 'Williams',
+      email: 'tyreese.williams@email.com',
+      initials: 'TW'
+    },
+    {
+      firstName: 'Lizzie',
+      lastName: 'Samuels',
+      email: 'lizzie.samuels@email.com',
+      initials: 'LS'
+    },
+    {
+      firstName: 'Theodore',
+      lastName: 'Douglas',
+      email: 'theodore.douglas@email.com',
+      initials: 'TD'
+    },
+    {
+      firstName: 'Maggie',
+      lastName: 'Rhee',
+      email: 'maggie.rhee@email.com',
+      initials: 'MR'
+    }
+  ];
 }
