9a317dbc751917fdc71bc327f24185ec8fedc638
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-16 13:31:03 -0600

    test: additional unit tests for top nav

--- src/app/navigation/top-nav/top-nav.component.spec.ts
+++ src/app/navigation/top-nav/top-nav.component.spec.ts
@@ -5,14 +5,20 @@ import { MockUserService } from 'src/mocks';
 import { UserService } from 'src/app/core/user.service';
 
 import { TopNavComponent } from './top-nav.component';
+import { By } from '@angular/platform-browser';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatButtonHarness } from '@angular/material/button/testing';
 
 describe('TopNavComponent', () => {
   let component: TopNavComponent;
   let fixture: ComponentFixture<TopNavComponent>;
+  let loader: HarnessLoader;
+  let notificationButtonHarness: MatButtonHarness;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TopNavComponent ],
+      declarations: [TopNavComponent],
       imports: [
         MatMenuModule,
         RouterTestingModule
@@ -21,12 +27,14 @@ describe('TopNavComponent', () => {
         { provide: UserService, useClass: MockUserService }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(TopNavComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    notificationButtonHarness = await loader.getHarness(MatButtonHarness.with({ selector: '#notification-button' }));
     fixture.detectChanges();
   });
 
@@ -34,19 +42,33 @@ describe('TopNavComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('should display logo', () => {
-    expect(component).toBeTruthy();
+  it('should display logo', () => {
+    const logo = fixture.debugElement.query(By.css('.logo'));
+    expect(logo).toBeTruthy();
   });
 
-  xit('should display notifications', () => {
-    expect(component).toBeTruthy();
+  it('should have notifications button', async () => {
+    expect(notificationButtonHarness).toBeTruthy();
   });
+
+  xit('should toggle notifications pane when button clicked', async () => {
+    const notificationsSpy = spyOn(component, 'toggleNotifications');
+    await notificationButtonHarness.click();
+    expect(notificationsSpy).toHaveBeenCalledOnceWith();
+    expect(component.notificationsVisible).toBeTrue();
+  });
+
+  // TODO: research karma-viewport for testing the following
 
   xit('should display a tab for every nav item', () => {
     expect(component).toBeTruthy();
   });
 
   xit('should display user profile', () => {
+    expect(component).toBeTruthy();
+  });
+
+  xit('should allow the user to sign out', () => {
     expect(component).toBeTruthy();
   });
 });
