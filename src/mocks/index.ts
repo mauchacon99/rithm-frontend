02629879fadbd6b555abb4fa4a663a6116b99041
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-16 12:01:14 -0600

    chore: initial mock notification service

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,4 +1,5 @@
 export * from './mock-dashboard-service';
+export * from './mock-notification-service';
 export * from './mock-popup-service';
 export * from './mock-station-service';
 export * from './mock-user-service';
