fb99295297d09602d4c029bd66c577d60e273770
Harrison King <harrison.king@inpivota.com>
2021-06-24 13:45:40 -0600

    re-applied overwritten changes

--- src/app/shared/station-documents-modal/station-documents-modal.component.ts
+++ src/app/shared/station-documents-modal/station-documents-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { DocumentService } from '../../core/document.service';
 import { first } from 'rxjs/operators';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -34,7 +35,8 @@ export class StationDocumentsModalComponent implements OnInit {
 
   constructor(private documentService: DocumentService,
     private errorService: ErrorService,
-    private utcTimeConversion: UtcTimeConversion) { }
+    private utcTimeConversion: UtcTimeConversion,
+    private dialogRef: MatDialogRef<StationDocumentsModalComponent>) { }
 
   /**
    * Gets the first page of documents on load.
@@ -79,5 +81,12 @@ export class StationDocumentsModalComponent implements OnInit {
     return this.utcTimeConversion.getElapsedTimeText(
       this.utcTimeConversion.getMillisecondsElapsed(timeEntered)
     );
+  }
+
+  /**
+   * Close modal after clicking a link.
+   */
+   closeModal(): void {
+    this.dialogRef.close();
   }
 }
