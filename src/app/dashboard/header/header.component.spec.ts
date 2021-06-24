6242084fdb6f54413d17fb6362d10ac198de0c4b
Harrison King <harrison.king@inpivota.com>
2021-06-24 11:12:59 -0600

    commit before switching branches

--- src/app/dashboard/header/header.component.spec.ts
+++ src/app/dashboard/header/header.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardService } from '../dashboard.service';
+import { DocumentService } from 'src/app/core/document.service';
 import { HeaderComponent } from './header.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MockDashboardService, MockPopupService, MockUserService } from 'src/mocks';
+import { MockDashboardService, MockPopupService, MockUserService, MockDocumentService } from 'src/mocks';
 import { PopupService } from 'src/app/core/popup.service';
 import { UserService } from 'src/app/core/user.service';
 
@@ -20,8 +21,9 @@ describe('HeaderComponent', () => {
       ],
       providers: [
         { provide: DashboardService, useClass: MockDashboardService },
+        { provide: DocumentService, useClass: MockDocumentService },
         { provide: UserService, useClass: MockUserService },
-        { provide: PopupService, useClass: MockPopupService }
+        { provide: PopupService, useClass: MockPopupService },
       ]
     })
       .compileComponents();
@@ -43,6 +45,10 @@ describe('HeaderComponent', () => {
 
   it('should return dashboard header data for previous documents', () => {
     expect(component.numStations).toBeGreaterThanOrEqual(0);
+  });
+
+  it('should link to a document page when you press "Start Working"', () => {
+    expect;
   });
 
 });
