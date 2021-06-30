c62e4ea167687667975e99733539b3d0163a5627
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-06-29 22:05:04 -0600

    chore: remove test comment

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,5 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-// TODO: remove me
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
