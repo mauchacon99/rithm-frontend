753af5e9cb7e238f9bb510790da10fe71946853d
Austin Bagley <aust.bagley@gmail.com>
2021-07-05 10:37:16 -0600

    add station card component

---
+++ src/app/detail/station-card/station-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-station-card',
+  templateUrl: './station-card.component.html',
+  styleUrls: ['./station-card.component.scss']
+})
+export class StationCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
