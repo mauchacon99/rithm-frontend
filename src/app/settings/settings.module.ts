84844d6bf24bc00a3b9162b8632b53afe22661bd
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-06 11:44:25 -0600

    chore: add initial general account settings component

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { GeneralAccountSettingsComponent } from './general-account-settings/general-account-settings.component';
 
 
 
 @NgModule({
   declarations: [
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    GeneralAccountSettingsComponent
   ],
   imports: [
     CommonModule,
