c8a4ac0fefcd7796bff604aad4c0056f64787903
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-14 07:51:24 -0600

    chore: add drawer component and module

---
+++ src/app/drawer/drawer/drawer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * Reusable component for a drawer.
+ */
+@Component({
+  selector: 'app-drawer',
+  templateUrl: './drawer.component.html',
+  styleUrls: ['./drawer.component.scss']
+})
+export class DrawerComponent {
+
+
+}
