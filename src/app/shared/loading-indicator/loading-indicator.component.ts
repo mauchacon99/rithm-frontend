58255ee47cc0d7a70c2f32d2b4bec67f32f4150c
Austin Bagley <aust.bagley@gmail.com>
2021-07-14 15:00:14 -0600

    docs

--- src/app/shared/loading-indicator/loading-indicator.component.ts
+++ src/app/shared/loading-indicator/loading-indicator.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+/**
+ * Loading indicator component.
+ */
 @Component({
   selector: 'app-loading-indicator',
   templateUrl: './loading-indicator.component.html',
   styleUrls: ['./loading-indicator.component.scss']
 })
-export class LoadingIndicatorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class LoadingIndicatorComponent {
 
 }
