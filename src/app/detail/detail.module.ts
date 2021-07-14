3bac35599059316fd144c50fc21732c64ee2087b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-14 14:35:12 -0600

    chore: rename history drawer component for consistency

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -5,7 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { StationInfoHeaderComponent } from './station-info-header/station-info-header.component';
 import { DocumentInfoHeaderComponent } from './document-info-header/document-info-header.component';
 import { ConnectedStationPaneComponent } from './connected-station-pane/connected-station-pane.component';
-import { HistoryComponent } from './history/history.component';
+import { HistoryDrawerComponent } from './history-drawer/history-drawer.component';
 import { CommentComponent } from './comment/comment.component';
 import { DocumentTemplateComponent } from './document-template/document-template.component';
 import { SubHeaderComponent } from './sub-header/sub-header.component';
@@ -22,11 +22,11 @@ import { CommentDrawerComponent } from './comment-drawer/comment-drawer.componen
     StationInfoHeaderComponent,
     DocumentInfoHeaderComponent,
     ConnectedStationPaneComponent,
-    HistoryComponent,
     CommentComponent,
     DocumentTemplateComponent,
     SubHeaderComponent,
     StationCardComponent,
+    HistoryDrawerComponent,
     CommentDrawerComponent
   ],
   imports: [
---
+++ src/app/detail/history-drawer/history-drawer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * Component for the history pane of a document/station.
+ */
+@Component({
+  selector: 'app-history-drawer',
+  templateUrl: './history-drawer.component.html',
+  styleUrls: ['./history-drawer.component.scss']
+})
+export class HistoryDrawerComponent {
+
+}
