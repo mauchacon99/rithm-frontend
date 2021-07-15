22612e8fb643baeab88c76ca2558fc62b31b56c1
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-15 14:16:25 -0600

    chore: change detail drawer contents with item type

--- src/app/detail/detail-drawer/detail-drawer.component.ts
+++ src/app/detail/detail-drawer/detail-drawer.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Component for all content inside the drawer for detail (station/document) pages.
  */
 @Component({
-  selector: 'app-detail-drawer',
+  selector: 'app-detail-drawer[itemType]',
   templateUrl: './detail-drawer.component.html',
   styleUrls: ['./detail-drawer.component.scss']
 })
 export class DetailDrawerComponent {
+
+  /**
+   * The type of detail item to display in the drawer.
+   */
+  @Input()
+  itemType!: 'comments' | 'history';
 
 }
