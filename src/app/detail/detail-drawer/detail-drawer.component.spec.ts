e044c842efba0e21bfcc76dc89d02ba192f29ca2
Austin Bagley <aust.bagley@gmail.com>
2021-07-17 22:25:40 -0600

    add material modules to test files

--- src/app/detail/detail-drawer/detail-drawer.component.spec.ts
+++ src/app/detail/detail-drawer/detail-drawer.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MockComponent } from 'ng-mocks';
 import { CommentDrawerComponent } from '../comment-drawer/comment-drawer.component';
 import { HistoryDrawerComponent } from '../history-drawer/history-drawer.component';
@@ -15,6 +17,10 @@ describe('DetailDrawerComponent', () => {
         DetailDrawerComponent,
         MockComponent(CommentDrawerComponent),
         MockComponent(HistoryDrawerComponent)
+      ],
+      imports: [
+        MatDialogModule,
+        MatSnackBarModule
       ]
     })
     .compileComponents();
