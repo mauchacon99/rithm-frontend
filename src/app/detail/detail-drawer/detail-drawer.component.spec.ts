bd400cbdadcc8e6c6fef39dea99ec4470619bda8
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-18 09:18:53 -0600

    test: update test dependencies

--- src/app/detail/detail-drawer/detail-drawer.component.spec.ts
+++ src/app/detail/detail-drawer/detail-drawer.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MockComponent } from 'ng-mocks';
+import { ErrorService } from 'src/app/core/error.service';
+import { MockErrorService } from 'src/mocks';
 import { CommentDrawerComponent } from '../comment-drawer/comment-drawer.component';
 import { HistoryDrawerComponent } from '../history-drawer/history-drawer.component';
 
@@ -18,9 +18,8 @@ describe('DetailDrawerComponent', () => {
         MockComponent(CommentDrawerComponent),
         MockComponent(HistoryDrawerComponent)
       ],
-      imports: [
-        MatDialogModule,
-        MatSnackBarModule
+      providers: [
+        { provide: ErrorService, useClass: MockErrorService }
       ]
     })
     .compileComponents();
