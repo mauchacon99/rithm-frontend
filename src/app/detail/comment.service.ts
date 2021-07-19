b6ac31d321e2f4c9e5b69b3972120cc503d1c47c
Adarsh <adarsh.achar@inpivota.com>
2021-07-19 10:47:51 +0530

    RIT-628 comment service changes

--- src/app/detail/comment.service.ts
+++ src/app/detail/comment.service.ts
@@ -34,14 +34,14 @@ export class CommentService {
   }
 
   /**
-    * Gets a list of comments.
-    *
-    * @param documentId The documentId of document for which comments needs to be fetched.
-    * @param stationId Id of station for which comments needs to be fetched.
-    * @param pageNumber The desired page number of results.
-    * @param commentsPerPage The limit of comments per page.
-    * @returns A list of comments based on documentId and stationId.
-    */
+   * Gets a list of comments.
+   *
+   * @param documentId The documentId of document for which comments needs to be fetched.
+   * @param stationId Id of station for which comments needs to be fetched.
+   * @param pageNumber The desired page number of results.
+   * @param commentsPerPage The limit of comments per page.
+   * @returns A list of comments based on documentId and stationId.
+   */
   getDocumentComments(documentId: string, stationId: string, pageNumber: number, commentsPerPage: number): Observable<Comment[]> {
     const comments: Comment[] = [{
       displayText: 'This is first comment',
