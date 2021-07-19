21282e85b6a8d888f44d76d73c24e5b0bc41ef50
Harrison King <harrison.king@inpivota.com>
2021-07-19 11:42:11 -0600

    setting up getDocumentComments

--- src/app/detail/comment-drawer/comment-drawer.component.ts
+++ src/app/detail/comment-drawer/comment-drawer.component.ts
@@ -15,17 +15,21 @@ import { Comment } from 'src/models';
   styleUrls: ['./comment-drawer.component.scss']
 })
 export class CommentDrawerComponent implements OnInit {
-  /** Is the posted comment loading? */
-  loadingPostedComment = false;
-
-  /** The posted comment data. */
-  postedComment?: Comment;
 
   /** Is the content being loaded. */
   isLoading = true;
 
   /** Are more comments being loaded. */
-  loadMore = true;
+  loadingMoreComments = false;
+
+  /** Current page to get comments from. */
+  commentPage = 0;
+
+  /** Is the posted comment loading? */
+  loadingPostedComment = false;
+
+  /** The posted comment data. */
+  postedComment?: Comment;
 
   /** List of comments for a document. */
   comments: Comment[] = [];
@@ -40,26 +44,29 @@ export class CommentDrawerComponent implements OnInit {
    */
   ngOnInit(): void {
     // getDocumentComments()
-    setTimeout(() => this.isLoading = false, 2000);
+    setTimeout(() => this.isLoading = false, 500);
   }
 
   /**
-   * Gets a list of comments.
+   * Gets the initial list of comments to load.
    *
    * @param documentId The documentId of document for which comments needs to be fetched.
    * @param stationId Id of station for which comments needs to be fetched.
-   * @param pageNumber The desired page number of results.
+   * @param pageNumber The desired page number of results, use this.commentPage.
    * @param commentsPerPage The limit of comments per page.
+   * @param initialGet Is this an initial get of comments?
    */
-  getDocumentComments(documentId: string, stationId: string, pageNumber: number, commentsPerPage: number): void {
-    this.isLoading = true;
+  // eslint-disable-next-line max-len
+  getDocumentComments(documentId: string, stationId: string, pageNumber: number, commentsPerPage: number, initialGet: boolean): void {
+    initialGet ? this.isLoading = true : this.loadingMoreComments = true;
     this.commentService.getDocumentComments(documentId, stationId, pageNumber, commentsPerPage)
       .pipe(first())
       .subscribe((commentsResponse) => {
         this.comments = commentsResponse;
-        this.isLoading = false;
+        ++this.commentPage;
+        initialGet ? this.isLoading = false : this.loadingMoreComments = false;
       }, (error: HttpErrorResponse) => {
-        this.isLoading = false;
+        initialGet ? this.isLoading = false : this.loadingMoreComments = false;
         this.errorService.displayError(
           'Something went wrong on our end and we\'re looking into it. Please try again in a little while.',
           error
