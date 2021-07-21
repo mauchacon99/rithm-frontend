845a9eeed6e1c4b8baa6369e59f6cceba6dd752d
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-21 15:59:12 -0600

    test: add unit test for form submission

--- src/app/detail/comment-input/comment-input.component.spec.ts
+++ src/app/detail/comment-input/comment-input.component.spec.ts
@@ -1,5 +1,8 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonHarness } from '@angular/material/button/testing';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +12,7 @@ import { CommentInputComponent } from './comment-input.component';
 describe('CommentInputComponent', () => {
   let component: CommentInputComponent;
   let fixture: ComponentFixture<CommentInputComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,10 +30,20 @@ describe('CommentInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentInputComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should require a comment before posting', async () => {
+    const buttonHarness = await loader.getHarness(MatButtonHarness.with({ selector: '#commentPostButton' }));
+    const comment = component.commentForm.controls['comment'];
+    expect(comment.valid).toBeFalse();
+    expect(comment.hasError('required')).toBeTrue();
+    expect(component.commentForm.valid).toBeFalse();
+    expect(await buttonHarness.isDisabled()).toBeTrue();
   });
 });
