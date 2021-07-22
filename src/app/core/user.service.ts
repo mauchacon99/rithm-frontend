19705f6c1272b809467f5caf8f65c01b431cb48d
Adarsh <adarsh.achar@inpivota.com>
2021-07-22 14:06:40 +0530

    RIT-694 remove mode param from getDocumentInfo request

--- src/app/core/user.service.ts
+++ src/app/core/user.service.ts
@@ -45,7 +45,6 @@ export class UserService {
     }, { withCredentials: true }).pipe(
       map((response) => {
         this.accessToken = new AccessToken(response.accessToken);
-        console.log(this.accessToken);
         this.user = response.user;
         localStorage.setItem('refreshTokenGuid', response.refreshTokenGuid);
         localStorage.setItem('user', JSON.stringify(this.user));
