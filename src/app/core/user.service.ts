81781f9a277dd4f107763b0fe89c574029fe9d46
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-22 09:52:29 -0600

    revert: re-add withCredentials to sign in

--- src/app/core/user.service.ts
+++ src/app/core/user.service.ts
@@ -42,7 +42,7 @@ export class UserService {
     return this.http.post<SignInResponse>(`${environment.baseApiUrl}${MICROSERVICE_PATH}/login`, {
       email,
       password
-    }).pipe(
+    }, { withCredentials: true }).pipe(
       map((response) => {
         this.accessToken = new AccessToken(response.accessToken);
         this.user = response.user;
