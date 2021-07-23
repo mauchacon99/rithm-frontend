f370952c6abd6a684f1d4560febd5cd24922291b
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-23 15:27:44 -0600

    chore: initial component structure for template area

---
+++ src/app/detail/address-field/address-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-field',
+  templateUrl: './address-field.component.html',
+  styleUrls: ['./address-field.component.scss']
+})
+export class AddressFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/detail/check-field/check-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-field',
+  templateUrl: './check-field.component.html',
+  styleUrls: ['./check-field.component.scss']
+})
+export class CheckFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
