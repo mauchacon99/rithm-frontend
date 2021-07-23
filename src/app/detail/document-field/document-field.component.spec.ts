56120ec281c2952ac18b22f7de476711f439bbe4
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-23 16:01:40 -0600

    test: fix document field tests because Tyler is dumb

--- src/app/detail/document-field/document-field.component.spec.ts
+++ src/app/detail/document-field/document-field.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+import { AddressFieldComponent } from '../address-field/address-field.component';
+import { CheckFieldComponent } from '../check-field/check-field.component';
+import { DateFieldComponent } from '../date-field/date-field.component';
+import { NumberFieldComponent } from '../number-field/number-field.component';
+import { SelectFieldComponent } from '../select-field/select-field.component';
+import { TextFieldComponent } from '../text-field/text-field.component';
 
 import { DocumentFieldComponent } from './document-field.component';
 
@@ -8,7 +15,15 @@ describe('DocumentFieldComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DocumentFieldComponent ]
+      declarations: [
+        DocumentFieldComponent,
+        MockComponent(TextFieldComponent),
+        MockComponent(NumberFieldComponent),
+        MockComponent(DateFieldComponent),
+        MockComponent(AddressFieldComponent),
+        MockComponent(SelectFieldComponent),
+        MockComponent(CheckFieldComponent),
+      ]
     })
     .compileComponents();
   });
