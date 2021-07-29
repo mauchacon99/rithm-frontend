1577a9bf19e076573b7a563bec4187fe5edab81c
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-29 14:51:13 -0600

    Revert "RIT-710 Update unit tests for reset password guard"
    This reverts commit 0a8765636704149dcd552761cb1ec0dd48732c7c.
    
    revert: unit test commit

--- src/app/core/reset-password.guard.spec.ts
+++ src/app/core/reset-password.guard.spec.ts
@@ -1,23 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRouteSnapshot, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { EmailLinkParams } from 'src/helpers';
-import { EmailLinkType } from 'src/models';
 
 import { ResetPasswordGuard } from './reset-password.guard';
 
 describe('ResetPasswordGuard', () => {
   let guard: ResetPasswordGuard;
-  let route: ActivatedRouteSnapshot;
-  const data = { queryParamMap: convertToParamMap({ type: 'reset', guid: 'kj343kh2o3ih23ih423', email: 'johndoe@email.com' }) };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
-      ],
-      providers: [
-        { provide: ActivatedRouteSnapshot, useValue: data }
       ]
     });
     guard = TestBed.inject(ResetPasswordGuard);
@@ -25,14 +17,6 @@ describe('ResetPasswordGuard', () => {
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
-  });
-
-  it('should determine if the path has valid query params', () => {
-    route = TestBed.inject(ActivatedRouteSnapshot);
-    guard.canActivate(route);
-    const linkParams = new EmailLinkParams(route.queryParamMap);
-    expect(linkParams.valid).toBeTrue();
-    expect(linkParams.type).toBe(EmailLinkType.forgotPassword);
   });
 
   xit('should navigate back to sign in if params are invalid', () => {
