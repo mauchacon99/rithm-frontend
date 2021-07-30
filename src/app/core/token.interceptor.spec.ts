90c1629281390b28188853d22d07b4ced5210bee
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-07-30 16:17:26 -0600

    test: refactor token interceptor tests, branch to 50%

--- src/app/core/token.interceptor.spec.ts
+++ src/app/core/token.interceptor.spec.ts
@@ -1,21 +1,22 @@
-/* eslint-disable  */
-import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+/* eslint-disable rxjs/no-ignored-error */
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { TokenInterceptor } from './token.interceptor';
 import { MockPopupService, MockUserService } from 'src/mocks';
-import { UserService } from './user.service';
 import { PopupService } from './popup.service';
 import { environment } from 'src/environments/environment';
 import { DashboardService } from '../dashboard/dashboard.service';
+import { UserService } from './user.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AccessToken } from 'src/helpers';
 
-const MICROSERVICE_PATH = '/userservice/api/user';
+const USER_SERVICE_PATH = '/userservice/api/user';
 
 describe('TokenInterceptor', () => {
-  let httpClient: HttpClient;
-  let http: HttpTestingController;
+  let httpTestingController: HttpTestingController;
+  let userService: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,24 +26,13 @@ describe('TokenInterceptor', () => {
       ],
       providers: [
         TokenInterceptor,
+        UserService,
         { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
         { provide: PopupService, useClass: MockPopupService },
-        UserService,
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: TokenInterceptor,
-          multi: true,
-        },
-        DashboardService,
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: TokenInterceptor,
-          multi: true,
-        },
       ]
     });
-    httpClient = TestBed.inject(HttpClient);
-    http = TestBed.inject(HttpTestingController);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    userService = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
@@ -50,34 +40,35 @@ describe('TokenInterceptor', () => {
     expect(interceptor).toBeTruthy();
   });
 
-  it('should add a token on auth-required routes', () => {
-    // http.post('domain.com/api/createflow', {flow: 'some flow info...'})
-    // .subscribe((response) => {
-    //   expect(response).toBeFalsy();
-    // });
-    // const httpRequest = httpTesting.expectOne('domain.com/api/createflow');
-    // expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
-    // expect(httpRequest.request.headers.get('Authorization')).toBe('');
-  });
+  it('should add a token on auth-required routes', fakeAsync(() => {
+    const token = 'j3434k-j3k4j3k-jk34jk3';
+    userService.accessToken = new AccessToken(token); // Ensure user is signed in
+    userService.updateUserAccount({}).subscribe();
+
+    tick(1000);
+
+    const httpRequest = httpTestingController.expectOne(`${environment.baseApiUrl}${USER_SERVICE_PATH}/update`);
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
+    expect(httpRequest.request.headers.get('Authorization')).toEqual(`Bearer ${token}`);
+    httpTestingController.verify();
+  }));
 
   it('should pass the request on regular routes', () => {
-    const service = TestBed.inject(UserService);
-    service.getTermsConditions().subscribe(response => {
-      expect(response).toBeTruthy();
-    });
-    const httpRequest = http.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/gettermsandconditions`);
+    userService.getTermsConditions().subscribe();
+
+    const httpRequest = httpTestingController.expectOne(`${environment.baseApiUrl}${USER_SERVICE_PATH}/gettermsandconditions`);
     expect(httpRequest.request.headers.has('Authorization')).toEqual(false);
   });
 
   it('should sign the user out if an auth-required request is made when the user is not signed in', () => {
-    const service = TestBed.inject(DashboardService);
-    const spy = spyOn(TestBed.inject(UserService), 'signOut');
-    let res;
-    service.getDashboardHeader().subscribe(response => {
-      res = response;
-    });
-    expect(spy).toHaveBeenCalled();
-    expect(res).toBeUndefined();
+    userService.signOut(); // Ensure that the user is actually signed out before test
+    const signOutSpy = spyOn(userService, 'signOut');
+
+    userService.updateUserAccount({}).subscribe();
+
+    httpTestingController.expectNone(`${environment.baseApiUrl}${USER_SERVICE_PATH}/update`);
+    httpTestingController.verify();
+    expect(signOutSpy).toHaveBeenCalledTimes(1);
   });
 
 });
