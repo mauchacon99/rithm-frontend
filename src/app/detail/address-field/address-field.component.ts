6fe670405353f87aa968f58e77e5826fa5c7462b
Harrison King <harrison.king@inpivota.com>
2021-08-02 12:51:32 -0600

    prelim commit

--- src/app/detail/address-field/address-field.component.ts
+++ src/app/detail/address-field/address-field.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FieldType, Question } from 'src/models';
 
 /**
  * Reusable component for every address field.
@@ -9,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./address-field.component.scss']
 })
 export class AddressFieldComponent {
+
+    /** The document field to display. */
+    @Input() field!: Question;
+
+    /** The field type of the input. */
+    fieldTypeEnum = FieldType;
 
 }
