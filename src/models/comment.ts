afe98e4c2648074dfbba69f22c6b0bee7b29aef1
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-08-02 13:30:13 -0600

    chore: update comment model to reflect API

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -1,12 +1,15 @@
-import { Document } from './document';
-import { Station } from './station';
-
 /**
  * Represents all info about a comment on a station or document.
  */
 export interface Comment {
   /** The text to display. */
   displayText: string;
+
+  /** Id of station where the comment is located. */
+  stationRithmId: string;
+
+  /** Id of document where the comment is located. Only present for document comments. */
+  documentRithmId?: string;
 
   /** The date the comment was created. */
   dateCreated?: string;
@@ -23,25 +26,18 @@ export interface Comment {
   /** Id of user that made the comment. */
   userRithmId?: string;
 
-  /** Id of document where the comment is located. */
-  documentRithmId?: string;
-
-  /** Document where comment is located. */
-  document?: Document;
-
-  /** Id of station where the comment is located. */
-  stationRithmId?: string;
-
-  /** Station where comment is located. */
-  station?: Station;
-
   /** First name of the user that made the comment. */
   userFirstName?: string;
 
   /** Last name of user the that made the comment. */
   userLastName?: string;
 
-  /** Full name of user the that made the comment. */
+  /**
+   * Full name of user the that made the comment.
+   *
+   * @deprecated Don't use this; it might be removed in the future. Use `userFirstName`
+   * and `userLastName` instead.
+   */
   userFullName?: string;
 
 }
