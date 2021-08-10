fd6099323eb24bbd0ba6c679e54c00fcdea0bc3f
Harrison King <harrison.king@inpivota.com>
2021-08-10 13:42:51 -0600

    prelim commit

--- src/app/dashboard/station-card/station-card.component.ts
+++ src/app/dashboard/station-card/station-card.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { DashboardStationData } from 'src/models';
+import { DashboardStationData, UserType } from 'src/models';
 import { MatDialog } from '@angular/material/dialog';
 import { StationDocumentsModalComponent } from 'src/app/shared/station-documents-modal/station-documents-modal.component';
+import { Router } from '@angular/router';
 
 /**
  * Component for displaying a card with station information on the dashboard.
@@ -15,7 +16,27 @@ export class StationCardComponent {
   /** The station info to display. */
   @Input() station!: DashboardStationData;
 
-  constructor(private dialog: MatDialog) {}
+  /** Role of the user. */
+  userType = UserType.None;
+
+
+  constructor(
+    private dialog: MatDialog,
+    private router: Router
+  ) { }
+
+  /**
+   * Determines if User has permission to proceed to a linked document.
+   *
+   * @param rithmId The rithmId property of the document we will link to.
+   */
+  checkDocPermission(rithmId: string): void {
+    if (this.userType === UserType.Admin) {
+      //this.router.navigateByUrl(`/station/${rithmId}`);
+      this.router.navigate(
+        [`/station/${rithmId}`], { queryParams: { stationId: this.station.rithmId }});
+    }
+  }
 
   /**
    * Opens Station Docs Modal with document information.
