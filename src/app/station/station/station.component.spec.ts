d2408b538a8c45ed42a99f0004314d6cf2cb358d
Adarsh <adarsh.achar@inpivota.com>
2021-08-13 00:56:49 +0530

    RIT-842 RIT-837 - UI for statio edit mode

--- src/app/station/station/station.component.spec.ts
+++ src/app/station/station/station.component.spec.ts
@@ -1,14 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from 'ng-mocks';
+import { DocumentService } from 'src/app/core/document.service';
+import { ErrorService } from 'src/app/core/error.service';
+import { DashboardComponent } from 'src/app/dashboard/dashboard/dashboard.component';
+import { ConnectedStationPaneComponent } from 'src/app/detail/connected-station-pane/connected-station-pane.component';
+import { DetailDrawerComponent } from 'src/app/detail/detail-drawer/detail-drawer.component';
+import { DocumentInfoHeaderComponent } from 'src/app/detail/document-info-header/document-info-header.component';
+import { DocumentTemplateComponent } from 'src/app/detail/document-template/document-template.component';
+import { StationInfoHeaderComponent } from 'src/app/detail/station-info-header/station-info-header.component';
+import { SubHeaderComponent } from 'src/app/detail/sub-header/sub-header.component';
+import { LoadingIndicatorComponent } from 'src/app/shared/loading-indicator/loading-indicator.component';
+import { MockDocumentService, MockErrorService } from 'src/mocks';
 
 import { StationComponent } from './station.component';
 
 describe('StationComponent', () => {
   let component: StationComponent;
   let fixture: ComponentFixture<StationComponent>;
+  const formBuilder = new FormBuilder();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StationComponent ]
+      declarations: [
+        StationComponent,
+        MockComponent(SubHeaderComponent),
+        MockComponent(DetailDrawerComponent),
+        MockComponent(ConnectedStationPaneComponent),
+        MockComponent(StationInfoHeaderComponent),
+        MockComponent(DocumentInfoHeaderComponent),
+        MockComponent(DocumentTemplateComponent),
+        MockComponent(LoadingIndicatorComponent)
+       ],
+      imports: [
+        NoopAnimationsModule,
+        RouterTestingModule.withRoutes(
+          [{ path: 'dashboard', component: MockComponent(DashboardComponent) }]
+        ),
+        MatSidenavModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        { provide: FormBuilder, useValue: formBuilder },
+        { provide: DocumentService, useClass: MockDocumentService },
+        { provide: ErrorService, useClass: MockErrorService }
+      ]
     })
     .compileComponents();
   });
