3bb7263ae01aec6e4bf07f4713b5352cf276ec0d
Harrison King <harrison.king@inpivota.com>
2021-08-12 11:48:23 -0600

    setting up station information on station

--- src/app/station/station/station.component.ts
+++ src/app/station/station/station.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DocumentStationInformation } from 'src/models';
 
 /**
  * Main component for viewing a station.
@@ -10,4 +11,55 @@ import { Component } from '@angular/core';
 })
 export class StationComponent {
 
+  /** The information about the document within a station. */
+  documentInformation!: DocumentStationInformation;
+
+  constructor() {
+    this.documentInformation = {
+      documentName: '',
+      documentPriority: 0,
+      currentAssignedUser: '',
+      flowedTimeUTC: '',
+      lastUpdatedUTC: '',
+      stationId: '',
+      stationName: 'Station name',
+      stationPriority: 0,
+      numberOfSupervisors: 2,
+      supervisorRoster: [
+        {
+          userRithmId: '',
+          firstName: 'T',
+          lastName: 'H',
+          email: '',
+          isAssigned: true
+        },
+        {
+          userRithmId: '',
+          firstName: 'A',
+          lastName: 'B',
+          email: '',
+          isAssigned: true
+        },
+      ],
+      numberOfWorkers: 5,
+      workerRoster: [
+        {
+          userRithmId: '',
+          firstName: 'H',
+          lastName: 'K',
+          email: '',
+          isAssigned: true
+        },
+        {
+          userRithmId: '',
+          firstName: 'A',
+          lastName: 'A',
+          email: '',
+          isAssigned: true
+        },
+      ],
+      documentRithmId: '',
+      questions: []
+    };
+  }
 }
