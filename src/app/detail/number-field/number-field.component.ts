1f61beb286875d4349a9c4eaefa2672faff5fd2a
Harrison King <harrison.king@inpivota.com>
2021-08-12 09:59:17 -0600

    update lint disable

--- src/app/detail/number-field/number-field.component.ts
+++ src/app/detail/number-field/number-field.component.ts
@@ -88,7 +88,7 @@ export class NumberFieldComponent implements OnInit, ControlValueAccessor, Valid
    *
    * @param val The value to be written.
    */
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  // eslint-disable-next-line
   writeValue(val: any): void {
     val && this.numberFieldForm.setValue(val, { emitEvent: false });
   }
@@ -98,7 +98,7 @@ export class NumberFieldComponent implements OnInit, ControlValueAccessor, Valid
    *
    * @param fn The function to register.
    */
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  // eslint-disable-next-line
   registerOnChange(fn: any): void {
     // TODO: check for memory leak
     // eslint-disable-next-line rxjs-angular/prefer-takeuntil
