d3b404845e158ed5ee5119ca47975d6e27cebe23
Austin Bagley <aust.bagley@gmail.com>
2021-08-13 16:37:48 -0600

    setup basics for station-template

--- src/app/station/station-field/station-field.component.ts
+++ src/app/station/station-field/station-field.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Question } from 'src/models';
 /**
  * Station Field Component.
  */
 @Component({
-  selector: 'app-station-field',
+  selector: 'app-station-field[field]',
   templateUrl: './station-field.component.html',
   styleUrls: ['./station-field.component.scss']
 })
 export class StationFieldComponent {
+  /** The document field to display. */
+  @Input() field!: Question;
 
 }
--- src/app/station/station-template/station-template.component.ts
+++ src/app/station/station-template/station-template.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { QuestionFieldType } from 'src/models';
 /**
  * Station Template Component.
  */
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./station-template.component.scss']
 })
 export class StationTemplateComponent {
+  /** The form to add to station. */
+  stationTemplateForm!: FormGroup;
+
+  /** The general instructions field. */
+  readonly stationInstructionsField = {
+    prompt: 'General Instructions',
+    instructions: '',
+    questionType: {
+      rithmId: '',
+      typeString: QuestionFieldType.LongText,
+      validationExpression: '.+',
+    },
+    isReadOnly: false,
+    isRequired: false,
+    isPrivate: false
+  };
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.stationTemplateForm = this.fb.group({
+      stationFieldForm: this.fb.control('')
+    });
+  }
 
 }
