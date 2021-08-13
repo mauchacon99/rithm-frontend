a7638a43cfa3f6ea943f61a0f6c831ec458ad2aa
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-08-13 11:35:51 -0600

    chore: remove unnecessary changes

--- src/app/dashboard/station-card/station-card.component.ts
+++ src/app/dashboard/station-card/station-card.component.ts
@@ -2,8 +2,6 @@ import { Component, Input } from '@angular/core';
 import { DashboardStationData } from 'src/models';
 import { MatDialog } from '@angular/material/dialog';
 import { StationDocumentsModalComponent } from 'src/app/shared/station-documents-modal/station-documents-modal.component';
-import { Router } from '@angular/router';
-import { UserService } from 'src/app/core/user.service';
 
 /**
  * Component for displaying a card with station information on the dashboard.
@@ -17,28 +15,9 @@ export class StationCardComponent {
   /** The station info to display. */
   @Input() station!: DashboardStationData;
 
-  /** Does the user have admin privileges? */
-  isAdmin = false;
-
   constructor(
-    private dialog: MatDialog,
-    private router: Router,
-    private userService: UserService
-  ) {
-    if (this.userService.user.role === 'admin') {
-      this.isAdmin = true;
-    }
-  }
-
-  /**
-   * Determines if User has permission to proceed to a linked document.
-   */
-  checkStationEditPermission(): void {
-    if (this.isAdmin) {
-      this.router.navigate(
-        [`/station`, this.station.rithmId]);
-    }
-  }
+    private dialog: MatDialog
+  ) {}
 
   /**
    * Opens Station Docs Modal with document information.
