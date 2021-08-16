f0482a94f4ca7853f7d8cf59a2ee72db349c02f5
Adarsh <adarsh.achar@inpivota.com>
2021-08-16 19:35:05 +0530

    RIT-551 Add tooltip to roster icons - PR changes

--- src/app/shared/user-avatar/user-avatar.component.ts
+++ src/app/shared/user-avatar/user-avatar.component.ts
@@ -19,6 +19,9 @@ export class UserAvatarComponent {
   /** Whether this avatar is being used to display the signed in user in the top navigation. */
   @Input() navProfile = false;
 
+  /** Whether this avatar is being used to display the signed in user in the top navigation. */
+  @Input() hideToolTip!: boolean;
+
   /**
    * The first + last initials for the user.
    *
