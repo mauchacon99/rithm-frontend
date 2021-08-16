978ff2deeed225ac7f5c79a2915313c64de89eb9
Austin Bagley <aust.bagley@gmail.com>
2021-08-16 11:23:30 -0600

    update unit tests

--- src/app/station/station-field/station-field.component.spec.ts
+++ src/app/station/station-field/station-field.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MockComponent } from 'ng-mocks';
 import { TextFieldComponent } from 'src/app/detail/text-field/text-field.component';
+import { QuestionFieldType } from 'src/models';
 
 import { StationFieldComponent } from './station-field.component';
 
@@ -25,6 +26,18 @@ describe('StationFieldComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StationFieldComponent);
     component = fixture.componentInstance;
+    component.field = {
+      prompt: 'prompt',
+      instructions: 'instructions',
+      questionType: {
+        rithmId: '',
+        typeString: QuestionFieldType.Number,
+        validationExpression: '.+'
+      },
+      isReadOnly: false,
+      isRequired: false,
+      isPrivate: false
+    };
     fixture.detectChanges();
   });
 
--- src/app/station/station-template/station-template.component.spec.ts
+++ src/app/station/station-template/station-template.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockComponent } from 'ng-mocks';
+import { TextFieldComponent } from 'src/app/detail/text-field/text-field.component';
 
 import { StationTemplateComponent } from './station-template.component';
 
@@ -8,7 +11,13 @@ describe('StationTemplateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StationTemplateComponent ]
+      declarations: [
+        StationTemplateComponent,
+        MockComponent(TextFieldComponent)
+      ],
+      imports: [
+        ReactiveFormsModule
+      ],
     })
     .compileComponents();
   });
