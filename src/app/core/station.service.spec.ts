d4f98f49d0b76fdb1ef0808fede03ba6d7759cbe
Adarsh <adarsh.achar@inpivota.com>
2021-08-18 16:34:16 +0530

    RIT-885 Integrate with /getstationinfo route

--- src/app/core/station.service.spec.ts
+++ src/app/core/station.service.spec.ts
@@ -28,11 +28,50 @@ describe('StationService', () => {
 
   it('should return station information', () => {
     const stationId = 'E204F369-386F-4E41';
+    const expectedResponse = {
+      rithmId: stationId,
+      name: 'Dry Goods & Liquids',
+      instructions: 'General instructions',
+      dueDate: '2021-08-22T17:26:47.3506612Z',
+      nextStations: [{
+        stationName: 'Development',
+        totalDocuments: 5,
+        isGenerator: true
+      }],
+      previousStations: [{
+        stationName: 'Station-1',
+        totalDocuments: 2,
+        isGenerator: true
+      }],
+      supervisors: [{
+        userRithmId: '',
+        firstName: 'Marry',
+        lastName: 'Poppins',
+        email: 'marrypoppins@inpivota.com'
+      }],
+      workers: [{
+        userRithmId: '',
+        firstName: 'Harry',
+        lastName: 'Potter',
+        email: 'harrypotter@inpivota.com'
+      }],
+      createdByRithmId: 'ED6148C9-PBK8-408E-A210-9242B2735B1C',
+      createdDate: '2021-07-16T17:26:47.3506612Z',
+      updatedByRithmId: 'AO970Z9-PBK8-408E-A210-9242B2735B1C',
+      updatedDate: '2021-07-18T17:26:47.3506612Z',
+      questions: []
+    };
 
     service.getStationInfo(stationId)
       .subscribe((response) => {
         expect(response).toBeDefined();
       });
+
+    const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/getstationinfo?stationRithmId=${stationId}`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(expectedResponse);
+    httpTestingController.verify();
   });
 
   it('should return a list of all stations', () => {
@@ -47,7 +86,7 @@ describe('StationService', () => {
         expect(response).toBeDefined();
       });
 
-      // outgoing request
+    // outgoing request
     const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}`);
     expect(req.request.method).toEqual('GET');
 
