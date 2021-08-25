ebb08dca8e2f9d7645b28ca093f794e1d3bdfd24
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-08-25 13:22:29 -0600

    chore: separated organization management component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { OrganizationManagementComponent } from './organization-management/organization-management.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    OrganizationManagementComponent
   ],
   imports: [
     CommonModule,
