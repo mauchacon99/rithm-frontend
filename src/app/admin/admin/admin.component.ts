b34e053fbc566ca18dc9293f2665915c40df97d1
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-08-25 11:36:26 -0600

    style: fix lint errors

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * Component for the main admin landing page.
+ */
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class AdminComponent {
 
 }
