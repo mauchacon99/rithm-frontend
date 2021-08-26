19e4af40af44feaa23869b107d188929dbeef6a7
Harrison King <harrison.king@inpivota.com>
2021-08-26 11:36:23 -0600

    copying code from station-documents-modal

--- src/app/admin/organization-management/organization-management.component.ts
+++ src/app/admin/organization-management/organization-management.component.ts
@@ -12,5 +12,41 @@ export class OrganizationManagementComponent {
   /** Whether the account settings is loading. */
   isLoading = false;
 
+  /** The current page number. */
+  /** Total number of users in this organization. */
+  totalNumUsers = 0;
 
+  /**
+   * Gets the first page of users on load.
+   */
+  ngOnInit(): void {
+    this.getUsers(1);
+  }
+
+  /**
+   * Gets a page list of users.
+   *
+   * @param pageNum The desired page of user results.
+   */
+  //TODO: look into making this more DRY since this method is a near copy of StationDocumentsModalComponent.getDocuments
+  getUsers(pageNum: number): void {
+    this.activeNum = pageNum;
+    this.isLoading = true;
+    this.documentService.getStationDocuments(this.stationRithmId, pageNum)
+      .pipe(first())
+      .subscribe((documentsResponse) => {
+        if (documentsResponse) {
+          this.documents = documentsResponse.documents;
+          this.totalNumDocs = documentsResponse.totalDocuments;
+          this.userType = documentsResponse.userType;
+        }
+        this.isLoading = false;
+      }, (error: unknown) => {
+        this.isLoading = false;
+        this.dialogRef.close();
+        this.errorService.displayError(
+          'Something went wrong on our end and we\'re looking into it. Please try again in a little while.',
+          error
+        );
+      });
 }
