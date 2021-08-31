489c53d0064310d43de6a61b8a5ddce23b5f5dd1
Adarsh <adarsh.achar@inpivota.com>
2021-08-31 17:30:23 +0530

    RIT-1086 Create interface to match the /GetUsersForOrganization API

---
+++ src/models/organization-users.ts
@@ -0,0 +1,18 @@
+import { User } from './user';
+/**
+ * Represents all Users for an organization.
+ */
+export interface OrganizationUsers {
+
+  /** The total users of an organization. */
+  totalOrgUsers: number;
+
+  /** The current page number. */
+  currentPageNum: number;
+
+  /** The number of users per page. */
+  userPerPage: number;
+
+  /** Users list of the current page. */
+  users: User[];
+}
