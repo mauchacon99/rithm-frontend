9eeba4e4d5d8c2b5e867a928f41cc85be995ad3a
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-08 20:09:00 -0600

    test: update API path and add additional test for org user removal

--- src/app/core/organization.service.spec.ts
+++ src/app/core/organization.service.spec.ts
@@ -74,10 +74,27 @@ describe('OrganizationService', () => {
       });
 
     // eslint-disable-next-line max-len
-    const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/getusersforOrganization?rithmid=${organizationId}&pageNum=${pageNum}&usersPerPage=${expectedResponse.userPerPage}`);
+    const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/users-organization?rithmid=${organizationId}&pageNum=${pageNum}&usersPerPage=${expectedResponse.userPerPage}`);
     expect(req.request.method).toEqual('GET');
 
     req.flush(expectedResponse);
+    httpTestingController.verify();
+  });
+
+  it('should remove a user from an organization', () => {
+    const organizationId = 'kdjfkd-kjdkfjd-jkjdfkdjk';
+    const userId = 'kdjfkd-kjdkfjd-jkjdfkdjk';
+
+    service.removeUserFromOrganization(organizationId, userId)
+      .subscribe((response) => {
+        expect(response).toBeFalsy();
+      });
+
+    // eslint-disable-next-line max-len
+    const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/users-organization`);
+    expect(req.request.method).toEqual('DELETE');
+
+    req.flush(null);
     httpTestingController.verify();
   });
 
