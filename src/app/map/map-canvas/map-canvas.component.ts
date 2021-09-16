d4dca4b2da7bb2b8b6f8ce6247b90a87c5eddefa
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-15 21:09:56 -0600

    chore: add initial canvas component methods and stubs

--- src/app/map/map-canvas/map-canvas.component.ts
+++ src/app/map/map-canvas/map-canvas.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Point } from 'src/models';
+import { MapService } from '../map.service';
+import { StationElementService } from '../station-element.service';
 
 /**
  * Component for the main `<canvas>` element used for the map.
@@ -8,6 +11,148 @@ import { Component } from '@angular/core';
   templateUrl: './map-canvas.component.html',
   styleUrls: ['./map-canvas.component.scss']
 })
-export class MapCanvasComponent {
+export class MapCanvasComponent implements OnInit {
 
+  /** The rendering context for the canvas element for the map. */
+  private context!: CanvasRenderingContext2D;
+
+  /** Reference to the main canvas element used for the map. */
+  @ViewChild('map', { static: true }) private mapCanvas!: ElementRef<HTMLCanvasElement>;
+
+  constructor(
+    private mapService: MapService,
+    private stationElementService: StationElementService
+  ) {}
+
+  /**
+   * Scales the canvas and does initial draw for elements.
+   */
+  ngOnInit(): void {
+    this.context = this.mapCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+    this.mapService.registerCanvasContext(this.context);
+
+    this.scaleCanvasForDPI();
+    this.drawElements();
+  }
+
+  /**
+   * Handles user input when a mouse button is pressed. Used for initiating dragging.
+   *
+   * @param event The mousedown event that was triggered.
+   */
+  @HostListener('mousedown', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  mouseDown(event: MouseEvent): void {
+    // TODO: Handle behavior when mouse is pressed
+  }
+
+  /**
+   * Handles user input when a mouse button is released. Used for placing dragged elements.
+   *
+   * @param event The mouseup event that was triggered.
+   */
+  @HostListener('mouseup', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  mouseUp(event: MouseEvent): void {
+    // TODO: Handle behavior when mouse button is released
+  }
+
+  /**
+   * Handles user input when a mouse cursor is moved. Used for calculating dragged element movement, or map pan drag.
+   *
+   * @param event The mousemove event that was triggered.
+   */
+  @HostListener('mousemove', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  mouseMove(event: MouseEvent): void {
+    // TODO: Handle behavior when mouse is moved
+  }
+
+  /**
+   * Handles user input when a mouse button is clicked. Used for clicking on elements.
+   *
+   * @param event The click event that was triggered.
+   */
+  @HostListener('click', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  click(event: MouseEvent): void {
+    // TODO: Handle behavior when mouse is clicked
+  }
+
+  /**
+   * Handles user input when a mouse button is right clicked. Used for bringing up the right click menu.
+   *
+   * @param event The contextmenu event that was triggered.
+   */
+  @HostListener('contextmenu', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  contextMenu(event: MouseEvent): void {
+    // TODO: Handle behavior when mouse is right clicked
+  }
+
+  /**
+   * Handles user input when a keyboard key is pressed. Used for keyboard shortcuts.
+   *
+   * @param event The keydown event that was triggered.
+   */
+  @HostListener('keydown', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  keyDown(event: KeyboardEvent): void {
+    // TODO: Handle behavior when key is pressed
+  }
+
+  /**
+   * Handles user input when a mouse wheel is scrolled. Used for zoom.
+   *
+   * @param event The wheel event that was triggered.
+   */
+  @HostListener('wheel', ['$event'])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  wheel(event: WheelEvent): void {
+    // TODO: Handle behavior when mouse wheel is scrolled
+  }
+
+  /**
+   * Draws all the elements on the canvas.
+   */
+   private drawElements(): void {
+    requestAnimationFrame(() => {
+      // Clear the canvas
+      this.context.clearRect(0, 0, this.mapCanvas.nativeElement.width, this.mapCanvas.nativeElement.height);
+
+      // Calculate the station canvas points
+
+      // Draw the connections
+
+      // Draw the stations
+
+      // Draw the flows
+    });
+  }
+
+  /**
+   * Scales the canvas for accurate display on HiDPI/Retina displays.
+   */
+  private scaleCanvasForDPI(): void {
+    const pixelRatio = window.devicePixelRatio || 1;
+    const canvasBoundingRect = this.mapCanvas.nativeElement.getBoundingClientRect();
+
+    this.mapCanvas.nativeElement.width = canvasBoundingRect.width * pixelRatio;
+    this.mapCanvas.nativeElement.height = canvasBoundingRect.height * pixelRatio;
+    this.context.scale(pixelRatio, pixelRatio);
+  }
+
+  /**
+   * Determines the point on the canvas that the mouse cursor is positioned.
+   *
+   * @param event The mouse event for the cursor information.
+   * @returns An accurate point for the mouse position on the canvas.
+   */
+   private getMouseCanvasPoint(event: MouseEvent): Point {
+    const canvasRect = this.mapCanvas.nativeElement.getBoundingClientRect();
+    return {
+      x: event.clientX - canvasRect.left,
+      y: event.clientY - canvasRect.top
+    };
+  }
 }
