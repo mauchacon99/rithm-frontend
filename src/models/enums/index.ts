09d3eeabcf74da7977e4eb0b62fd76f9e246427a
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-15 21:41:43 -0600

    chore: add enum for hover types for a station element

--- src/models/enums/index.ts
+++ src/models/enums/index.ts
@@ -2,3 +2,4 @@ export * from './email-link-type.enum';
 export * from './user-type.enum';
 export * from './question-field-type.enum';
 export * from './map-mode.enum';
+export * from './station-element-hover-type.enum';
