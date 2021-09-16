fcc1234fd8533f4f86bb8328f24fdb59a85331eb
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-16 14:57:48 -0600

    chore: add methods and stubs for drawing flows

--- src/app/map/flow-element.service.ts
+++ src/app/map/flow-element.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Point } from 'src/models';
+import { MapService } from './map.service';
 
 /**
  * Service for rendering and other behavior for a flow on the map.
@@ -6,4 +8,113 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class FlowElementService { }
+export class FlowElementService {
+
+  /** The rendering this.canvasContext for the canvas element for the map. */
+  private canvasContext?: CanvasRenderingContext2D;
+
+  constructor(
+    private mapService: MapService
+  ) { }
+
+  /**
+   * Draws a flow on the map.
+   */
+  drawFlow(): void {
+    this.canvasContext = this.mapService.canvasContext;
+    if (!this.canvasContext) {
+      throw new Error('Cannot draw flow if context is not defined');
+    }
+    // TODO: Draw flow
+  }
+
+  /**
+   * Gets a new array of points representing the convex hull of the given set of points. The convex hull excludes collinear points.
+   * This algorithm runs in O(n log n) time.
+   *
+   * @param points The unsorted points for which to get the points.
+   * @returns The convex hull for the points.
+   */
+  private getConvexHull(points: Point[]): Point[] {
+    const newPoints = points.slice();
+    newPoints.sort(this.comparePoints);
+    return this.getConvexHullPresorted(newPoints);
+  }
+
+  /**
+   * Gets the convex hull of a pre-sorted set of points.
+   *
+   * @param points The sorted points to be contained in the convex hull.
+   * @returns The convex hull for the points.
+   */
+  private getConvexHullPresorted(points: Point[]): Point[] {
+    if (points.length <= 1) {
+      return points.slice();
+    }
+
+    // Andrew's monotone chain algorithm. Positive y coordinates correspond to "up"
+    // as per the mathematical convention, instead of "down" as per the computer
+    // graphics convention. This doesn't affect the correctness of the result.
+
+    // TODO: Refactor this; some duplicated code and vague variable names
+
+    const upperHull: Point[] = [];
+    for (let i = 0; i < points.length; i++) {
+      const p = points[i];
+      while (upperHull.length >= 2) {
+        const q = upperHull[upperHull.length - 1];
+        const r = upperHull[upperHull.length - 2];
+        if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x)) {
+          upperHull.pop();
+        } else {
+          break;
+        }
+      }
+      upperHull.push(p);
+    }
+    upperHull.pop();
+
+    const lowerHull: Point[] = [];
+    for (let i = points.length - 1; i >= 0; i--) {
+      const p = points[i];
+      while (lowerHull.length >= 2) {
+        const q = lowerHull[lowerHull.length - 1];
+        const r = lowerHull[lowerHull.length - 2];
+        if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x)) {
+          lowerHull.pop();
+        } else {
+          break;
+        }
+      }
+      lowerHull.push(p);
+    }
+    lowerHull.pop();
+
+    if (upperHull.length === 1 && lowerHull.length === 1 && upperHull[0].x === lowerHull[0].x && upperHull[0].y === lowerHull[0].y) {
+      return upperHull;
+    } else {
+      return upperHull.concat(lowerHull);
+    }
+  }
+
+  /**
+   * Compares the position of one point to another.
+   *
+   * @param pointA The first point to compare.
+   * @param pointB The second point to compare.
+   * @returns A number indicating the position of `pointA` in relation to `pointB`.
+   */
+  private comparePoints(pointA: Point, pointB: Point): number {
+    if (pointA.x < pointB.x) {
+      return -1;
+    } else if (pointA.x > pointB.x) {
+      return 1;
+    } else if (pointA.y < pointB.y) {
+      return -1;
+    } else if (pointA.y > pointB.y) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
