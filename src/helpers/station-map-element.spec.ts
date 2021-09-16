70fdcc0e9dfdc785bc0a008eb4450d5d34a64e07
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-15 22:22:18 -0600

    chore: create class and interface for station map element

---
+++ src/helpers/station-map-element.spec.ts
@@ -0,0 +1,7 @@
+import { StationMapElement } from './station-map-element';
+
+describe('StationMapElement', () => {
+  it('should create an instance', () => {
+    expect(new StationMapElement()).toBeTruthy();
+  });
+});
