79b57b973c3a432aa2aecde913d7c6db9383e919
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-09-16 15:00:53 -0600

    chore: stub methods in map overlay

--- src/app/map/map-overlay/map-overlay.component.ts
+++ src/app/map/map-overlay/map-overlay.component.ts
@@ -10,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class MapOverlayComponent {
 
+  /**
+   * Enters build mode for the map.
+   */
+   build(): void {
+    // TODO: Implement build
+  }
+
+  /**
+   * Publishes map changes.
+   */
+   publish(): void {
+    // TODO: Implement publish
+  }
+
+  /**
+   * Cancels the map changes and returns to view mode.
+   */
+   cancel(): void {
+    // TODO: Implement cancel
+  }
+
+  /**
+   * Zooms the map in to center.
+   */
+   zoomIn(): void {
+    // TODO: Implement zoom in
+  }
+
+  /**
+   * Zooms the map out from center.
+   */
+  zoomOut(): void {
+    // TODO: Implement zoom out
+  }
+
 }
