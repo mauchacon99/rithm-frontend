f0b33508ea70583364d511f4a0e23634338d2dab
Adarsh <adarsh.achar@inpivota.com>
2021-09-21 21:20:17 +0530

    Unit test issue fixed

--- src/app/map/map.service.spec.ts
+++ src/app/map/map.service.spec.ts
@@ -17,17 +17,30 @@ describe('MapService', () => {
   });
 
   it('should return all map elements for a given organization', () => {
-    const expectedResponse: StationMapData[] = [{
-      id: 'ED6148C9-ABB7-408E-A210-9242B2735B1C',
-      name: 'Development',
-      numberOfDocuments: 5,
-      mapPoint: {
-        x: 12,
-        y: 15
+    const expectedResponse: StationMapData[] = [
+      {
+        id: 'ED6148C9-ABB7-408E-A210-9242B2735B1C',
+        name: 'Development',
+        numberOfDocuments: 5,
+        mapPoint: {
+          x: 12,
+          y: 15
+        },
+        incomingStationIds: ['ED6148C9-ABB7-408E-A210-9242B2735B1C', 'AAAEBE98-YU01-97ER-A7BB-285PP25B0989'],
+        outgoingStationIds: ['CCAEBE24-AF01-48AB-A7BB-279CC25B0989', 'CCCAAA00-IO01-97QW-Z7LK-877MM25Z0989']
       },
-      incomingStationIds: ['ED6148C9-ABB7-408E-A210-9242B2735B1C', 'AAAEBE98-YU01-97ER-A7BB-285PP25B0989'],
-      outgoingStationIds: ['CCAEBE24-AF01-48AB-A7BB-279CC25B0989', 'CCCAAA00-IO01-97QW-Z7LK-877MM25Z0989']
-    }];
+      {
+        id: 'CCAEBE24-AF01-48AB-A7BB-279CC25B0989',
+        name: 'Step 1',
+        numberOfDocuments: 5,
+        mapPoint: {
+          x: 12,
+          y: 15
+        },
+        incomingStationIds: ['ED6148C9-ABB7-408E-A210-9242B2735B1C'],
+        outgoingStationIds: ['CCAEBE24-AF01-48AB-A7BB-279CC25B0989']
+      }
+    ];
 
     service.getMapElements()
       .subscribe((response) => {
