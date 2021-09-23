70b6157abe1042d1ffe8146796fd10f5b80b7e9c
Publio Quintero <publio.quintero@gmail.com>
2021-09-23 09:23:42 -0600

    Update map-toolbar.component.ts

--- src/app/map/map-toolbar/map-toolbar.component.ts
+++ src/app/map/map-toolbar/map-toolbar.component.ts
@@ -9,82 +9,82 @@ import { User, OrganizationInfo } from 'src/models';
  * Component for managing the toolbar on the map.
  */
 @Component({
-  selector: 'app-map-toolbar',
-  templateUrl: './map-toolbar.component.html',
-  styleUrls: ['./map-toolbar.component.scss'],
+	selector: 'app-map-toolbar',
+	templateUrl: './map-toolbar.component.html',
+	styleUrls: ['./map-toolbar.component.scss'],
 })
 export class MapToolbarComponent implements OnInit {
+	/** The users of the organization. */
+	users: User[] = [];
 
-  /** The users of the organization. */
-  users: User[] = [];
+	/** The current signed in user. */
+	currentUser!: User;
 
-  /** The current signed in user. */
-  currentUser!: User;
+	/** Whether the organization information is loading. */
+	orgLoading = false;
 
-  /** Whether the organization information is loading. */
-  orgLoading = false;
+	/** The organization information object. */
+	orgInfo?: OrganizationInfo;
 
-  /** The organization information object. */
-  orgInfo?: OrganizationInfo;
+	constructor(
+		private userService: UserService,
+		private organizationService: OrganizationService,
+		private errorService: ErrorService
+	) { }
 
-  constructor(
-    private userService: UserService,
-    private organizationService: OrganizationService,
-    private errorService: ErrorService
-  ) {}
+	/**
+	 * Gets the first page of users on load.
+	 */
+	ngOnInit(): void {
+		this.currentUser = this.userService.user;
+		this.getOrganizationInfo();
+	}
 
-  /**
-   * Gets the first page of users on load.
-   */
-  ngOnInit(): void {
-    this.currentUser = this.userService.user;
-    this.getOrganizationInfo();
-  }
+	/**
+	 * Sets the map to add flow mode in preparation for a flow to be selected.
+	 */
+	addFlow(): void {
+		// TODO: Implement add flow
+	}
 
-  /**
-   * Sets the map to add flow mode in preparation for a flow to be selected.
-   */
-  addFlow(): void {
-    // TODO: Implement add flow
-  }
+	/**
+	 * Sets the map to add station mode in preparation for a station to be selected.
+	 */
+	addStation(): void {
+		// TODO: Implement add station
+	}
 
-  /**
-   * Sets the map to add station mode in preparation for a station to be selected.
-   */
-  addStation(): void {
-    // TODO: Implement add station
-  }
+	// MVP +1 below
 
-  // MVP +1 below
+	// undo(): void {}
+	// redo(): void {}
+	// search(): void {}
 
-  // undo(): void {}
-  // redo(): void {}
-  // search(): void {}
-
-  /**
-   * Gets organization information.
-   */
-  getOrganizationInfo(): void {
-    this.orgLoading = true;
-    //const organizationId: string = this.userService.user?.organizations[0];
-    //const organizationId: string = localStorage.getItem('user');
-    this.organizationService
-      .getOrganizationInfo('CCAEBE24-AF01-48AB-A7BB-279CC25B0989')
-      .pipe(first())
-      .subscribe(
-        (organization) => {
-          if (organization) {
-            this.orgInfo = organization;
-          }
-          this.orgLoading = false;
-        },
-        (error: unknown) => {
-          this.orgLoading = false;
-          this.errorService.displayError(
-            'Something went wrong on our end and we\'re looking into it. Please try again in a little while.',
-            error
-          );
-        }
-      );
-  }
+	/**
+	 * Gets organization information.
+	 */
+	getOrganizationInfo(): void {
+    //console.log(this.userService.user?.organization)
+		this.orgLoading = true;
+		//const organizationId: string = this.userService.user?.organizations[0];
+		//const organizationId: string = localStorage.getItem('user');
+		this.organizationService
+			.getOrganizationInfo('CCAEBE24-AF01-48AB-A7BB-279CC25B0989')
+			.pipe(first())
+			.subscribe(
+				(organization) => {
+					if (organization) {
+						this.orgInfo = organization;
+					}
+					this.orgLoading = false;
+				},
+				(error: unknown) => {
+					this.orgLoading = false;
+					this.errorService.displayError(
+						'Something went wrong on our end and we\'re looking into it. Please try again in a little while.',
+						error
+					);
+				}
+			);
+	}
 }
