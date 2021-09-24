285a2bd3afe9edb5e161936bb4c8dcaa18310067
Harrison King <harrison.king@inpivota.com>
2021-09-24 16:07:39 -0600

    added mockMapService to test

--- src/app/map/map-toolbar/map-toolbar.component.spec.ts
+++ src/app/map/map-toolbar/map-toolbar.component.spec.ts
@@ -2,7 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ErrorService } from 'src/app/core/error.service';
 import { OrganizationService } from 'src/app/core/organization.service';
 import { UserService } from 'src/app/core/user.service';
-import { MockErrorService, MockOrganizationService, MockUserService } from 'src/mocks';
+import { MockErrorService, MockMapService, MockOrganizationService, MockUserService } from 'src/mocks';
+import { MapService } from '../map.service';
 
 import { MapToolbarComponent } from './map-toolbar.component';
 
@@ -16,7 +17,8 @@ describe('MapToolbarComponent', () => {
       providers: [
         { provide: UserService, useClass: MockUserService },
         { provide: ErrorService, useClass: MockErrorService },
-        { provide: OrganizationService, useClass: MockOrganizationService }
+        { provide: OrganizationService, useClass: MockOrganizationService },
+        { provide: MapService, useClass: MockMapService }
       ]
     }).compileComponents();
   });
