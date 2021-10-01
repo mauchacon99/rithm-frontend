d300d169d54213a9c7b08296f9be53bc6f2e470b
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-10-01 14:57:56 -0400

    pass test unit ng test
    -Understand procees in service mapMode

--- src/app/map/map-overlay/map-overlay.component.ts
+++ src/app/map/map-overlay/map-overlay.component.ts
@@ -68,7 +68,7 @@ export class MapOverlayComponent implements OnDestroy {
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.complete();
-    this.mapService.cancelMapChanges();
+    this.mapService.mapMode$.next(MapMode.view);
   }
 
   /**
