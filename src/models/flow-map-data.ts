141487e91767c8ffef6fdc890b2f77d639255a87
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-10-04 14:09:23 -0600

    chore: add status property to flow model, update docs

--- src/models/flow-map-data.ts
+++ src/models/flow-map-data.ts
@@ -1,3 +1,5 @@
+import { MapItemStatus } from '.';
+
 /**
  * Represents all info about a flow as returned from the API.
  */
@@ -8,9 +10,17 @@ export interface FlowMapData {
   /** The name of the flow. */
   name: string;
 
-  /** The ids of the stations contained immediately within this flow. Does not include stations in nested flows. */
+  /**
+   * The ids of the stations contained immediately within this flow. Does not include stations in nested flows (immediate children only).
+   */
   stationIds: string[];
 
-  /** The ids of the flows contained immediately within this flow. Does not include flows nested more than one level deep. */
+  /**
+   * The ids of the flows contained immediately within this flow. Does not include flows nested more than one level deep (immediate children
+   * only).
+   */
   flowIds: string[];
+
+  /** The status of the flow (what should happen to this flow). */
+  status: MapItemStatus;
 }
