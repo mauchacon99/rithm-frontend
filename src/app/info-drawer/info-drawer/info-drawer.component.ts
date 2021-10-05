16003b8f6e9f04dd8d5f9a2df5d3f7c1a01574af
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-10-05 14:46:12 -0400

    Update info-drawer.component.ts

--- src/app/info-drawer/info-drawer/info-drawer.component.ts
+++ src/app/info-drawer/info-drawer/info-drawer.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class InfoDrawerComponent implements OnInit {
 
   constructor() {
-    // TODO: IMPORTS MODULES
+    // TODO: import modules
   }
 
   /**
