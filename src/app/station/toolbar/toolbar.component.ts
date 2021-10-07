e15b316df4aab9ed72079d7e9ce9a42263a89527
jhonnystrut <jhonny.zambrano@strutsoftwaresolutions.com>
2021-10-07 16:05:15 -0400

    RTM-1208
    error was in toolbar.component.ts because FieldTypeEnum was neted

--- src/app/station/toolbar/toolbar.component.ts
+++ src/app/station/toolbar/toolbar.component.ts
@@ -45,7 +45,7 @@ export class ToolbarComponent {
     {
       name: 'Address',
       icon: 'far fa-address-card',
-      typeString: this.fieldTypeEnum.Nested
+      typeString: this.fieldTypeEnum.AddressLine
     },
     {
       name: 'Number',
