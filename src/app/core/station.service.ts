1a81d122317c23953ba90ba5ba4f7e2614c94ec6
Cesar Augusto Quijada Macuart <cesar.quijada@strutsoftwaresolutions.com>
2021-10-14 10:33:49 -0500

    minor changes rxjs objects

--- src/app/core/station.service.ts
+++ src/app/core/station.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Station, StationInformation } from 'src/models';
 
