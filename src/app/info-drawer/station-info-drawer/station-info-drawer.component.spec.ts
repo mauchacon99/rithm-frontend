df017dff5442fc41d9738b591db503dc61c97470
Cesar Augusto Quijada Macuart <cesar.quijada@strutsoftwaresolutions.com>
2021-10-18 17:15:27 -0500

    RTM-1277 Alert dialog added

--- src/app/info-drawer/station-info-drawer/station-info-drawer.component.spec.ts
+++ src/app/info-drawer/station-info-drawer/station-info-drawer.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StationInfoDrawerComponent } from './station-info-drawer.component';
 import { StationService } from 'src/app/core/station.service';
-import { MockErrorService, MockStationService, MockUserService } from 'src/mocks';
+import { MockErrorService, MockPopupService, MockStationService, MockUserService } from 'src/mocks';
 import { ErrorService } from 'src/app/core/error.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UserService } from 'src/app/core/user.service';
@@ -12,6 +12,7 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
+import { PopupService } from 'src/app/core/popup.service';
 
 describe('StationInfoDrawerComponent', () => {
   let component: StationInfoDrawerComponent;
@@ -36,7 +37,8 @@ describe('StationInfoDrawerComponent', () => {
         { provide: UserService, useClass: MockUserService },
         { provide: FormBuilder, useValue: formBuilder },
         { provide: StationService, useClass: MockStationService },
-        { provide: ErrorService, useClass: MockErrorService }
+        { provide: ErrorService, useClass: MockErrorService },
+        { provide: PopupService, useClass: MockPopupService },
       ]
     })
       .compileComponents();
