75c5fec95ef9a78553cc4cf69355193dfab68e53
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-10-21 12:55:48 -0400

    resolve conflicts in merge to dev

--- src/app/core/station.service.spec.ts
+++ src/app/core/station.service.spec.ts
@@ -266,13 +266,14 @@ describe('StationService', () => {
         expect(response).toEqual(expectedResponse);
 
       });
-
-    it('should delete a station', () => {
-      const stationId = 'E204F369-386F-4E41';
-
-      service.deleteStation(stationId)
-        .subscribe((response) => {
-          expect(response).toBeFalsy();
-        });
-    });
   });
+
+  it('should delete a station', () => {
+    const stationId = 'E204F369-386F-4E41';
+
+    service.deleteStation(stationId)
+      .subscribe((response) => {
+        expect(response).toBeFalsy();
+      });
+  });
+});
