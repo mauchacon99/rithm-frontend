2c3069ada4ae28482db71a8d02b2613ee7846891
Cesar Augusto Quijada Macuart <cesar.quijada@strutsoftwaresolutions.com>
2021-10-21 18:52:12 -0500

    RTM-1279 Api connected with failures

--- src/app/core/station.service.ts
+++ src/app/core/station.service.ts
@@ -106,8 +106,7 @@ export class StationService {
    * @param stationId The Specific id of station.
    * @returns Returns an empty observable.
    */
-   // eslint-disable-next-line @typescript-eslint/no-unused-vars
    deleteStation(stationId: string): Observable<unknown> {
-      return of(void 0).pipe(delay(1000));
+    return this.http.delete<void>(`${environment.baseApiUrl}${MICROSERVICE_PATH}/${stationId}`);
    }
 }
