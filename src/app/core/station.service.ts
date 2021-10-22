de65af9f769df050f59e3a0d03de3f3d3bc362ee
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-10-22 16:25:55 -0400

    resolve response type text and add pipe in response http get

--- src/app/core/station.service.ts
+++ src/app/core/station.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { DocumentGenerationStatus, Question, Station, StationInformation } from 'src/models';
 
@@ -71,7 +71,8 @@ export class StationService {
   getStationDocumentGenerationStatus(stationId: string): Observable<DocumentGenerationStatus> {
     const params = new HttpParams()
       .set('rithmId', stationId);
-    return this.http.get<DocumentGenerationStatus>(`${environment.baseApiUrl}${MICROSERVICE_PATH}/generator-status`, { params });
+    return this.http.get(`${environment.baseApiUrl}${MICROSERVICE_PATH}/generator-status`, { params, responseType: 'text' })
+      .pipe(map((value) => value as DocumentGenerationStatus));
   }
 
   /**
