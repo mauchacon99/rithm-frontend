a1d4fed3ef38ef0c4fc0518150dc86ac92708f7a
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-10-26 16:06:35 -0400

    Update station.service.ts

--- src/app/core/station.service.ts
+++ src/app/core/station.service.ts
@@ -109,7 +109,7 @@ export class StationService {
    * @param pageNum The current page.
    * @returns Users for the organization bind to station.
    */
-   getPotentialStationRosterMembers(organizationId: string, stationRithmId: string, pageNum: number): Observable<StationRosterMember[]> {
+  getPotentialStationRosterMembers(organizationId: string, stationRithmId: string, pageNum: number): Observable<StationRosterMember[]> {
     if (!organizationId || !pageNum) {
       return throwError(new HttpErrorResponse({
         error: {
@@ -117,31 +117,12 @@ export class StationService {
         }
       })).pipe(delay(1000));
     } else {
-      const orgUsers: StationRosterMember[] = [{
-        rithmId: '12dasd1-asd12asdasd-asdas',
-        firstName: 'Cesar',
-        lastName: 'Quijada',
-        email: 'strut@gmail.com',
-        isOwner: true,
-        isWorker: true,
-      },
-      {
-        rithmId: '12dasd1-asd12asdasd-ffff1',
-        firstName: 'Maria',
-        lastName: 'Quintero',
-        email: 'Maquin@gmail.com',
-        isOwner: true,
-        isWorker: true,
-      },
-      {
-        rithmId: '12dasd1-asd12asdasd-a231',
-        firstName: 'Pedro',
-        lastName: 'Perez',
-        email: 'pperez@gmail.com',
-        isOwner: true,
-        isWorker: true,
-      }];
-      return of(orgUsers).pipe(delay(1000));
+      const params = new HttpParams()
+        .set('stationRithmId', stationRithmId)
+        .set('organizationRithmId', organizationId)
+        .set('pageNum', pageNum)
+        .set('workersPerPage', 20);
+      return this.http.get<StationRosterMember[]>(`${environment.baseApiUrl}${MICROSERVICE_PATH}/worker-roster`, { params });
     }
   }
 
@@ -151,7 +132,7 @@ export class StationService {
    * @param stationId The Specific id of station.
    * @returns Returns an empty observable.
    */
-   deleteStation(stationId: string): Observable<unknown> {
+  deleteStation(stationId: string): Observable<unknown> {
     return this.http.delete<void>(`${environment.baseApiUrl}${MICROSERVICE_PATH}/${stationId}`);
-   }
+  }
 }
