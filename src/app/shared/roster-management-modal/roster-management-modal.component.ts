9ed7d7bf8b6c1894b2f1049d7deabb2b017e4aac
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-10-27 09:07:43 -0400

    Update roster-management-modal.component.ts

--- src/app/shared/roster-management-modal/roster-management-modal.component.ts
+++ src/app/shared/roster-management-modal/roster-management-modal.component.ts
@@ -115,6 +115,11 @@ export class RosterManagementModalComponent implements OnInit {
         if (data) {
           this.workerRosterStation = data;
         }
+      }, (error: unknown) => {
+        this.errorService.displayError(
+          'Something went wrong on our end and we\'re looking into it. Please try again in a little while.',
+          error
+        );
       });
   }
 }
