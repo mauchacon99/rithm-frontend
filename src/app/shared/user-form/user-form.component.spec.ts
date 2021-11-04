7e7bfb165089404a1f688b176a4782f43d4ab524
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-11-03 19:21:51 -0600

    test: update tests for password requirements

--- src/app/shared/user-form/user-form.component.spec.ts
+++ src/app/shared/user-form/user-form.component.spec.ts
@@ -132,25 +132,24 @@ describe('UserFormComponent', () => {
     expect(component.confirmPasswordLabel).toEqual('Confirm new password');
   });
 
-  it('should toggle password requirements based on focus', async () => {
+  it('should show password requirements based on focus', async () => {
     const passwordRequirementsSpy = spyOn(component, 'togglePasswordRequirements').and.callThrough();
     const passwordHarness = await loader.getHarness(MatInputHarness.with({ selector: '#password' }));
 
     await passwordHarness.focus();
     expect(passwordRequirementsSpy).toHaveBeenCalledOnceWith('password');
-
-    await passwordHarness.blur();
-    expect(passwordRequirementsSpy).toHaveBeenCalledOnceWith('password');
+    // await passwordHarness.blur();
+    // expect(passwordRequirementsSpy).toHaveBeenCalledOnceWith('password');
   });
 
-  it('should toggle confirm password requirements based on focus', async () => {
+  it('should show confirm password requirements based on focus', async () => {
     const passwordRequirementsSpy = spyOn(component, 'togglePasswordRequirements');
     const confirmPasswordHarness = await loader.getHarness(MatInputHarness.with({ selector: '#confirmPassword' }));
 
     await confirmPasswordHarness.focus();
     expect(passwordRequirementsSpy).toHaveBeenCalledOnceWith('confirmPassword');
-    await confirmPasswordHarness.blur();
-    expect(passwordRequirementsSpy).toHaveBeenCalledOnceWith('confirmPassword');
+    // await confirmPasswordHarness.blur();
+    // expect(passwordRequirementsSpy).toHaveBeenCalledOnceWith('confirmPassword');
   });
 
   it('should toggle password requirements', () => {
