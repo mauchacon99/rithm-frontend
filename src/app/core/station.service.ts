6bb00995c59cf5c25760dcdd375c68d9dcf55d09
Cesar Augusto Quijada Macuart <cesar.quijada@strutsoftwaresolutions.com>
2021-11-09 14:52:33 -0500

    rxjs element added

--- src/app/core/station.service.ts
+++ src/app/core/station.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError, of } from 'rxjs';
 import { delay, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import {
