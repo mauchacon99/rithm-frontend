9d3b427ebbc1b7035e006c24d388e331066e7ce3
catrival <andres.triana0429@gmail.com>
2021-11-15 11:42:59 -0500

    Update document-name-fields.ts

--- src/models/document-name-fields.ts
+++ src/models/document-name-fields.ts
@@ -3,7 +3,7 @@
  */
 export interface DocumentNameFields {
   /** The rithm ID for these document field names. */
-  rithmId: string;
+  rithmId?: string;
   /** The name/label of the document field name. */
   prompt: string;
 }
