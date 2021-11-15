7f48ce0ebcba83fe2969577a0e3f531c610bb0e3
catrival <andres.triana0429@gmail.com>
2021-11-15 10:29:53 -0500

    RTM-1420-create-an-interface-for-document-fields

---
+++ src/models/document-name-fields.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents all information about document name fields.
+ */
+export interface DocumentNameFields {
+  /** The global Rithm ID for these document name fields. */
+  rithmId: string;
+  /** The name/label of the document name fields. */
+  prompt: string;
+}
