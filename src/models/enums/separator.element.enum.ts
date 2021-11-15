cf396921c507424707fa6fd494c5f4cd7f6aeb22
catrival <andres.triana0429@gmail.com>
2021-11-15 13:02:26 -0500

    Create separator.element.enum.ts

---
+++ src/models/enums/separator.element.enum.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents all separator elements of select options.
+ */
+export enum SeparatorElement {
+
+  /** 'Slash' separator type. */
+  slash = '/',
+
+  /** 'Hyphen' separator type. */
+  hyphen = '-',
+
+  /** 'Plus' separator type. */
+  plusSign = '+',
+
+  /** Ampersand separator type. */
+  ampersand = '&'
+}
