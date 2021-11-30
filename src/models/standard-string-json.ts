ab8c7c02ae4be561c840e2888b3b061ea519c765
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2021-11-29 17:35:10 -0700

    chore: add model for standard string JSON format

---
+++ src/models/standard-string-json.ts
@@ -0,0 +1,8 @@
+/**
+ * Standard JSON format for requests/responses to/from the API where the only item concerned is a single string.
+ */
+export interface StandardStringJSON {
+
+  /** The actual string value. */
+  data: string;
+}
