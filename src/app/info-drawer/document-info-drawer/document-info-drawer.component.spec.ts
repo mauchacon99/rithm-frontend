888c65b98aefe08ada9e230b32032e99ec773271
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-12-02 19:25:47 -0400

    Update document-info-drawer.component.spec.ts

--- src/app/info-drawer/document-info-drawer/document-info-drawer.component.spec.ts
+++ src/app/info-drawer/document-info-drawer/document-info-drawer.component.spec.ts
@@ -76,9 +76,6 @@ describe('DocumentInfoDrawerComponent', () => {
   });
 
   it('should return boolean to method userTypeOwnerOrAdmin', () => {
-    const userType = { role: 'admin' };
-    localStorage.setItem('refreshTokenGuid', 'ee5655c8-5896-4ba8-9420-c14f28bf5b1f');
-    localStorage.setItem('user', JSON.stringify(userType));
     expect(component.isUserAnAdmin).toBe(false);
   });
 });
