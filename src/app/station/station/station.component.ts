105c3229a466010c450e77d452812f5a3e40b374
catrival <andres.triana0429@gmail.com>
2021-12-07 11:46:28 -0500

    RTM-1655-add-stationRithmId-in-request-for-station-questions

--- src/app/station/station/station.component.ts
+++ src/app/station/station/station.component.ts
@@ -235,7 +235,7 @@ export class StationComponent implements OnInit, OnDestroy, AfterContentChecked 
         this.stationForm.controls.generalInstructions.value),
 
       // Update Questions.
-      this.stationService.updateStationQuestions(this.stationInformation.questions)
+      this.stationService.updateStationQuestions(this.stationInformation.rithmId, this.stationInformation.questions)
     ];
 
     forkJoin(petitionsUpdateStation)
