9501f7b147d81c6022d9c2b1a8407f19a7e120e2
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-12-13 17:51:10 -0400

    RTM-1691 Create an interface for document auto-flow

---
+++ src/models/document-auto-flow.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface for document auto-flow.
+ */
+
+export interface DocumentAutoFlow {
+  /** Station specific id of the station for the document. */
+  stationRithmId: string;
+
+  /** Specific id for the document. */
+  documentRithmId: string;
+
+  /** Test mode for flow. */
+  testMode: boolean;
+}
