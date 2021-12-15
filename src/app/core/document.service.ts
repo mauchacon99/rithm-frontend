c85a3c1f6e3df2d2087793fa6e849617cf1cda0b
Andersson Arellano <andersson.arellano.d@gmail.com>
2021-12-15 14:54:51 -0500

    removed of in document.services

--- src/app/core/document.service.ts
+++ src/app/core/document.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
-import { BehaviorSubject, delay, map, Observable, of, throwError } from 'rxjs';
+import { BehaviorSubject, delay, map, Observable, throwError } from 'rxjs';
 // eslint-disable-next-line max-len
 import { StationDocuments, ForwardPreviousStationsDocument, DocumentStationInformation, StandardStringJSON, DocumentAnswer, DocumentName } from 'src/models';
 import { environment } from 'src/environments/environment';
