cbdab5d7e4e1da3169b1b311427638ae7c29921d
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-12-16 10:56:40 -0400

    RTM-1544 Disable flow button if required fields have not been filled out

--- src/app/document/document/document.component.spec.ts
+++ src/app/document/document/document.component.spec.ts
@@ -15,7 +15,7 @@ import { DetailDrawerComponent } from 'src/app/detail/detail-drawer/detail-drawe
 import { DashboardComponent } from 'src/app/dashboard/dashboard/dashboard.component';
 import { LoadingIndicatorComponent } from 'src/app/shared/loading-indicator/loading-indicator.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { PopupService } from 'src/app/core/popup.service';
 import { Router } from '@angular/router';
@@ -366,5 +366,29 @@ describe('DocumentComponent', () => {
     const spyQuestionAnswer = spyOn(TestBed.inject(DocumentService), 'saveDocumentAnswer').and.callThrough();
     component.saveDocumentAnswer(expectedAnswers);
     expect(spyQuestionAnswer).toHaveBeenCalledWith(component.documentInformation.documentRithmId, expectedAnswers);
+  });
+
+  it('should validate the form controls initial value', () => {
+    const form = component.documentForm.controls;
+    const expectFormFirst = ['documentTemplateForm'];
+
+    expect(Object.keys(form)).toEqual(expectFormFirst);
+    expect(form['documentTemplateForm'].value).toBe('');
+  });
+
+  it('should button flow disabled if form is not valid', () => {
+    component.documentLoading = false;
+    component.documentForm.get('documentTemplateForm')?.addValidators(Validators.required);
+    fixture.detectChanges();
+    const btnFlow = fixture.debugElement.nativeElement.querySelector('#document-flow');
+    expect(btnFlow.disabled).toBeTruthy();
+  });
+
+  it('should button flow enabled if form is valid', () => {
+    component.documentLoading = false;
+    component.documentForm.controls['documentTemplateForm'].setValue('Dev');
+    fixture.detectChanges();
+    const btnFlow = fixture.debugElement.nativeElement.querySelector('#document-flow');
+    expect(btnFlow.disabled).toBeFalsy();
   });
 });
