816a09a4f3a8b6979155d9db913abf8b7d672e3f
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2021-12-17 10:57:21 -0400

    RTM-1542 Connect to API to flow a document

--- src/app/core/document.service.spec.ts
+++ src/app/core/document.service.spec.ts
@@ -327,7 +327,13 @@ describe('DocumentService', () => {
 
     service.autoFlowDocument(expectedData)
       .subscribe((response) => {
-        expect(response).toBeFalsy();
+        expect(response).toEqual(expectedData);
       });
+
+    const req = httpTestingController.expectOne(`${environment.baseApiUrl}${MICROSERVICE_PATH}/auto-flow`);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(expectedData);
+    req.flush(expectedData);
+    httpTestingController.verify();
   });
 });
