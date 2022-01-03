57c4d3ccc699b89e70f8992c4f44a2beba0a6378
Luna Arturo <56488686+arturoluona@users.noreply.github.com>
2022-01-03 16:27:22 -0400

    Update src/app/station/rule-modal/rule-modal.component.spec.ts
    Co-authored-by: Adams Contreras <90850547+AdamsContreras999@users.noreply.github.com>

--- src/app/station/rule-modal/rule-modal.component.spec.ts
+++ src/app/station/rule-modal/rule-modal.component.spec.ts
@@ -1,7 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {MatDialogModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {RuleModalComponent} from './rule-modal.component';
-import {ConnectedStationInfo} from "../../../models";
+import {ConnectedStationInfo} from ".src/models";
 
 describe('RuleModalComponent', () => {
   let component: RuleModalComponent;
