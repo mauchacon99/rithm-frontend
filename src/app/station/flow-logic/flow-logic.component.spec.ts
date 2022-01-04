b524fc5e5e1f9b0568bfb5fcdbbe6e793e7084c7
arturoluna879 <arturoluna879@gmail.com>
2022-01-04 15:54:52 -0400

    fix test

--- src/app/station/flow-logic/flow-logic.component.spec.ts
+++ src/app/station/flow-logic/flow-logic.component.spec.ts
@@ -4,6 +4,7 @@ import { FlowLogicComponent } from './flow-logic.component';
 import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 import { ConnectedStationInfo } from "src/models";
 import { RuleModalComponent } from "../rule-modal/rule-modal.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FlowLogicComponent', () => {
   let component: FlowLogicComponent;
@@ -16,7 +17,8 @@ describe('FlowLogicComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        MatDialogModule
+        MatDialogModule,
+        BrowserAnimationsModule
       ],
       declarations: [FlowLogicComponent, RuleModalComponent]
     })
@@ -35,21 +37,23 @@ describe('FlowLogicComponent', () => {
   });
 
   describe('New rule modal', () => {
-
-    it('should to call function openModal', () => {
-      const openModal = spyOn(component, 'openModal');
-      const btnOpenModal = fixture.nativeElement.querySelector('#new-rule');
-      expect(btnOpenModal).toBeTruthy();
-      btnOpenModal.click();
-      expect(openModal).toHaveBeenCalled();
+    it('should to call MatDialog service', async () => {
+      const expectDataModal = {
+        panelClass: ['w-5/6', 'sm:w-4/5'],
+        maxWidth: '1024px',
+        data: { station: nextStations[0] }
+      };
+      const dialogSpy = spyOn(TestBed.inject(MatDialog), 'open').and.callThrough();
+      await component.openModal(nextStations[0]);
+      expect(dialogSpy).toHaveBeenCalledOnceWith(RuleModalComponent, expectDataModal);
     });
 
-    it('should to call dialog service', () => {
-      const dialogSpy = spyOn(TestBed.inject(MatDialog), 'open');
+    it('should to call method openModal after clicked in button', () => {
+      const spyFunc = spyOn(component, 'openModal').and.callThrough();
       const btnOpenModal = fixture.nativeElement.querySelector('#new-rule');
       expect(btnOpenModal).toBeTruthy();
       btnOpenModal.click();
-      expect(dialogSpy).toHaveBeenCalled();
+      expect(spyFunc).toHaveBeenCalledOnceWith(nextStations[0]);
     });
   });
 });
