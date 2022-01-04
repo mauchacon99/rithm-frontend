151d24c4ebcbf7a454ce36d73114e55821ecf175
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2022-01-04 11:49:06 -0700

    test: stub tests for station element behavior

--- src/helpers/station-map-element.spec.ts
+++ src/helpers/station-map-element.spec.ts
@@ -22,4 +22,33 @@ describe('StationMapElement', () => {
   it('should create an instance', () => {
     expect(station).toBeTruthy();
   });
+
+  xit('should be marked as deleted', () => {
+    // TODO: add test
+  });
+
+  xit('should throw an error when marking it as deleted when it is newly created', () => {
+    // TODO: add test
+  });
+
+  xit('should be marked as updated', () => {
+    // TODO: add test
+  });
+
+  xit('should not be marked as updated when it has been deleted', () => {
+    // TODO: add test
+  });
+
+  xit('should not be marked as updated when it has been newly created', () => {
+    // TODO: add test
+  });
+
+  xit('should report that a station is identical to itself', () => {
+    // TODO: add test
+  });
+
+  xit('should report that a station is different from itself', () => {
+    // TODO: add test
+  });
+
 });
