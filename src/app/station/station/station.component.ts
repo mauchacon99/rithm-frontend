510cbfa55c150c871179cca3792a489a8f032b88
Tyler Hendrickson <6372535+HendricksonTyler@users.noreply.github.com>
2022-01-05 12:15:37 -0700

    fix: add missing closing bracket cause by conflict?

--- src/app/station/station/station.component.ts
+++ src/app/station/station/station.component.ts
@@ -116,16 +116,25 @@ export class StationComponent
     });
 
     this.stationService.stationQuestion$
-    .pipe(takeUntil(this.destroyed$))
-    .subscribe((question) => {
-      const prevQuestion = this.stationInformation.questions.find(field => field.rithmId === question.rithmId);
-      if (prevQuestion){
-        const questionIndex = this.stationInformation.questions.indexOf(prevQuestion);
-        if (!question.isPossibleAnswer){
-          this.stationInformation.questions[questionIndex].prompt = question.prompt;
-        } else {
-          if (prevQuestion.possibleAnswers){
-            this.populatePossibleAnswers(question, questionIndex, prevQuestion.possibleAnswers);
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((question) => {
+        const prevQuestion = this.stationInformation.questions.find(
+          (field) => field.rithmId === question.rithmId
+        );
+        if (prevQuestion) {
+          const questionIndex =
+            this.stationInformation.questions.indexOf(prevQuestion);
+          if (!question.isPossibleAnswer) {
+            this.stationInformation.questions[questionIndex].prompt =
+              question.prompt;
+          } else {
+            if (prevQuestion.possibleAnswers) {
+              this.populatePossibleAnswers(
+                question,
+                questionIndex,
+                prevQuestion.possibleAnswers
+              );
+            }
           }
         }
       });
@@ -138,20 +147,34 @@ export class StationComponent
    * @param questionIndex The index of the current question in the station.
    * @param arrayAnswers The array of possible answers in the current question.
    */
-  private populatePossibleAnswers(answer: Question, questionIndex: number, arrayAnswers: PossibleAnswer[] = []): void {
+  private populatePossibleAnswers(
+    answer: Question,
+    questionIndex: number,
+    arrayAnswers: PossibleAnswer[] = []
+  ): void {
     const newAnswer: PossibleAnswer = {
-      rithmId: answer.originalStationRithmId ? answer.originalStationRithmId : '',
+      rithmId: answer.originalStationRithmId
+        ? answer.originalStationRithmId
+        : '',
       text: answer.prompt,
       default: false,
     };
-    if (this.stationInformation.questions[questionIndex].possibleAnswers !== undefined){
-      const possibleAnswer = arrayAnswers.find( a => a.rithmId === newAnswer.rithmId);
+    if (
+      this.stationInformation.questions[questionIndex].possibleAnswers !==
+      undefined
+    ) {
+      const possibleAnswer = arrayAnswers.find(
+        (a) => a.rithmId === newAnswer.rithmId
+      );
       if (possibleAnswer) {
         const answerIndex = arrayAnswers.indexOf(possibleAnswer);
         arrayAnswers[answerIndex] = newAnswer;
-        this.stationInformation.questions[questionIndex].possibleAnswers = arrayAnswers;
+        this.stationInformation.questions[questionIndex].possibleAnswers =
+          arrayAnswers;
       } else {
-        this.stationInformation.questions[questionIndex].possibleAnswers?.push(newAnswer);
+        this.stationInformation.questions[questionIndex].possibleAnswers?.push(
+          newAnswer
+        );
       }
     }
   }
@@ -281,14 +304,19 @@ export class StationComponent
       isReadOnly: false,
       isRequired: fieldType === QuestionFieldType.Instructions ? true : false,
       isPrivate: false,
-      children: fieldType === QuestionFieldType.AddressLine ? this.addAddressChildren() : [],
-      originalStationRithmId: this.stationRithmId
+      children:
+        fieldType === QuestionFieldType.AddressLine
+          ? this.addAddressChildren()
+          : [],
+      originalStationRithmId: this.stationRithmId,
     };
-    if (fieldType === QuestionFieldType.CheckList ||
+    if (
+      fieldType === QuestionFieldType.CheckList ||
       fieldType === QuestionFieldType.Select ||
-      fieldType === QuestionFieldType.MultiSelect){
-        newQuestion.possibleAnswers = [];
-      }
+      fieldType === QuestionFieldType.MultiSelect
+    ) {
+      newQuestion.possibleAnswers = [];
+    }
     this.stationInformation.questions.push(newQuestion);
     this.stationService.touchStationForm();
   }
