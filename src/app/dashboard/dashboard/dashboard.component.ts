c78610b36fca94e14f1aedee5dcb281231a2bf2d
Austin Bagley <aust.bagley@gmail.com>
2022-01-05 14:04:20 -0700

    initialize split via service call

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
+import { SplitService } from 'src/app/core/split.service';
 import { StationService } from 'src/app/core/station.service';
 import { UserService } from 'src/app/core/user.service';
 import { Station } from 'src/models';
@@ -12,14 +13,15 @@ import { Station } from 'src/models';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   // TODO: remove when admin users can access stations through map
   /** The list of all stations for an admin to view. */
   stations: Station[] = [];
 
   constructor(
     private stationService: StationService,
-    private userService: UserService
+    private userService: UserService,
+    private splitService: SplitService
   ) {
     // TODO: remove when admin users can access stations through map
     if (this.isAdmin) {
@@ -29,6 +31,16 @@ export class DashboardComponent {
         .subscribe((stations) => {
           this.stations = stations;
         });
+    }
+  }
+
+  /**
+   * Initialize split on page load.
+   */
+  ngOnInit(): void {
+    const user = this.userService.user;
+    if (user) {
+      this.splitService.initSdk(user.rithmId);
     }
   }
 
