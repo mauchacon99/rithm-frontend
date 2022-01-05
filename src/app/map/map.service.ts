1f15c6e018106a290fe0a9177db02b906bf8558a
Harrison King <authorharrisonking@gmail.com>
2022-01-05 16:39:30 -0700

    wrapping 2

--- src/app/map/map.service.ts
+++ src/app/map/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import {
   MapMode,
