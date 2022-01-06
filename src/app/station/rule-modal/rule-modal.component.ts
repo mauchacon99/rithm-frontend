21be808bf7652b9139f02a670f1d155851457458
masanmarting <mauro.gonzalez@rithmxo.com>
2022-01-06 08:22:06 -0700

    Rule Modal close function

--- src/app/station/rule-modal/rule-modal.component.ts
+++ src/app/station/rule-modal/rule-modal.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 /**
  * Reusable component for displaying the information to add a new rule.
@@ -14,15 +13,17 @@ export class RuleModalComponent {
   /** Station Rithm id. */
   stationRithmId = '';
 
-  constructor(public dialogRef: MatDialogRef<RuleModalComponent>,@Inject(MAT_DIALOG_DATA) public rithmId: string) {
+  constructor(
+    public dialogRef: MatDialogRef<RuleModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public rithmId: string
+  ) {
     this.stationRithmId = rithmId;
   }
 
-/**
- * Close rule Modal.
- */
-   onNoClick(): void {
+  /**
+   * Close rule Modal.
+   */
+  onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
