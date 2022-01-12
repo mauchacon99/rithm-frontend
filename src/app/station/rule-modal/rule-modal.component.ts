a9b7a93205d8f2f93b7c8ccf0f18deadb22bf9b9
Andersson Arellano <andersson.arellano.d@gmail.com>
2022-01-12 16:31:51 -0400

    RTM-1883 RTM-1884 UI display a message if there aren't added rules in case ALL are true and UI display a message if there aren't added rules in case ANY are true

--- src/app/station/rule-modal/rule-modal.component.ts
+++ src/app/station/rule-modal/rule-modal.component.ts
@@ -1,12 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { StepperOrientation } from '@angular/material/stepper';
 import { Observable } from 'rxjs';
-import { first, map } from 'rxjs/operators';
-import { StationService } from 'src/app/core/station.service';
-import { FlowLogicRule } from 'src/models';
-import { ErrorService } from 'src/app/core/error.service';
+import { map } from 'rxjs/operators';
 /**
  * Reusable component for displaying the information to add a new rule.
  */
@@ -15,15 +12,12 @@ import { ErrorService } from 'src/app/core/error.service';
   templateUrl: './rule-modal.component.html',
   styleUrls: ['./rule-modal.component.scss'],
 })
-export class RuleModalComponent implements OnInit {
+export class RuleModalComponent {
   /** Station Rithm id. */
   stationRithmId = '';
 
   /** Orientation for stepper. */
   stepperOrientation$: Observable<StepperOrientation>;
-
-  /** The station Flow Logic Rule. */
-  stationFlowLogic!: FlowLogicRule;
 
   /** The value of the first operand. */
   firstOperand = '';
@@ -31,9 +25,7 @@ export class RuleModalComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<RuleModalComponent>,
     @Inject(MAT_DIALOG_DATA) public rithmId: string,
-    breakpointObserver: BreakpointObserver,
-    private stationService: StationService,
-    private errorService: ErrorService
+    breakpointObserver: BreakpointObserver
   ) {
     this.stationRithmId = rithmId;
     this.stepperOrientation$ = breakpointObserver
@@ -42,36 +34,9 @@ export class RuleModalComponent implements OnInit {
   }
 
   /**
-   * Life cycle init the component.
-   */
-  ngOnInit(): void {
-    this.getStationFlowLogicRule();
-  }
-
-  /**
    * Close rule Modal.
    */
   closeModal(): void {
     this.dialogRef.close();
-  }
-
-  /**
-   * Get each station flow rules.
-   */
-  private getStationFlowLogicRule(): void {
-    this.stationService
-      .getStationFlowLogicRule(this.stationRithmId)
-      .pipe(first())
-      .subscribe({
-        next: (stationFlowLogic) => {
-          this.stationFlowLogic = stationFlowLogic;
-        },
-        error: (error: unknown) => {
-          this.errorService.displayError(
-            "Something went wrong on our end and we're looking into it. Please try again in a little while.",
-            error
-          );
-        },
-      });
   }
 }
--- src/mocks/mock-station-service.ts
+++ src/mocks/mock-station-service.ts
@@ -828,22 +828,24 @@ export class MockStationService {
       const stationFlowLogic: FlowLogicRule = {
         stationRithmId: '3813442c-82c6-4035-893a-86fa9deca7c3',
         destinationStationRithmId: '73d47261-1932-4fcf-82bd-159eb1a7243f',
-        flowRule: {
-          ruleType: RuleType.Or,
-          equations: [
-            {
-              leftOperand: {
-                type: OperandType.Field,
-                value: 'birthday',
+        flowRules: [
+          {
+            ruleType: RuleType.Or,
+            equations: [
+              {
+                leftOperand: {
+                  type: OperandType.Field,
+                  value: 'birthday',
+                },
+                operatorType: OperatorType.Before,
+                rightOperand: {
+                  type: OperandType.Date,
+                  value: '5/27/1982',
+                },
               },
-              operatorType: OperatorType.Before,
-              rightOperand: {
-                type: OperandType.Date,
-                value: '5/27/1982',
-              },
-            },
-          ],
-        },
+            ],
+          },
+        ],
       };
       return of(stationFlowLogic).pipe(delay(1000));
     }
