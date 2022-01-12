08e419d839dff3f31302fd3a619feb1af46bbeff
DarwinMMC <darwin.cerpa1@gmail.com>
2022-01-12 10:48:01 -0500

    comment widgeType

--- src/models/enums/widget-type.enum.ts
+++ src/models/enums/widget-type.enum.ts
@@ -2,9 +2,10 @@
  * Represents the types widgets.
  */
 export enum WidgetType {
+  /* Widget type flow*/
   Flow = 'flow',
-
+  /* Widget type station*/
   Station = 'station',
-
+  /* Widget type document*/
   Document = 'document',
 }
