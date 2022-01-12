cc6e632c77fbcf17cfa11833ea7adcc3d3e09254
ManojK <manoj.kumar@rithm.software>
2022-01-12 19:28:34 +0530

    RTM-1882 Add a form field that allows you to change a group's title to the drawer
    Add a form field that allows you to change a group's title to the drawer

--- src/app/map/station-group-info-drawer/station-group-info-drawer.component.ts
+++ src/app/map/station-group-info-drawer/station-group-info-drawer.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { SidenavDrawerService } from 'src/app/core/sidenav-drawer.service';
 import { MapService } from '../map.service';
 import { PopupService } from 'src/app/core/popup.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MapMode } from 'src/models';
 
 /**
  * Component for station group info drawer.
@@ -16,11 +18,44 @@ export class StationGroupInfoDrawerComponent implements OnDestroy {
   /** Subject for when the component is destroyed. */
   private destroyed$ = new Subject<void>();
 
+  /** Form field viewed in Build mode. */
+  editMode = false;
+
+  /** Group name form. */
+  groupNameForm: FormGroup;
+
+  /** The current mode of the map. */
+  currentMode = MapMode.View;
+
+  /**
+   * Whether the map is in any building mode.
+   *
+   * @returns True if the map is in any building mode, false otherwise.
+   */
+   get isBuilding(): boolean {
+    return (
+      this.currentMode === MapMode.Build
+    );
+   }
+
   constructor(
     private sidenavDrawerService: SidenavDrawerService,
     private mapService: MapService,
+    private fb: FormBuilder,
     private popupService: PopupService
-  ) {}
+  ) {
+    this.groupNameForm = this.fb.group({
+      name: [''],
+    });
+    this.mapService.mapMode$.pipe(takeUntil(this.destroyed$)).subscribe({
+      next: (mapMode) => {
+        this.currentMode = mapMode;
+      },
+      error: (error: unknown) => {
+        throw new Error(`Map overlay subscription error: ${error}`);
+      },
+    });
+  }
 
   /**
    * Toggles the open state of the drawer for station info.
