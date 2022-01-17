93e3a003fe0f546a04ecad4dea81b88bb6851c86
catrival <andres.triana0429@gmail.com>
2022-01-17 09:00:38 -0500

    RTM-1876-display-a-loading-indicator-while-the-request-for-getting-station-questions

--- src/app/station/rule-modal/rule-modal.component.spec.ts
+++ src/app/station/rule-modal/rule-modal.component.spec.ts
@@ -9,11 +9,10 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { of, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { StationService } from 'src/app/core/station.service';
 import { MockErrorService, MockStationService } from 'src/mocks';
 import { ErrorService } from 'src/app/core/error.service';
-import { Question, QuestionFieldType } from 'src/models';
 import { LoadingIndicatorComponent } from 'src/app/shared/loading-indicator/loading-indicator.component';
 import { MockComponent } from 'ng-mocks';
 
@@ -99,23 +98,11 @@ describe('RuleModalComponent', () => {
   it('should show loading-indicator-questions while get current and previous questions', () => {
     component.questionStationLoading = false;
     component.stationRithmId = stationId;
-    const questions: Question[] = [
-      {
-        prompt: 'Fake question 1',
-        rithmId: '3j4k-3h2j-hj4j',
-        questionType: QuestionFieldType.Number,
-        isReadOnly: false,
-        isRequired: true,
-        isPrivate: false,
-        children: [],
-        value: '1',
-      },
-    ];
     component.getStationQuestions();
     spyOn(
       TestBed.inject(StationService),
       'getStationQuestions'
-    ).and.returnValue(of(questions));
+    ).and.callThrough();
     fixture.detectChanges();
     expect(component.questionStationLoading).toBeTrue();
 
