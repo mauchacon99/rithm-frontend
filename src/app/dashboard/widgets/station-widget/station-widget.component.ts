c05614f56f47e14d9319b7fb5325f2ab363859b7
Darwin Mercado <56990125+DarwinMMC@users.noreply.github.com>
2022-01-17 10:42:06 -0500

    Update src/app/dashboard/widgets/station-widget/station-widget.component.ts
    Co-authored-by: Adams Contreras <90850547+AdamsContreras999@users.noreply.github.com>

--- src/app/dashboard/widgets/station-widget/station-widget.component.ts
+++ src/app/dashboard/widgets/station-widget/station-widget.component.ts
@@ -20,7 +20,7 @@ export class StationWidgetComponent implements OnInit {
   dataStationWidget!: StationWidgetData;
 
   /** Show error loading widget. */
-  failedLoadDocument = false;
+  failedLoadWidget = false;
 
   constructor(
     private documentService: DocumentService,
