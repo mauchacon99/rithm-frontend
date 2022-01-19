297f96f4632af39c224a0cef7046f9cc65d04184
AdamsContreras999 <adams.contreras@strutsoftwaresolutions.com>
2022-01-19 11:19:12 -0400

    RTM-1908 Add button that closes in menu

--- src/app/dashboard/dashboard-menu/header-menu/header-menu.component.spec.ts
+++ src/app/dashboard/dashboard-menu/header-menu/header-menu.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SidenavDrawerService } from 'src/app/core/sidenav-drawer.service';
 
 import { HeaderMenuComponent } from './header-menu.component';
 
@@ -9,6 +10,9 @@ describe('HeaderMenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderMenuComponent],
+      providers: [
+        { provide: SidenavDrawerService, useClass: SidenavDrawerService },
+      ],
     }).compileComponents();
   });
 
@@ -20,5 +24,21 @@ describe('HeaderMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call to toggle sidenavService and hidden menu dashboard', () => {
+    const spyMenu = spyOn(TestBed.inject(SidenavDrawerService), 'toggleDrawer');
+    component.toggleMenu('menuDashboard');
+    expect(spyMenu).toHaveBeenCalledOnceWith('menuDashboard');
+  });
+
+  it('should call method toggleMenu if clicked button close', () => {
+    const spyMethod = spyOn(component, 'toggleMenu');
+    const buttonClose = fixture.debugElement.nativeElement.querySelector(
+      '#close-menu-dashboard'
+    );
+    expect(buttonClose).toBeTruthy();
+    buttonClose.click();
+    expect(spyMethod).toHaveBeenCalledOnceWith('menuDashboard');
   });
 });
