bf417472e71ec4192a55c035ae7a27689526ae73
arturoluna879 <arturoluna879@gmail.com>
2022-01-19 13:07:05 -0500

    RTM-1909-display-logo-and-organization-in-menu

--- src/app/dashboard/dashboard-menu/header-menu/header-menu.component.ts
+++ src/app/dashboard/dashboard-menu/header-menu/header-menu.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/user.service';
+import { OrganizationService } from 'src/app/core/organization.service';
+import { OrganizationInfo } from 'src/models';
+import { ErrorService } from 'src/app/core/error.service';
+import { first } from 'rxjs/operators';
 
 /**
  * Header for dashboard menu drawer.
@@ -8,4 +13,40 @@ import { Component } from '@angular/core';
   templateUrl: './header-menu.component.html',
   styleUrls: ['./header-menu.component.scss'],
 })
-export class HeaderMenuComponent {}
+export class HeaderMenuComponent implements OnInit {
+  /** Information about organization. */
+  organizationInfo!: OrganizationInfo;
+
+  constructor(
+    private userService: UserService,
+    private errorService: ErrorService,
+    private organizationService: OrganizationService
+  ) {}
+
+  /** Get signed in user and information about organization. */
+  ngOnInit(): void {
+    this.getOrganizationInfo(this.userService.user.organization);
+  }
+
+  /**
+   * Get information about organization.
+   *
+   * @param organizationId String of user organization.
+   */
+  getOrganizationInfo(organizationId: string): void {
+    this.organizationService
+      .getOrganizationInfo(organizationId)
+      .pipe(first())
+      .subscribe({
+        next: (organization) => {
+          this.organizationInfo = organization;
+        },
+        error: (error: unknown) => {
+          this.errorService.displayError(
+            "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+            error
+          );
+        },
+      });
+  }
+}
