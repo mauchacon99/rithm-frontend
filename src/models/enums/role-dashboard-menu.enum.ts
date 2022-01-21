55c69c3e0bf17e7dbfb48262db77bd9bd5b5775c
Darwin Mercado <56990125+DarwinMMC@users.noreply.github.com>
2022-01-21 14:31:18 -0500

    Update src/models/enums/role-dashboard-menu.enum.ts
    Co-authored-by: Austin Bagley <aust.bagley@gmail.com>

--- src/models/enums/role-dashboard-menu.enum.ts
+++ src/models/enums/role-dashboard-menu.enum.ts
@@ -1,13 +1,7 @@
 export enum RoleDashboardMenu {
-  /** Dashboard default. */
-  DashboardDefault,
-
   /** Dashboard for organization. */
   DashboardOrganization,
 
   /** Dashboard personal. */
-  DashboardPersonal,
-
-  /** Dashboard for admin. */
-  DashboardAdmin,
+  DashboardPersonal
 }
