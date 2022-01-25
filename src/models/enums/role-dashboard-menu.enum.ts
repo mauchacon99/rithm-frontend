5777edcd7bc4717784453bba8e3816ec11377955
Adams Contreras <90850547+AdamsContreras999@users.noreply.github.com>
2022-01-25 19:35:31 -0400

    Update src/models/enums/role-dashboard-menu.enum.ts

--- src/models/enums/role-dashboard-menu.enum.ts
+++ src/models/enums/role-dashboard-menu.enum.ts
@@ -3,5 +3,5 @@ export enum RoleDashboardMenu {
   DashboardOrganization = 'DashboardOrganization',
 
   /** Dashboard personal. */
-  DashboardPersonal = 'DashboardPersonal',
+  DashboardPersonal = 'dashboardPersonal',
 }
