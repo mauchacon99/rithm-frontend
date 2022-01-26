d366e0446161fad7a7a16b0549475e4db0d9858e
AvocadoCasualty <baibeebambi@gmail.com>
2022-01-26 16:05:16 -0700

    Fixed failing test

--- src/app/dashboard/dashboard-menu/options-menu/options-menu.component.spec.ts
+++ src/app/dashboard/dashboard-menu/options-menu/options-menu.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OptionsMenuComponent } from './options-menu.component';
 import { MatMenuModule } from '@angular/material/menu';
+import { DashboardService } from 'src/app/dashboard/dashboard.service';
+import { MockDashboardService, MockPopupService } from 'src/mocks';
+import { PopupService } from 'src/app/core/popup.service';
 
 describe('OptionsMenuComponent', () => {
   let component: OptionsMenuComponent;
@@ -11,6 +13,10 @@ describe('OptionsMenuComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [OptionsMenuComponent],
       imports: [MatMenuModule],
+      providers: [
+        { provide: DashboardService, useClass: MockDashboardService },
+        { provide: PopupService, useClass: MockPopupService },
+      ],
     }).compileComponents();
   });
 
