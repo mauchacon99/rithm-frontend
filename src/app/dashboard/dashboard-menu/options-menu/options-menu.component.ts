f8146a5ddc0e5657ca6cfdab8bb827feba6ff450
DarwinMMC <darwin.cerpa1@gmail.com>
2022-01-28 10:41:47 -0500

    Format

--- src/app/dashboard/dashboard-menu/options-menu/options-menu.component.ts
+++ src/app/dashboard/dashboard-menu/options-menu/options-menu.component.ts
@@ -20,7 +20,7 @@ export class OptionsMenuComponent {
   roleDashboardMenu = RoleDashboardMenu;
 
   /** Show option. */
-  @Input() optionType!:OptionDashboardMenu;
+  @Input() optionType!: OptionDashboardMenu;
 
   /** Validate type of option.*/
   optionDashboardMenu = OptionDashboardMenu;
