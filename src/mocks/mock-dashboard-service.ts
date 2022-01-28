fd360ac12fb3547fd911623e259d6951b3212591
masanmarting <96437511+masanmarting@users.noreply.github.com>
2022-01-28 06:58:47 -0700

    Update src/mocks/mock-dashboard-service.ts
    Co-authored-by: Adams Contreras <90850547+AdamsContreras999@users.noreply.github.com>

--- src/mocks/mock-dashboard-service.ts
+++ src/mocks/mock-dashboard-service.ts
@@ -234,7 +234,25 @@ export class MockDashboardService {
   updatePersonalDashboard(
     dashboardUpdate: DashboardData
   ): Observable<DashboardData> {
-    return of(dashboardUpdate).pipe(delay(1000));
+        const dashboardData: DashboardData = {
+      rithmId: '123-131-132',
+      name: 'New Dashboard',
+      widgets: [
+        {
+          cols: 4,
+          rows: 1,
+          x: 0,
+          y: 0,
+          widgetType: WidgetType.Station,
+          data: '{"stationRithmId":"247cf568-27a4-4968-9338-046ccfee24f3"}',
+          minItemCols: 4,
+          minItemRows: 4,
+          maxItemCols: 12,
+          maxItemRows: 12,
+        },
+      ],
+    };
+    return of(dashboardData).pipe(delay(1000));
   }
 
   /**
