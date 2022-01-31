c5e8e5b62c139a68d62eaee59d35ee4faaf8caf1
Andersson Arellano <andersson.arellano.d@gmail.com>
2022-01-31 01:19:58 -0400

    RTM-1382 render group name along flow boundary path

--- src/app/map/station-group-element.service.ts
+++ src/app/map/station-group-element.service.ts
@@ -9,7 +9,6 @@ import {
   CONNECTION_LINE_WIDTH,
   BUTTON_DEFAULT_COLOR,
   DEFAULT_SCALE,
-  STATION_GROUP_NAME_PADDING,
   FONT_SIZE_MODIFIER,
   NODE_HOVER_COLOR,
   CONNECTION_LINE_WIDTH_SELECTED,
@@ -20,6 +19,10 @@ import {
   TOOLTIP_HEIGHT,
   TOOLTIP_WIDTH,
   TOOLTIP_PADDING,
+  STATION_GROUP_NAME_TRANSLATE,
+  GROUP_NAME_HEIGHT,
+  STATION_GROUP_NAME_MAX_ANGLE_ROTATE,
+  SLOPE_RANGE_NOT_ALLOWED,
 } from './map-constants';
 import { MapService } from './map.service';
 
@@ -266,6 +269,8 @@ export class StationGroupElementService {
         'Cannot draw station group name if context is not defined'
       );
     }
+    // Save canvas, it is used to avoid having to rotate the context again.
+    this.canvasContext.save();
     // The name of the station group.
     // Change color when hovered over.
     this.canvasContext.fillStyle =
@@ -283,14 +288,50 @@ export class StationGroupElementService {
         : BUTTON_DEFAULT_COLOR;
     const fontSize = Math.ceil(FONT_SIZE_MODIFIER * this.mapScale);
     this.canvasContext.font = `bold ${fontSize}px Montserrat`;
-    //Place so that it covers the line.
-    this.canvasContext.fillText(
-      stationGroup.title,
-      stationGroup.boundaryPoints[0].x +
-        STATION_GROUP_NAME_PADDING * this.mapScale,
-      stationGroup.boundaryPoints[stationGroup.boundaryPoints.length - 1].y +
-        STATION_GROUP_NAME_PADDING * this.mapScale
+
+    // Calculates the position of the first straightest line.
+    const newPosition = this.positionStraightestLine(
+      stationGroup.boundaryPoints
     );
+
+    // Calculation of the angle of rotation of station group name.
+    const rotateAngleStationGroupName = Math.atan(
+      this.slopeLine(
+        stationGroup.boundaryPoints[newPosition],
+        stationGroup.boundaryPoints[newPosition - 1]
+      )
+    );
+
+    // Moves the point on the line.
+    const newPoint = this.movePointOnLine(
+      stationGroup.boundaryPoints[newPosition],
+      stationGroup.boundaryPoints[newPosition - 1],
+      STATION_GROUP_NAME_TRANSLATE * this.mapScale
+    );
+
+    // translate the canvas to the new point.
+    this.canvasContext.translate(newPoint.x, newPoint.y);
+
+    // Rotate station group name.
+    this.canvasContext.rotate(rotateAngleStationGroupName);
+
+    // Delete the line under the station group name.
+    this.canvasContext.clearRect(
+      -5,
+      -5,
+      this.canvasContext.measureText(stationGroup.title).width +
+        STATION_GROUP_PADDING,
+      GROUP_NAME_HEIGHT * this.mapScale
+    );
+
+    // Paint the station group name.
+    this.canvasContext.fillText(stationGroup.title, 5, 0);
+
+    // Reset translate and rotate.
+    this.canvasContext.rotate(-rotateAngleStationGroupName);
+    this.canvasContext.translate(-newPoint.x, -newPoint.y);
+    // Restore Canvas.
+    this.canvasContext.restore();
   }
 
   /**
@@ -459,7 +500,6 @@ export class StationGroupElementService {
     // graphics convention. This doesn't affect the correctness of the result.
 
     // TODO: Refactor this; some duplicated code and vague variable names
-
     const upperHull: Point[] = [];
     for (let i = 0; i < points.length; i++) {
       const p = points[i];
@@ -523,5 +563,85 @@ export class StationGroupElementService {
     } else {
       return 0;
     }
+  }
+
+  /**
+   * Calculate the slope between two points.
+   *
+   * @param pointStart The start point of the line.
+   * @param pointEnd The end point of the line.
+   * @returns The slope of a line.
+   */
+  slopeLine(pointStart: Point, pointEnd: Point): number {
+    // If the denominator approximates 0 then return pi to deal with the error.
+    if (
+      pointEnd.x - pointStart.x > -SLOPE_RANGE_NOT_ALLOWED &&
+      pointEnd.x - pointStart.x < SLOPE_RANGE_NOT_ALLOWED
+    )
+      return Math.PI;
+
+    return (pointEnd.y - pointStart.y) / (pointEnd.x - pointStart.x);
+  }
+
+  /**
+   * Move a point on the line.
+   *
+   * @param pointStart The start point of the line.
+   * @param pointEnd The end point of the line.
+   * @param displacement The Amount of displacement.
+   * @param coordinate The coordinate to move, if true the coordinate is X else the coordinate is Y.
+   * @returns The new Point on line.
+   */
+  movePointOnLine(
+    pointStart: Point,
+    pointEnd: Point,
+    displacement: number,
+    coordinate = true
+  ): Point {
+    const newPoint = pointStart;
+
+    // Calculate the slope between two points.
+    const m = this.slopeLine(pointStart, pointEnd);
+    // If the slope is 0.
+    if (m === Math.PI) return newPoint;
+
+    // If true the coordinate is X else the coordinate is Y
+    if (coordinate) {
+      // x-coordinate displacement.
+      newPoint.x += displacement;
+      // The point-slope equation evaluating X.
+      newPoint.y = m * (newPoint.x - pointEnd.x) + pointEnd.y;
+    } else {
+      // Y-coordinate displacement.
+      newPoint.y += displacement;
+      // The point-slope equation evaluating Y.
+      newPoint.x = (newPoint.y - pointEnd.y) / m + pointEnd.x;
+    }
+    return newPoint;
+  }
+
+  /**
+   * Position of the points that make the first straight line.
+   *
+   * @param points A Array the points.
+   * @returns The position of the points that make the first straight line.
+   */
+  positionStraightestLine(points: Point[]): number {
+    let newPosition = points.length - 1;
+
+    for (let i = points.length - 1; i > 1; i--) {
+      // Calculate the slope between two points.
+      const m = this.slopeLine(points[i], points[i - 1]);
+      // If the slope is equal a Pi than continue with next point.
+      if (m === Math.PI) continue;
+      // Calculation of the angle of rotation.
+      const rotateAngleStationGroupName = Math.atan(m);
+      // If the angle of rotation is greater than the maximum angle of rotation.
+      if (rotateAngleStationGroupName > STATION_GROUP_NAME_MAX_ANGLE_ROTATE) {
+        newPosition = i;
+        break;
+      }
+    }
+    return newPosition;
   }
 }
