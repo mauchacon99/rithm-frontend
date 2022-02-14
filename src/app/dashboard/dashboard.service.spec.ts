f70381e74e258d2942deed5d7568fd56763082fe
DarwinMMC <darwin.cerpa1@gmail.com>
2022-02-14 10:05:22 -0500

    Resolve conflict

--- src/app/dashboard/dashboard.service.spec.ts
+++ src/app/dashboard/dashboard.service.spec.ts
@@ -550,28 +550,6 @@ describe('DashboardService', () => {
     service.toggleLoadingDashboard(false);
   });
 
-  it('should call method getDocumentWidget', () => {
-    const expectedResponse = {
-      documentName: 'Untitled Dashboard',
-      documentRithmId: '8263330A-BCAA-40DB-8C06-D4C111D5C9DA',
-      questions: [
-        {
-          rithmId: '8263330A-BCAA-40DB-8C06-D4C111D5C9D2',
-          prompt: '',
-          questionType: QuestionFieldType.ShortText,
-          isReadOnly: true,
-          isRequired: true,
-          isPrivate: false,
-          children: [],
-        },
-      ],
-    };
-
-    service.getDocumentWidget().subscribe((response) => {
-      expect(response).toEqual(expectedResponse);
-    });
-  });
-
   it('should delete a organization dashboard', () => {
     const rithmId = 'E204F369-386F-4E41-B3CA-2459E674DF52';
     service.deleteOrganizationDashboard(rithmId).subscribe((response) => {
