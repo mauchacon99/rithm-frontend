3c600fa627196c6101403604beeb54ac0d5a44de
arturoluna879 <arturoluna879@gmail.com>
2022-02-18 13:26:13 -0400

    RTM-2217-RTM-2228-loading-ui-show-select-in-station-widget-drawer

--- src/app/dashboard/drawer-widget/station-widget-drawer/station-widget-drawer.component.ts
+++ src/app/dashboard/drawer-widget/station-widget-drawer/station-widget-drawer.component.ts
@@ -2,7 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SidenavDrawerService } from 'src/app/core/sidenav-drawer.service';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { StationColumnWidget, WidgetDrawerStationData } from 'src/models';
+import { ColumnsDocumentInfo } from 'src/models/enums/columns-document-info';
 
 /**
  * Component for Station widget drawer.
@@ -13,6 +15,11 @@ import { StationColumnWidget, WidgetDrawerStationData } from 'src/models';
   styleUrls: ['./station-widget-drawer.component.scss'],
 })
 export class StationWidgetDrawerComponent implements OnInit, OnDestroy {
+  /** Form. */
+  formColumns: FormGroup = new FormGroup({
+    columns: new FormArray([]),
+  });
+
   /** Station RithmId. */
   stationRithmId!: string;
 
@@ -24,6 +31,12 @@ export class StationWidgetDrawerComponent implements OnInit, OnDestroy {
 
   /** Subject for when the component is destroyed. */
   private destroyed$ = new Subject<void>();
+
+  /** Enum of columns statics. */
+  columnsDocumentInfo = ColumnsDocumentInfo;
+
+  /** Static columns. */
+  staticColumnsInfo: StationColumnWidget[] = [];
 
   constructor(private sidenavDrawerService: SidenavDrawerService) {}
 
@@ -49,6 +62,39 @@ export class StationWidgetDrawerComponent implements OnInit, OnDestroy {
           this.widgetIndex = dataDrawer.widgetIndex;
         }
       });
+    this.setColumnsInfo();
+  }
+
+  /** Set enum values of ColumnsDocumentInfo to staticColumnsInfo. */
+  setColumnsInfo(): void {
+    Object.keys(this.columnsDocumentInfo).map((column) => {
+      this.staticColumnsInfo.push({ name: column });
+    });
+  }
+
+  /**
+   * Get array form.
+   *
+   * @returns FormArray to ngFor.
+   */
+  get getFormColumns(): FormArray {
+    return this.formColumns.get('columns') as FormArray;
+  }
+
+  /**
+   * Add new column.
+   *
+   * @param name Name of the column.
+   * @param questionId Id of question of the column.
+   */
+  addNewColumn(name = '', questionId = ''): void {
+    const refColumns = this.formColumns.get('columns') as FormArray;
+    refColumns.push(
+      new FormGroup({
+        name: new FormControl(name),
+        questionId: new FormControl(questionId),
+      })
+    );
   }
 
   /**
---
+++ src/models/enums/columns-document-info.ts
@@ -0,0 +1,15 @@
+/**
+ * Represent columns of the list documents in station widget.
+ */
+export enum ColumnsDocumentInfo {
+  /** Document name. */
+  DocumentName = 'documentName',
+  /** Priority. */
+  Priority = 'priority',
+  /** Time in station. */
+  TimeInStation = 'timeInStation',
+  /** Last updated. */
+  LastUpdated = 'lastUpdated',
+  /** Assigned user. */
+  AssignedUser = 'assignedUser',
+}
