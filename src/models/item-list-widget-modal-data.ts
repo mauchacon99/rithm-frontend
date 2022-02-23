3568d185574a9c6ab60d4369d651e531ec59e0c5
masanmarting <mauro.gonzalez@rithmxo.com>
2022-02-23 11:23:26 -0700

    format

--- src/models/item-list-widget-modal-data.ts
+++ src/models/item-list-widget-modal-data.ts
@@ -1,7 +1,7 @@
 import { itemTypeWidgetModal } from './enums/item-type-widget-modal';
 
 /**
- * Represents the items of the documents, stations, groups tab.
+ * Represents the items of the documents, stations, groups tab in modal to add widgets.
  */
 export interface ItemListWidgetModal {
   /** Id of the element according to the list type. */
