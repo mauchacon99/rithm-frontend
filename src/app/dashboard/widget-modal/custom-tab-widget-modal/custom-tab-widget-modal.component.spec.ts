5b2130cc9566fa0349bfdb791d3440e30094df8f
DarwinMMC <darwin.cerpa1@gmail.com>
2022-03-01 12:34:01 -0500

    Applied suggestions arturo

--- src/app/dashboard/widget-modal/custom-tab-widget-modal/custom-tab-widget-modal.component.spec.ts
+++ src/app/dashboard/widget-modal/custom-tab-widget-modal/custom-tab-widget-modal.component.spec.ts
@@ -24,4 +24,14 @@ describe('CustomTabWidgetModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should selected index tabs', () => {
+    const indexTab = 1;
+    const spyTabs = spyOn(component,'selectedTab').and.callThrough();
+    const btnTab = fixture.nativeElement.querySelector('#tab-button-station');
+    expect(btnTab).toBeTruthy();
+    btnTab.click();
+    expect(spyTabs).toHaveBeenCalledOnceWith(indexTab);
+    expect(component.indexTab).toBe(indexTab);
+  });
 });
