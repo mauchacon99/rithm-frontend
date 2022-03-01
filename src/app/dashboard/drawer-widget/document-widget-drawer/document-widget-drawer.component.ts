27bdf9bc48053134701e68a153349a6be7755adb
arturoluna879 <arturoluna879@gmail.com>
2022-03-01 19:51:10 -0400

    RTM-2350-RTM-2351-ui-show-multiple-selection-in-drawer-for-document-widget-and-show-loading-indicator

--- src/app/dashboard/drawer-widget/document-widget-drawer/document-widget-drawer.component.ts
+++ src/app/dashboard/drawer-widget/document-widget-drawer/document-widget-drawer.component.ts
@@ -5,9 +5,20 @@ import {
   OnInit,
   Output,
 } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
+import { first } from 'rxjs/operators';
 import { SidenavDrawerService } from 'src/app/core/sidenav-drawer.service';
-import { DashboardItem, EditDataWidget } from 'src/models';
+import {
+  ColumnFieldsWidget,
+  DashboardItem,
+  EditDataWidget,
+  QuestionList,
+} from 'src/models';
+import { DocumentService } from 'src/app/core/document.service';
+import { ErrorService } from 'src/app/core/error.service';
+import { DashboardService } from 'src/app/dashboard/dashboard.service';
+
 /**
  * Component for Station widget drawer.
  */
@@ -23,8 +34,17 @@ export class DocumentWidgetDrawerComponent implements OnInit, OnDestroy {
   /** Subject for when the component is destroyed. */
   private destroyed$ = new Subject<void>();
 
+  /** Form to multiselect columns to document. */
+  formColumns = new FormControl();
+
   /** Widget item of opened widget-drawer. */
   widgetItem!: DashboardItem;
+  
+  /** Questions the document. */
+  questions!: QuestionList[];
+
+  /** Document RithmId. */
+  documentRithmId!: string;
 
   /** Widget index of opened widget-drawer. */
   widgetIndex!: number;
@@ -32,7 +52,23 @@ export class DocumentWidgetDrawerComponent implements OnInit, OnDestroy {
   /** Element list in drawer. */
   quantityElementsWidget = 0;
 
-  constructor(private sidenavDrawerService: SidenavDrawerService) {}
+  /** Loading drawer. */
+  isLoading = false;
+
+  /** Columns list to display in select. */
+  documentFields: ColumnFieldsWidget[] = [];
+
+
+  /** Document columns. */
+  documentColumns: ColumnFieldsWidget[] = [];
+
+  constructor(
+    private sidenavDrawerService: SidenavDrawerService,
+    private documentService: DocumentService,
+    private errorService: ErrorService,
+    private dashboardService: DashboardService
+  ) {
+  }
 
   /**
    * Initial Method.
@@ -51,9 +87,66 @@ export class DocumentWidgetDrawerComponent implements OnInit, OnDestroy {
           this.widgetItem = dataDrawer.widgetItem;
           this.widgetIndex = dataDrawer.widgetIndex;
           this.quantityElementsWidget = dataDrawer.quantityElementsWidget;
+          const dataWidget = JSON.parse(this.widgetItem.data);
+          this.documentColumns = dataWidget.columns || [];
+          this.documentRithmId = dataWidget.documentRithmId;
           this.setWidgetIndex.emit(this.widgetIndex);
+          this.getDocumentWidget();
         }
       });
+  }
+
+  /** Get document widget. */
+  private getDocumentWidget(): void {
+    this.isLoading = true;
+    this.documentService
+      .getDocumentWidget(this.documentRithmId)
+      .pipe(first())
+      .subscribe({
+        next: (documentWidget) => {
+          this.isLoading = false;
+          this.questions = documentWidget.questions;
+          this.loadColumnsSelect();
+        },
+        error: (error: unknown) => {
+          this.isLoading = false;
+          this.errorService.displayError(
+            "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+            error
+          );
+        },
+      });
+  }
+
+  /** Load list of selects. */
+  private loadColumnsSelect(): void {
+    this.formColumns.reset();
+    this.documentFields = [];
+    this.questions.map((questionList) => {
+      questionList.questions.map((question) => {
+        this.documentFields.push({
+          name: question.prompt,
+          questionId: question.rithmId,
+        });
+      });
+    });
+    this.formColumns.setValue(
+      !this.documentColumns.length ? this.documentFields : this.documentColumns
+    );
+  }
+
+  /** Update widget. */
+  updateWidget(): void {
+    this.documentColumns = this.formColumns.value;
+    this.widgetItem.data = JSON.stringify({
+      stationRithmId: this.documentRithmId,
+      columns: this.documentColumns,
+    });
+    this.dashboardService.updateDashboardWidgets({
+      widgetItem: this.widgetItem,
+      widgetIndex: this.widgetIndex,
+      quantityElementsWidget: this.quantityElementsWidget,
+    });
   }
 
   /**
