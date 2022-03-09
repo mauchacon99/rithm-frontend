fe8ea88d9f099a329cee7af28a01b4b342a6c472
DarwinMMC <darwin.cerpa1@gmail.com>
2022-03-09 11:24:27 -0500

    RTM-2452 show load indicator in tab group

--- src/app/dashboard/widget-modal/custom-tab-widget-modal/custom-tab-widget-modal.component.ts
+++ src/app/dashboard/widget-modal/custom-tab-widget-modal/custom-tab-widget-modal.component.ts
@@ -33,6 +33,9 @@ export class CustomTabWidgetModalComponent implements OnInit {
   /** Whether the getting tab document list is loading. */
   isLoadingDocumentTab = false;
 
+  /** Whether the getting tab group list is loading. */
+  isLoadingGroupTab = false;
+
   /** Error loading petition station tab. */
   errorLoadingStationTab = false;
 
@@ -123,6 +126,7 @@ export class CustomTabWidgetModalComponent implements OnInit {
    * Get the list for the groups the stations tabs.
    */
   private getGroupStationTabList(): void {
+    this.isLoadingGroupTab = true;
     this.dashboardService
       .getGroupStationTabList()
       .pipe(first())
@@ -131,8 +135,10 @@ export class CustomTabWidgetModalComponent implements OnInit {
           this.dataSourceTableGroup = new MatTableDataSource(
             itemsListGroupsStation
           );
+          this.isLoadingGroupTab = false;
         },
         error: (error: unknown) => {
+          this.isLoadingGroupTab = false;
           this.errorService.displayError(
             "Something went wrong on our end and we're looking into it. Please try again in a little while.",
             error
