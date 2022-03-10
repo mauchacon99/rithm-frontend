4da2c789662cb92cb0e0ea26146381efec560177
JoharaP <jpabon@travelsupport.info>
2022-03-10 13:56:43 -0400

    RTM-2471-add-build-categories

--- src/app/station/build-drawer/build-drawer.component.spec.ts
+++ src/app/station/build-drawer/build-drawer.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatListModule,
+  MatListOption,
+  MatSelectionList,
+} from '@angular/material/list';
+import { By } from '@angular/platform-browser';
 import { BuildDrawerComponent } from './build-drawer.component';
 
 describe('BuildDrawerComponent', () => {
@@ -8,6 +13,7 @@ describe('BuildDrawerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatListModule],
       declarations: [BuildDrawerComponent],
     }).compileComponents();
   });
@@ -20,5 +26,37 @@ describe('BuildDrawerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create mat-selection-list and mat-list-option and a possible selection to an item', async () => {
+    const categoryItem = fixture.debugElement.queryAll(
+      By.directive(MatListOption)
+    );
+    const categoriesList = fixture.debugElement.query(
+      By.directive(MatSelectionList)
+    );
+
+    expect(categoriesList).toBeTruthy();
+    expect(categoryItem.length).toEqual(4);
+
+    const categorySelected =
+      categoryItem[2].injector.get<MatListOption>(MatListOption);
+    const categoriesListSelected =
+      categoriesList.injector.get<MatSelectionList>(
+        MatSelectionList
+      ).selectedOptions;
+
+    expect(categoriesListSelected.selected.length).toBe(0);
+    expect(categoryItem[2].nativeElement.getAttribute('aria-selected')).toBe(
+      'false'
+    );
+
+    categorySelected.toggle();
+    fixture.detectChanges();
+
+    expect(categoryItem[2].nativeElement.getAttribute('aria-selected')).toBe(
+      'true'
+    );
+    expect(categoriesListSelected.selected.length).toBe(1);
   });
 });
