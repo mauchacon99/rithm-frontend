57c2b74ce8197f597dcdf409c204ffb2695ed104
DarwinMMC <darwin.cerpa1@gmail.com>
2022-03-14 17:14:43 -0500

    applied suggestions

--- src/app/dashboard/widget-modal/description-widget-modal/description-widget-modal.component.ts
+++ src/app/dashboard/widget-modal/description-widget-modal/description-widget-modal.component.ts
@@ -3,11 +3,11 @@ import { SelectedItemWidgetModel } from 'src/models';
 
 /** Description widget modal. */
 @Component({
-  selector: 'app-description-widget-modal[itemWidget]',
+  selector: 'app-description-widget-modal[itemWidgetModalSelected]',
   templateUrl: './description-widget-modal.component.html',
   styleUrls: ['./description-widget-modal.component.scss'],
 })
 export class DescriptionWidgetModalComponent {
   /** Widget item selected. */
-  @Input() itemWidget!: SelectedItemWidgetModel;
+  @Input() itemWidgetModalSelected!: SelectedItemWidgetModel;
 }
