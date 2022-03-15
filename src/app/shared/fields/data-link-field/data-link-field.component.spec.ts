59f757a6c56748ee990e39fc4b91e7f87e763382
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2022-03-15 18:27:44 -0400

    fix format

--- src/app/shared/fields/data-link-field/data-link-field.component.spec.ts
+++ src/app/shared/fields/data-link-field/data-link-field.component.spec.ts
@@ -15,7 +15,6 @@ import { LoadingIndicatorComponent } from 'src/app/shared/loading-indicator/load
 import { DataLinkFieldComponent } from './data-link-field.component';
 import { MatDividerModule } from '@angular/material/divider';
 
-
 describe('DataLinkFieldComponent', () => {
   let component: DataLinkFieldComponent;
   let fixture: ComponentFixture<DataLinkFieldComponent>;
