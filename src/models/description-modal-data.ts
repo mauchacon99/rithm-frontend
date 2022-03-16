e35394b037b4d1f92f997b9cd871e62d57978e0f
DarwinMMC <darwin.cerpa1@gmail.com>
2022-03-16 16:34:06 -0500

    Create interface

---
+++ src/models/description-modal-data.ts
@@ -0,0 +1,11 @@
+/** Interface for show description in document modal. */
+export interface DescriptionModalData {
+  /** Type of widget data. */
+  type: string;
+
+  /** Customizable data. */
+  customizable: string;
+
+  /** Description widget. */
+  description: string;
+}
