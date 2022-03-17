9765f2aaf78829c79612e7601aa9d830b0c47b22
catrival <andres.triana0429@gmail.com>
2022-03-17 10:29:36 -0500

    RTM-2431-create-an-upload-file-modal-component-with-basic-ui

---
+++ src/app/shared/fields/upload-file-modal/upload-file-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * Reusable component to upload a file from the document.
+ */
+@Component({
+  selector: 'app-upload-file-modal',
+  templateUrl: './upload-file-modal.component.html',
+  styleUrls: ['./upload-file-modal.component.scss'],
+})
+export class UploadFileModalComponent {
+  constructor() {
+    /** */
+  }
+}
