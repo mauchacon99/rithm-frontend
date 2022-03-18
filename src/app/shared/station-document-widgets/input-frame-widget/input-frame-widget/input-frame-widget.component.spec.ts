b9d038a2638f1b79f4aeb0ceb5ef675c9a4a37f4
JoharaP <jpabon@travelsupport.info>
2022-03-18 14:20:04 -0400

    add unit test

--- src/app/shared/station-document-widgets/input-frame-widget/input-frame-widget/input-frame-widget.component.spec.ts
+++ src/app/shared/station-document-widgets/input-frame-widget/input-frame-widget/input-frame-widget.component.spec.ts
@@ -2,7 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputFrameWidgetComponent } from './input-frame-widget.component';
 
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, DragDropModule, copyArrayItem } from '@angular/cdk/drag-drop';
+import { Question, QuestionFieldType } from 'src/models';
+import { of } from 'rxjs';
 
 describe('InputFrameWidgetComponent', () => {
   let component: InputFrameWidgetComponent;
@@ -23,5 +25,39 @@ describe('InputFrameWidgetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('TestDragDropElements', () => {
+    it('should call the method addElementDrag', async () => {
+      const secondList: Question[] = [];
+      const firstList: Question[] = [
+        {
+          prompt: 'Fake question 1',
+          rithmId: '3j4k-3h2j-hj4j',
+          questionType: QuestionFieldType.Number,
+          isReadOnly: false,
+          isRequired: true,
+          isPrivate: false,
+          children: [],
+        },
+      ];
+
+      const cdkEvent = {
+        container: { data: firstList, id: 'form-inputs' },
+        previousContainer: { data: secondList, id: 'input-widget'},
+      } as CdkDragDrop<Question[]>;
+
+     const spyDrag = spyOn(component, 'addElementDrag').and.callFake(() => of(cdkEvent));
+      component.addElementDrag(cdkEvent);
+      fixture.detectChanges();
+
+      expect(cdkEvent.container).not.toEqual(cdkEvent.previousContainer);
+      expect(cdkEvent.container.data).toEqual(firstList);
+      expect(cdkEvent.previousContainer.data).toEqual(secondList);
+
+      copyArrayItem(firstList,secondList,0,0);
+      expect(secondList).toEqual(firstList);
+      expect(spyDrag).toHaveBeenCalledOnceWith(cdkEvent);
+    });
   });
 });
