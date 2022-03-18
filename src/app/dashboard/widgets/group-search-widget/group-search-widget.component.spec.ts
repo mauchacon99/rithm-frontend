e4bec8bd5b8850cca41e85a76e91e67c8729af18
arturoluna879 <arturoluna879@gmail.com>
2022-03-18 15:57:07 -0400

    RTM-2629-ui-show-list-documents-in-group-search-widget-component

--- src/app/dashboard/widgets/group-search-widget/group-search-widget.component.spec.ts
+++ src/app/dashboard/widgets/group-search-widget/group-search-widget.component.spec.ts
@@ -5,6 +5,7 @@ import { GroupSearchWidgetComponent } from './group-search-widget.component';
 import { StationService } from 'src/app/core/station.service';
 import { ErrorService } from 'src/app/core/error.service';
 import { MockErrorService, MockStationService } from 'src/mocks';
+import { StationGroupWidgetData } from 'src/models';
 
 describe('GroupSearchWidgetComponent', () => {
   let component: GroupSearchWidgetComponent;
@@ -12,6 +13,24 @@ describe('GroupSearchWidgetComponent', () => {
   const dataWidget =
     // eslint-disable-next-line max-len
     '{"stationGroupRithmId":"4fb462ec-0772-49dc-8cfb-3849d70ad168"}';
+
+  const dataStationGroupWidget: StationGroupWidgetData = {
+    rithmId: '6375027-78345-73824-54244',
+    title: 'Station Group',
+    SubStationGroups: [],
+    stations: [
+      {
+        rithmId: '3237520-7837-78378-78378',
+        name: 'StationName',
+        workers: [],
+        StationOwners: [],
+      },
+    ],
+    admins: [],
+    users: [],
+    IsChained: true,
+    IsImplicitRootFlow: true,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,6 +46,7 @@ describe('GroupSearchWidgetComponent', () => {
     fixture = TestBed.createComponent(GroupSearchWidgetComponent);
     component = fixture.componentInstance;
     component.dataWidget = dataWidget;
+    component.dataStationGroupWidget = dataStationGroupWidget;
     fixture.detectChanges();
   });
 
@@ -59,5 +79,29 @@ describe('GroupSearchWidgetComponent', () => {
     ).and.callThrough();
     component.ngOnInit();
     expect(spyService).toHaveBeenCalled();
+  });
+
+  it('should show list of stations groups', () => {
+    const listStations = fixture.debugElement.nativeElement.querySelector(
+      '#list-stations-groups'
+    );
+    const stationGroupsEmpty = fixture.debugElement.nativeElement.querySelector(
+      '#station-group-empty'
+    );
+    expect(listStations).toBeTruthy();
+    expect(stationGroupsEmpty).toBeNull();
+  });
+
+  it('should show message list of stations groups empty', () => {
+    component.dataStationGroupWidget.stations = [];
+    fixture.detectChanges();
+    const listStations = fixture.debugElement.nativeElement.querySelector(
+      '#list-stations-groups'
+    );
+    const stationGroupsEmpty = fixture.debugElement.nativeElement.querySelector(
+      '#station-group-empty'
+    );
+    expect(listStations).toBeNull();
+    expect(stationGroupsEmpty).toBeTruthy();
   });
 });
