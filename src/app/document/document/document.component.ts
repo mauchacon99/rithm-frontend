661238c048e4b460c8097f2e45b96df1d48400f8
mauchacon99 <53098149+mauchacon99@users.noreply.github.com>
2022-03-21 17:21:57 -0400

    Update src/app/document/document/document.component.ts
    Co-authored-by: Austin Bagley <aust.bagley@gmail.com>

--- src/app/document/document/document.component.ts
+++ src/app/document/document/document.component.ts
@@ -515,7 +515,7 @@ export class DocumentComponent implements OnInit, OnDestroy, AfterViewChecked {
       .subscribe({
         next: (data) => {
           this.documentLoading = false;
-          this.popupService.notify('The container has flowed successfully');
+          this.popupService.notify('The container has saved and flowed successfully');
 
           if (
             !this.isWidget &&
