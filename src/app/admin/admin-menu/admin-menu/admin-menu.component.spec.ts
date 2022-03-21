b63909470836605c25e8fc5a0a278172768ab4ad
arturoluna879 <arturoluna879@gmail.com>
2022-03-21 18:15:35 -0400

    RTM-2669-ui-show-header-the-admin-menu-component

--- src/app/admin/admin-menu/admin-menu/admin-menu.component.spec.ts
+++ src/app/admin/admin-menu/admin-menu/admin-menu.component.spec.ts
@@ -1,4 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+import { throwError } from 'rxjs';
+import { ErrorService } from 'src/app/core/error.service';
+import { OrganizationService } from 'src/app/core/organization.service';
+import { UserService } from 'src/app/core/user.service';
+import { LoadingIndicatorComponent } from 'src/app/shared/loading-indicator/loading-indicator.component';
+import {
+  MockUserService,
+  MockErrorService,
+  MockOrganizationService,
+} from 'src/mocks';
 
 import { AdminMenuComponent } from './admin-menu.component';
 
@@ -8,7 +19,15 @@ describe('AdminMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AdminMenuComponent],
+      declarations: [
+        AdminMenuComponent,
+        MockComponent(LoadingIndicatorComponent),
+      ],
+      providers: [
+        { provide: UserService, useClass: MockUserService },
+        { provide: ErrorService, useClass: MockErrorService },
+        { provide: OrganizationService, useClass: MockOrganizationService },
+      ],
     }).compileComponents();
   });
 
@@ -20,5 +39,73 @@ describe('AdminMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be to get information about organization', () => {
+    const expectOrganizationId = TestBed.inject(UserService).user.organization;
+    const expectSpyService = spyOn(
+      TestBed.inject(OrganizationService),
+      'getOrganizationInfo'
+    ).and.callThrough();
+
+    component.ngOnInit();
+    expect(expectSpyService).toHaveBeenCalledOnceWith(expectOrganizationId);
+  });
+
+  it('should show error message when request information about organization', () => {
+    spyOn(
+      TestBed.inject(OrganizationService),
+      'getOrganizationInfo'
+    ).and.returnValue(
+      throwError(() => {
+        throw new Error();
+      })
+    );
+
+    const expectSpyService = spyOn(
+      TestBed.inject(ErrorService),
+      'displayError'
+    ).and.callThrough();
+
+    component.ngOnInit();
+    expect(expectSpyService).toHaveBeenCalled();
+  });
+
+  it('should show name organization', () => {
+    component.isLoading = false;
+    fixture.detectChanges();
+    const organizationInfo =
+      fixture.debugElement.nativeElement.querySelector('#info-organization');
+    expect(organizationInfo).toBeTruthy();
+
+    const showError = fixture.debugElement.nativeElement.querySelector(
+      '#failed-info-organization'
+    );
+    expect(showError).toBeNull();
+  });
+
+  it('should show error message when fail request get name', () => {
+    component.failedGetOrganization = true;
+    fixture.detectChanges();
+
+    const organizationInfo =
+      fixture.debugElement.nativeElement.querySelector('#info-organization');
+    expect(organizationInfo).toBeNull();
+
+    const showError = fixture.debugElement.nativeElement.querySelector(
+      '#failed-info-organization'
+    );
+    expect(showError).toBeTruthy();
+  });
+
+  it('should show loading organization info while data is loading', () => {
+    const data = '123-123';
+    component['getOrganizationInfo'](data);
+    fixture.detectChanges();
+    expect(component.isLoading).toBeTrue();
+    const loadingComponent = fixture.debugElement.nativeElement.querySelector(
+      '#organization-name-loading'
+    );
+    expect(loadingComponent).toBeTruthy();
   });
 });
--- src/app/admin/admin-menu/admin-menu/admin-menu.component.ts
+++ src/app/admin/admin-menu/admin-menu/admin-menu.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { ErrorService } from 'src/app/core/error.service';
+import { OrganizationService } from 'src/app/core/organization.service';
+import { UserService } from 'src/app/core/user.service';
+import { OrganizationInfo } from 'src/models';
 /**
  * Admin menu component.
  */
@@ -7,4 +12,51 @@ import { Component } from '@angular/core';
   templateUrl: './admin-menu.component.html',
   styleUrls: ['./admin-menu.component.scss'],
 })
-export class AdminMenuComponent {}
+export class AdminMenuComponent implements OnInit {
+  /** Information about organization. */
+  organizationInfo!: OrganizationInfo;
+
+  /** Show error if fail get organization info.*/
+  failedGetOrganization = false;
+
+  /** Organization information loading inline. */
+  isLoading = true;
+
+  constructor(
+    private userService: UserService,
+    private errorService: ErrorService,
+    private organizationService: OrganizationService
+  ) {}
+
+  /** Get signed in user and information about organization. */
+  ngOnInit(): void {
+    this.getOrganizationInfo(this.userService.user.organization);
+  }
+
+  /**
+   * Get information about organization.
+   *
+   * @param organizationId String of user organization.
+   */
+  private getOrganizationInfo(organizationId: string): void {
+    this.isLoading = true;
+    this.organizationService
+      .getOrganizationInfo(organizationId)
+      .pipe(first())
+      .subscribe({
+        next: (organization) => {
+          this.isLoading = false;
+          this.failedGetOrganization = false;
+          this.organizationInfo = organization;
+        },
+        error: (error: unknown) => {
+          this.isLoading = false;
+          this.failedGetOrganization = true;
+          this.errorService.displayError(
+            "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+            error
+          );
+        },
+      });
+  }
+}
