eac8d84d4916d76b0c26ccfa420b2e925d71ec41
maalexdev <mfigueroa9406@gmail.com>
2022-03-22 11:15:03 -0600

    Apply suggestions

--- src/app/admin/admin-menu/admin-menu/admin-menu.component.spec.ts
+++ src/app/admin/admin-menu/admin-menu/admin-menu.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatListModule } from '@angular/material/list';
+import { ListAdminOptionMenuType } from 'src/models';
 import { AdminMenuComponent } from './admin-menu.component';
 
 describe('AdminMenuComponent', () => {
@@ -21,5 +22,12 @@ describe('AdminMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit itemMenuSelected', () => {
+    const itemToEmit = ListAdminOptionMenuType.GroupHierarchy;
+    const spyEmit = spyOn(component.itemMenuSelected, 'emit').and.callThrough();
+    component.getItemSelected(itemToEmit);
+    expect(spyEmit).toHaveBeenCalledOnceWith(itemToEmit);
   });
 });
