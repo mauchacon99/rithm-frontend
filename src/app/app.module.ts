121223ab6f1057bee188855df2ba1def70661267
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2022-03-23 19:22:21 -0400

    RTM-2676-create-a-title-widget-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import { SettingsModule } from './settings/settings.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { AdminModule } from './admin/admin.module';
+import { TitleWidgetComponent } from './shared/station-document-widgets/title-widget/title-widget.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TitleWidgetComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
