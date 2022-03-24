36e92ce8894bd7eb6ab5c0ef43ad574d3955f46f
arturoluona <arturoluna879@gmail.com>
2022-03-24 15:38:33 -0400

    RTM-2673-ui-show-data-in-list-in-group-list-hierarchy-component

--- src/app/admin/action-admin-menu/group-hierarchy/group-list-hierarchy/group-list-hierarchy.component.ts
+++ src/app/admin/action-admin-menu/group-hierarchy/group-list-hierarchy/group-list-hierarchy.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { ErrorService } from 'src/app/core/error.service';
+import { StationService } from 'src/app/core/station.service';
+import { StationGroupData } from 'src/models';
 
 /**
  * Component to show options list.
@@ -8,4 +12,36 @@ import { Component } from '@angular/core';
   templateUrl: './group-list-hierarchy.component.html',
   styleUrls: ['./group-list-hierarchy.component.scss'],
 })
-export class GroupListHierarchyComponent {}
+export class GroupListHierarchyComponent implements OnInit {
+  stationGroups!: StationGroupData;
+
+  constructor(
+    private stationService: StationService,
+    private errorService: ErrorService
+  ) {}
+
+  /**
+   * Initial Method.
+   */
+  ngOnInit(): void {
+    this.getStationGroups();
+  }
+
+  /** Get stationGroups. */
+  private getStationGroups(): void {
+    this.stationService
+      .getStationGroups('')
+      .pipe(first())
+      .subscribe({
+        next: (stationGroup) => {
+          this.stationGroups = stationGroup;
+        },
+        error: (error: unknown) => {
+          this.errorService.displayError(
+            "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+            error
+          );
+        },
+      });
+  }
+}
