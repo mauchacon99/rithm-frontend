7b1f37db8dbf5605d98ec05a733cd04950e12db6
arturoluona <arturoluna879@gmail.com>
2022-03-24 09:09:41 -0400

    refactor more

--- src/app/dashboard/widget-modal/description-widget-modal/description-widget-modal.component.ts
+++ src/app/dashboard/widget-modal/description-widget-modal/description-widget-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { DashboardItem, SelectedItemWidgetModel, WidgetType } from 'src/models';
 import { DashboardService } from 'src/app/dashboard/dashboard.service';
-import { AddWidgetModalComponent } from '../add-widget-modal/add-widget-modal.component';
+import { AddWidgetModalComponent } from 'src/app/dashboard/widget-modal/add-widget-modal/add-widget-modal.component';
 
 /** Description widget modal. */
 @Component({
@@ -52,57 +52,6 @@ export class DescriptionWidgetModalComponent implements OnInit {
 
   /** Widget type without default. */
   widgetTypeWithoutDefault!: WidgetType;
-
-  /** Data Description for description-widget-modal.  */
-  dataDescriptionTemplate = {
-    // station data
-    [WidgetType.Station]: {
-      title: 'Table Widget',
-      type: 'Station Template',
-      customizable: 'Table Columns',
-      description: `By default. the table has a single column showing each document in your selected station.
-      Custom banner images can also be added to the widget`,
-    },
-    // StationTableBanner data
-    [WidgetType.StationTableBanner]: {
-      title: 'Table Widget',
-      type: 'Station Template',
-      customizable: 'Table Columns & Image',
-      description: `By default. the table has a single column showing each document in your selected station.
-      Additional columns of data can be added as desired. Custom banner images can also be added to the widget`,
-    },
-    // groups data
-    [WidgetType.StationGroup]: {
-      title: '',
-      type: '',
-      customizable: '',
-      description: ``,
-    },
-    // document data
-    [WidgetType.Document]: {
-      title: 'List Widget',
-      type: 'Document Template',
-      customizable: 'List Values',
-      description: `By default, the list widget displays all of the values associated with a document.
-      Values can be optionally be hidden from on the widget in needed.`,
-    },
-    // DocumentListBanner data
-    [WidgetType.DocumentListBanner]: {
-      title: 'List Widget',
-      type: 'Document Template',
-      customizable: 'List Values & Image',
-      description: `Upload an image from the container to display as a banner image.
-      List values can also be optionally hidden from on the widget as needed.`,
-    },
-    // StationGroupSearch data
-    [WidgetType.StationGroupSearch]: {
-      title: 'Search Widget',
-      type: 'Group Template',
-      customizable: 'Search result values',
-      description: `User can query all the values saved on documents within a flow. By default the search results display the document
-      name and the field that matches your search.`,
-    },
-  };
 
   /** Data static for each template by widgetType. */
   dataTemplate;
