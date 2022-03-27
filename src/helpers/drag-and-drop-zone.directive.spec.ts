f484580df9cac257ed63d5488906bb7af65c7c11
Cesar Augusto Quijada Macuart <cesar.quijada@strutsoftwaresolutions.com>
2022-03-27 15:20:39 -0500

    RTM-2681 directive to drag and drop

---
+++ src/helpers/drag-and-drop-zone.directive.spec.ts
@@ -0,0 +1,9 @@
+// import { DragAndDropZoneDirective } from './drag-and-drop-zone.directive';
+// import { TestBed } from '@angular/core/testing';
+
+describe('DragAndDropZoneDirective', () => {
+  xit('should create an instance', () => {
+    // const directive = new DragAndDropZoneDirective();
+    // expect(directive).toBeTruthy();
+  });
+});
