dc02e6f200061d1b84bf578b3b5bff37452c3283
Cesar Augusto Quijada Macuart <cesar.quijada@strutsoftwaresolutions.com>
2022-03-28 17:33:08 -0500

    Created directives directory

--- src/helpers/drag-and-drop-zone.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-// import { DragAndDropZoneDirective } from './drag-and-drop-zone.directive';
-// import { TestBed } from '@angular/core/testing';
-
-describe('DragAndDropZoneDirective', () => {
-  xit('should create an instance', () => {
-    // const directive = new DragAndDropZoneDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
