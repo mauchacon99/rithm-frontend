8cb2c9038d981cf05d2b8fd1ff34bbbf8bd6a188
DarwinMMC <darwin.cerpa1@gmail.com>
2022-03-28 13:31:43 -0500

    Changes

--- src/app/admin/action-admin-menu/group-hierarchy/user-group-station-admin/user-group-station-admin.component.ts
+++ src/app/admin/action-admin-menu/group-hierarchy/user-group-station-admin/user-group-station-admin.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StationGroupData, StationListGroup } from 'src/models';
 /** Component group station admin. */
 @Component({
-  selector: 'app-user-group-station-admin',
+  selector: 'app-user-group-station-admin[selectedItem][typeSelectedItem]',
   templateUrl: './user-group-station-admin.component.html',
   styleUrls: ['./user-group-station-admin.component.scss'],
 })
-export class UserGroupStationAdminComponent {}
+export class UserGroupStationAdminComponent {
+  /** Value of selected item. */
+  @Input() selectedItem!: StationGroupData | StationListGroup;
+
+  /** Type of selected item. */
+  @Input() typeSelectedItem!: 'group' | 'station';
+}
