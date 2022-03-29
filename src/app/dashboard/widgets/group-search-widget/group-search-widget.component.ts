dae6c44b61a055406688412cd688fa46abee6ab6
maalexdev <mfigueroa9406@gmail.com>
2022-03-29 13:35:45 -0600

    Apply suggestions

--- src/app/dashboard/widgets/group-search-widget/group-search-widget.component.ts
+++ src/app/dashboard/widgets/group-search-widget/group-search-widget.component.ts
@@ -122,4 +122,14 @@ export class GroupSearchWidgetComponent implements OnInit {
       });
     }
   }
+
+  /**
+   * Navigate the user to the station on the map.
+   */
+   goToStationGroupOnMap(): void {
+    this.mapService.centerStationRithmId$.next(this.stationRithmId);
+    this.mapService.viewStationButtonClick$.next(true);
+    this.router.navigate([`/map`]);
+  }
+
 }
