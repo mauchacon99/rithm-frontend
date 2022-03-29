ecdebafed447180503ebface2f9c3c1231cb22d3
ManojK <manoj.kumar@rithm.software>
2022-03-29 22:40:02 +0530

    RTM-2654 UI - Add an input setting section to update the current questions properties
    UI - Add an input setting section to update the current questions properties

--- src/app/shared/setting-drawer/setting-drawer.component.spec.ts
+++ src/app/shared/setting-drawer/setting-drawer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingDrawerComponent } from './setting-drawer.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 describe('SettingDrawerComponent', () => {
   let component: SettingDrawerComponent;
@@ -9,6 +10,7 @@ describe('SettingDrawerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SettingDrawerComponent],
+      imports: [MatSlideToggleModule],
     }).compileComponents();
   });
 
--- src/app/shared/setting-drawer/setting-drawer.module.ts
+++ src/app/shared/setting-drawer/setting-drawer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingDrawerComponent } from 'src/app/shared/setting-drawer/setting-drawer.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [SettingDrawerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule],
   exports: [SettingDrawerComponent],
 })
 export class SettingDrawerModule {}
