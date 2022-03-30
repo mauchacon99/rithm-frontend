4e461215a3cc37c9572622075f2596b094dbb76e
catrival <andres.triana0429@gmail.com>
2022-03-30 13:00:23 -0500

    RTM-2650-add-a-title-to-the-settingdrawer

--- src/app/shared/setting-drawer/setting-drawer.module.ts
+++ src/app/shared/setting-drawer/setting-drawer.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingDrawerComponent } from 'src/app/shared/setting-drawer/setting-drawer.component';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SettingDrawerComponent],
-  imports: [CommonModule, MatInputModule],
+  imports: [CommonModule, MatInputModule, FormsModule],
   exports: [SettingDrawerComponent],
 })
 export class SettingDrawerModule {}
