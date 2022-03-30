e40a3bf92c5847bf36f0dc4174e8d1e33aa741b3
Andersson Arellano <andersson.arellano.d@gmail.com>
2022-03-29 23:20:27 -0400

    format changes

--- src/app/shared/station-info-drawer/station-info-drawer.component.ts
+++ src/app/shared/station-info-drawer/station-info-drawer.component.ts
@@ -293,7 +293,7 @@ export class StationInfoDrawerComponent implements OnInit, OnDestroy {
   get displayCreateDocumentButton(): boolean {
     return (
       this.stationDocumentGenerationStatus ===
-      DocumentGenerationStatus.Manual &&
+        DocumentGenerationStatus.Manual &&
       (this.mapMode === MapMode.View ||
         this.mapMode === MapMode.Build ||
         this.mapMode === undefined) &&
@@ -330,10 +330,10 @@ export class StationInfoDrawerComponent implements OnInit, OnDestroy {
       status === 'None' && statusNew.checked
         ? DocumentGenerationStatus.None
         : status === 'None' && !statusNew.checked
-          ? DocumentGenerationStatus.Manual
-          : status === 'Manual' && statusNew.checked
-            ? DocumentGenerationStatus.Manual
-            : DocumentGenerationStatus.None;
+        ? DocumentGenerationStatus.Manual
+        : status === 'Manual' && statusNew.checked
+        ? DocumentGenerationStatus.Manual
+        : DocumentGenerationStatus.None;
 
     this.updateStationDocumentGenerationStatus(this.stationRithmId, value);
   }
