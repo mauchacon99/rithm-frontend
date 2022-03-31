11ef918a81b223a8093b453b10c00e586b77aa03
ManojK <manoj.kumar@rithm.software>
2022-03-31 18:21:59 +0530

    RTM-2655 UI - Add a delete button below the inputSetting Section to remove the current question from the widget
    UI - Add a delete button below the inputSetting Section to remove the current question from the widget

--- src/app/shared/setting-drawer/setting-drawer.module.ts
+++ src/app/shared/setting-drawer/setting-drawer.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingDrawerComponent } from 'src/app/shared/setting-drawer/setting-drawer.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SettingDrawerComponent],
-  imports: [CommonModule, MatSlideToggleModule],
+  imports: [CommonModule, MatSlideToggleModule, MatButtonModule],
   exports: [SettingDrawerComponent],
 })
 export class SettingDrawerModule {}
