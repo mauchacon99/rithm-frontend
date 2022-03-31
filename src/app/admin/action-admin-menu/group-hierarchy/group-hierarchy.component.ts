3d2c8194eb95c3f0f822c50b59e6d18a42d3c098
DarwinMMC <darwin.cerpa1@gmail.com>
2022-03-31 15:15:06 -0500

    RTM-2759

--- src/app/admin/action-admin-menu/group-hierarchy/group-hierarchy.component.ts
+++ src/app/admin/action-admin-menu/group-hierarchy/group-hierarchy.component.ts
@@ -1,6 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { StationGroupData, StationListGroup } from 'src/models';
 
+interface itemSelected {
+  /** Selected item type. */
+  type: string;
+
+  /** Selected item RithmId. */
+  rithmId: string;
+
+  /** Selected item name. */
+  name: string;
+
+  /** Selected item data. */
+  data: string;
+}
 /**
  * Component to show hierarchy on admin panel.
  */
@@ -13,8 +26,25 @@ export class GroupHierarchyComponent {
   /** Value to show or hidden this component based on permission. */
   @Input() showGroupHierarchy!: boolean;
 
-  /** Columns of render with stationGroupRithmId. */
-  columnsStationGroupRithmId: string[] = [''];
+  /** List element selected. */
+  itemListSelected: itemSelected[] = [
+    {
+      type: 'group',
+      rithmId: '',
+      name: 'Home',
+      data: '{}',
+    },
+  ];
+
+  /** List group selected. */
+  groupItemListSelected: itemSelected[] = [
+    {
+      type: 'group',
+      rithmId: '',
+      name: 'Home',
+      data: '{}',
+    },
+  ];
 
   /** Value of selected item. */
   selectedItem!: StationGroupData | StationListGroup;
@@ -39,9 +69,42 @@ export class GroupHierarchyComponent {
     index: number
   ): void {
     this.selectedItem = itemSelected;
-    this.columnsStationGroupRithmId.splice(index + 1);
+    this.itemListSelected.splice(index + 1);
+    this.groupItemListSelected.splice(index + 1);
+
+    this.itemListSelected.push({
+      type: this.isGroup ? 'group' : 'station',
+      rithmId: itemSelected.rithmId,
+      name: this.isGroup
+        ? (itemSelected as StationGroupData).title
+        : (itemSelected as StationListGroup).name,
+      data: JSON.stringify(itemSelected),
+    });
+
     if (this.isGroup) {
-      this.columnsStationGroupRithmId.push(itemSelected.rithmId);
+      this.groupItemListSelected.push({
+        type: 'group',
+        rithmId: itemSelected.rithmId,
+        name: (itemSelected as StationGroupData).title,
+        data: JSON.stringify(itemSelected),
+      });
     }
+  }
+
+  /**
+   * Navigation in options list columns.
+   *
+   * @param index Index selected.
+   */
+  moveList(index: number): void {
+    this.groupItemListSelected.splice(index + 1, this.itemListSelected.length);
+    this.itemListSelected.forEach((item) => {
+      if (
+        item.rithmId ===
+        this.groupItemListSelected[this.groupItemListSelected.length - 1]
+          .rithmId
+      )
+        this.selectedItem = JSON.parse(item.data);
+    });
   }
 }
