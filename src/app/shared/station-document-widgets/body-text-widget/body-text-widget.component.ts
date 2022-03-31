2a8a1138cdbce2d4d6b99607d8a38edb429c195e
Pedro R. Chacon <pedrodevchacon@gmail.com>
2022-03-31 00:20:44 -0400

    RTM-2665 UI - Dropping the BodyText item should generate an inputFrame with a textArea inside.

--- src/app/shared/station-document-widgets/body-text-widget/body-text-widget.component.ts
+++ src/app/shared/station-document-widgets/body-text-widget/body-text-widget.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Question } from 'src/models';
 /**
  * Reusable component for displaying an body-text-widget in station grid.
  */
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './body-text-widget.component.html',
   styleUrls: ['./body-text-widget.component.scss'],
 })
-export class BodyTextWidgetComponent {}
+export class BodyTextWidgetComponent {
+  /** The mode to display fields inside the widget. */
+  @Input() widgetMode!: 'layout' | 'setting';
+
+  /** Event Emitter will open a field setting drawer on the right side of the station. */
+  @Output() openSettingDrawer: EventEmitter<Question> =
+    new EventEmitter<Question>();
+
+  /**
+   * Open setting drawer.
+   *
+   * @param field The field for the setting drawer.
+   */
+  handleOpenSettingDrawer(field?: Question): void {
+    if (this.widgetMode === 'setting') {
+      this.openSettingDrawer.emit(field);
+    }
+  }
+}
