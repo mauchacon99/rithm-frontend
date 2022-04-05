976f02ae730d10e1c521321af9626954018fb559
Adarsh <adarsh.achar@inpivota.com>
2022-04-05 23:02:13 +0530

    RTM-2863 Move randRithmId method as a helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -13,3 +13,4 @@ export * from './map/service/map-connection-helper';
 export * from './map/service/map-station-group-helper';
 export * from './map/service/center-helper';
 export * from './map/service/zoom-helper';
+export * from './randomId-generator';
