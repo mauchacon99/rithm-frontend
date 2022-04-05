87cd4bd5660d3939fee64db402b6f8e5402dbeb9
JoharaP <jpabon@travelsupport.info>
2022-04-05 12:29:29 -0400

    RTM-2899 If the question belongs to a previous station group, then display a static text in the drawer.

--- src/app/shared/setting-drawer/setting-drawer.component.ts
+++ src/app/shared/setting-drawer/setting-drawer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { first, Subject, takeUntil } from 'rxjs';
+import { ErrorService } from 'src/app/core/error.service';
 import { SidenavDrawerService } from 'src/app/core/sidenav-drawer.service';
 import { Question } from 'src/models';
 
@@ -18,7 +20,14 @@ export class SettingDrawerComponent implements OnInit, OnDestroy {
   /** The field information for your setting. */
   fieldSetting!: Question;
 
-  constructor(private sideNavDrawerService: SidenavDrawerService) {}
+  /** The station id of the current station. */
+  stationRithmId!: string;
+
+  constructor(
+    private sideNavDrawerService: SidenavDrawerService,
+    private route: ActivatedRoute,
+    private errorService: ErrorService
+  ) {}
 
   /**
    * Listen the DrawerData Service.
@@ -29,6 +38,7 @@ export class SettingDrawerComponent implements OnInit, OnDestroy {
       .subscribe((data) => {
         const dataDrawer = data as Question;
         this.fieldSetting = dataDrawer;
+        this.getStationId();
       });
   }
 
@@ -37,6 +47,25 @@ export class SettingDrawerComponent implements OnInit, OnDestroy {
    */
   ngOnInit(): void {
     this.subscribeDrawerData$();
+  }
+
+  /**
+   * Get from the route parameters the id of the current station.
+   */
+  private getStationId(): void {
+    this.route.params.pipe(first()).subscribe({
+      next: (params) => {
+        if (params.stationId) {
+          this.stationRithmId = params.stationId;
+        }
+      },
+      error: (error: unknown) => {
+        this.errorService.displayError(
+          "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+          error
+        );
+      },
+    });
   }
 
   /**
