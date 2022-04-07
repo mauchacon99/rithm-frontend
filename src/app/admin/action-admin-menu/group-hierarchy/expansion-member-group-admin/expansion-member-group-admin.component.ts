e24b6261930ef6007d8924db06fb0a07e91f09bb
DarwinMMC <darwin.cerpa1@gmail.com>
2022-04-06 19:34:05 -0500

    RTM-2740-RTM-2743 remove members

--- src/app/admin/action-admin-menu/group-hierarchy/expansion-member-group-admin/expansion-member-group-admin.component.ts
+++ src/app/admin/action-admin-menu/group-hierarchy/expansion-member-group-admin/expansion-member-group-admin.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { first } from 'rxjs';
 import { ErrorService } from 'src/app/core/error.service';
+import { PopupService } from 'src/app/core/popup.service';
 import { StationService } from 'src/app/core/station.service';
 import { RosterManagementModalComponent } from 'src/app/shared/roster-management-modal/roster-management-modal.component';
 import {
@@ -69,7 +70,8 @@ export class ExpansionMemberGroupAdminComponent implements OnInit {
   constructor(
     private stationService: StationService,
     private errorService: ErrorService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private popupService: PopupService
   ) {}
 
   /**
@@ -172,5 +174,57 @@ export class ExpansionMemberGroupAdminComponent implements OnInit {
         );
       },
     });
+  }
+
+  /**
+   * Remove users from the station specific roster.
+   *
+   * @param usersId The selected user id to remove.
+   */
+  removeMemberFromRosterStation(usersId: string): void {
+    this.isLoading = true;
+    this.isErrorGetUsers = false;
+    const removeUserMemberRosterStation$ = this.isAdmin
+      ? this.stationService.removeUsersFromOwnerRoster(
+          this._stationOrGroupSelected.rithmId,
+          [usersId]
+        )
+      : this.stationService.removeUsersFromWorkerRoster(
+          this._stationOrGroupSelected.rithmId,
+          [usersId]
+        );
+
+    removeUserMemberRosterStation$.pipe(first()).subscribe({
+      next: (members) => {
+        this.isLoading = false;
+        this.isErrorGetUsers = false;
+        this.members = members;
+      },
+      error: (error: unknown) => {
+        this.isLoading = false;
+        this.isErrorGetUsers = true;
+        this.errorService.displayError(
+          "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+          error
+        );
+      },
+    });
+  }
+
+  /**
+   * Confirm remove member .
+   *
+   * @param usersId The selected user id to remove.
+   */
+  async confirmRemoveMember(usersId: string): Promise<void> {
+    const response = await this.popupService.confirm({
+      title: 'Remove Member?',
+      message: 'This cannot be undone!',
+      okButtonText: 'Yes',
+      cancelButtonText: 'No',
+      important: true,
+    });
+
+    if (response) this.removeMemberFromRosterStation(usersId);
   }
 }
