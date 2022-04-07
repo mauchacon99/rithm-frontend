0fd62b3e171fe5822bee4054c740d42f2bb30572
Adarsh <adarsh.achar@inpivota.com>
2022-04-07 15:11:40 +0530

    Modules update

--- src/app/shared/fields/upload-file-modal/upload-file-modal.module.ts
+++ src/app/shared/fields/upload-file-modal/upload-file-modal.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { UploadFileModalComponent } from './upload-file-modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,7 +7,6 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 @NgModule({
   declarations: [UploadFileModalComponent],
   imports: [
-    CommonModule,
     MatDialogModule,
     MatButtonModule,
     MatProgressBarModule,
