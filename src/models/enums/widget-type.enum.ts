9099834b978c878bf66a097b369712f74f5c1724
Adams Contreras <90850547+AdamsContreras999@users.noreply.github.com>
2022-04-07 16:36:18 -0400

    Update src/models/enums/widget-type.enum.ts
    Co-authored-by: Luna Arturo <56488686+arturoluona@users.noreply.github.com>

--- src/models/enums/widget-type.enum.ts
+++ src/models/enums/widget-type.enum.ts
@@ -25,6 +25,6 @@ export enum WidgetType {
   StationGroupSearch = 'station-group-search-widget',
   /** Widget type station-group. */
   StationGroup = 'station-group',
-  /** Widget type station-group-traffic. */
+  /** Widget type group-traffic. */
   StationGroupTraffic = 'group-traffic',
 }
