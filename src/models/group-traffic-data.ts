8a294122b94725d5aa78f7e76f622e3000473627
DarwinMMC <darwin.cerpa1@gmail.com>
2022-04-08 16:04:29 -0500

    Add interface

---
+++ src/models/group-traffic-data.ts
@@ -0,0 +1,14 @@
+/** Interface to graph document traffic by station. */
+export interface GroupTrafficData {
+  /** RithmId group station. */
+  stationGroupRithmId: string;
+  /** Station names. */
+  labels: string[];
+  /** Dataset chart. */
+  datasets: {
+    /** Represents the number of documents per station. */
+    stationDocuments: number[];
+    /** Represents the average number of document flows per station. */
+    averageDocumentStation: number[];
+  };
+}
