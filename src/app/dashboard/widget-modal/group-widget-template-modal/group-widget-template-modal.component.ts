8db5f8471a76fdc377507798ee6588223b96d5ab
arturoluona <arturoluna879@gmail.com>
2022-04-11 21:49:56 -0400

    RTM-2856-ui-show-the-reporting-section-for-the-groups-tab-in-the-wdgets-modal

--- src/app/dashboard/widget-modal/group-widget-template-modal/group-widget-template-modal.component.ts
+++ src/app/dashboard/widget-modal/group-widget-template-modal/group-widget-template-modal.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy } from '@angular/core';
 import { DashboardService } from 'src/app/dashboard/dashboard.service';
 import { WidgetType } from 'src/models';
+import { Chart, ChartConfiguration, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 /**
  * The component for templates widgets of the groups.
@@ -10,7 +12,9 @@ import { WidgetType } from 'src/models';
   templateUrl: './group-widget-template-modal.component.html',
   styleUrls: ['./group-widget-template-modal.component.scss'],
 })
-export class GroupWidgetTemplateModalComponent {
+export class GroupWidgetTemplateModalComponent
+  implements OnDestroy, AfterViewInit
+{
   /** Type of widget to show. */
   @Input() widgetType: WidgetType = WidgetType.StationGroupSearch;
 
@@ -20,7 +24,84 @@ export class GroupWidgetTemplateModalComponent {
   /** Data static for each template by widgetType. */
   dataTemplate;
 
+  /** Chart to group traffic. */
+  chartGroupTraffic!: Chart;
+
+  /** Data static to show chart only group-traffic. */
+  dataChart: ChartConfiguration = {
+    type: 'line',
+    data: {
+      labels: [
+        'Station 1',
+        'Station 2',
+        'Station 3',
+        'Station 4',
+        'Station 5',
+        'Station 6',
+        'Station 7',
+      ],
+      datasets: [
+        {
+          // Documents
+          label: 'Documents',
+          data: [20, 50, 32, 45, 40, 51, 26],
+          borderColor: '#3b82f6',
+          backgroundColor: 'rgba(59,130,246,0.63)',
+          stack: 'combined',
+          type: 'bar',
+        },
+        {
+          // Documents Flow
+          label: 'Documents Flow',
+          data: [5, 20, 7, 5, 6, 8, 9],
+          borderColor: '#3b82f6',
+          backgroundColor: 'rgba(59,130,246,0.63)',
+          stack: 'combined',
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          stacked: true,
+          ticks: {
+            font: {
+              size: 8,
+            },
+          },
+        },
+        x: {
+          ticks: {
+            font: {
+              size: 6,
+            },
+          },
+        },
+      },
+    },
+  };
+
   constructor(private dashboardService: DashboardService) {
     this.dataTemplate = dashboardService.dataTemplatePreviewWidgetModal;
+  }
+
+  /** After view init. */
+  ngAfterViewInit(): void {
+    if (this.widgetType === this.enumWidgetType.StationGroupTraffic) {
+      this.chartGroupTraffic = new Chart('chartTrafficGroup', this.dataChart);
+    }
+  }
+
+  /** Destroy component. */
+  ngOnDestroy(): void {
+    if (this.widgetType === this.enumWidgetType.StationGroupTraffic) {
+      this.chartGroupTraffic.destroy();
+    }
   }
 }
