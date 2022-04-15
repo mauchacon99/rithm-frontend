1b5098d294f0491729f246dc7d09b003b73b529a
maalexdev <mfigueroa9406@gmail.com>
2022-04-15 15:09:43 -0600

    Format

--- src/app/dashboard/widgets/station-pre-built-widget/station-pre-built-widget.component.spec.ts
+++ src/app/dashboard/widgets/station-pre-built-widget/station-pre-built-widget.component.spec.ts
@@ -1,24 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ErrorService } from 'src/app/core/error.service';
+import { StationService } from 'src/app/core/station.service';
+import {
+  MockErrorService,
+  MockStationService,
+  MockUserService,
+} from 'src/mocks';
 
 import { StationPreBuiltWidgetComponent } from './station-pre-built-widget.component';
+import { UserService } from 'src/app/core/user.service';
+import { throwError } from 'rxjs';
 
 describe('StationPreBuiltWidgetComponent', () => {
   let component: StationPreBuiltWidgetComponent;
   let fixture: ComponentFixture<StationPreBuiltWidgetComponent>;
+  let stationService: StationService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [StationPreBuiltWidgetComponent],
+      providers: [
+        { provide: ErrorService, useClass: MockErrorService },
+        { provide: StationService, useClass: MockStationService },
+        { provide: UserService, useClass: MockUserService },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StationPreBuiltWidgetComponent);
     component = fixture.componentInstance;
+    stationService = TestBed.inject(StationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getUserStationData', () => {
+    const spyMethod = spyOn(component, 'getUserStationData').and.callThrough();
+    const spyGetUserStationData = spyOn(
+      stationService,
+      'getUserStationData'
+    ).and.callThrough();
+    component.ngOnInit();
+    expect(spyMethod).toHaveBeenCalled();
+    expect(spyGetUserStationData).toHaveBeenCalled();
+  });
+
+  it('should catch an error if the request getUserStationData fails', () => {
+    const spyError = spyOn(
+      stationService,
+      'getUserStationData'
+    ).and.returnValue(
+      throwError(() => {
+        throw new Error();
+      })
+    );
+    component.ngOnInit();
+    expect(spyError).toHaveBeenCalled();
   });
 });
--- src/app/dashboard/widgets/station-pre-built-widget/station-pre-built-widget.component.ts
+++ src/app/dashboard/widgets/station-pre-built-widget/station-pre-built-widget.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { ErrorService } from 'src/app/core/error.service';
+import { StationService } from 'src/app/core/station.service';
+import { UserService } from 'src/app/core/user.service';
 import { StationRosterMember } from 'src/models';
 /**
  * Component for station prebuilt.
@@ -9,34 +12,44 @@ import { StationRosterMember } from 'src/models';
   templateUrl: './station-pre-built-widget.component.html',
   styleUrls: ['./station-pre-built-widget.component.scss'],
 })
-export class StationPreBuiltWidgetComponent {
+export class StationPreBuiltWidgetComponent implements OnInit {
   /** Edit mode toggle from dashboard. */
   @Input() editMode = false;
+
+  /*User station data. */
+  userStations: StationRosterMember[] = [];
+
+  constructor(
+    private stationService: StationService,
+    private errorService: ErrorService,
+    private userService: UserService
+  ) {}
+
+  /**
+   * Initial Method.
+   */
+  ngOnInit(): void {
+    this.getUserStationData();
+  }
 
   /**
    * Get user stations.
    *
-   * @returns User Stations.
    */
-  getUserStationData(): Observable<StationRosterMember[]> {
-    const listStations: StationRosterMember[] = [
-      {
-        rithmId: '',
-        firstName: 'Marry',
-        lastName: 'Poppins',
-        email: 'marrypoppins@inpivota.com',
-        isOwner: false,
-        isWorker: true,
-      },
-      {
-        rithmId: '',
-        firstName: 'Worker',
-        lastName: 'User',
-        email: 'workeruser@inpivota.com',
-        isOwner: false,
-        isWorker: true,
-      },
-    ];
-    return of(listStations).pipe(delay(1000));
+  getUserStationData(): void {
+    this.stationService
+      .getUserStationData(this.userService.user.rithmId)
+      .pipe(first())
+      .subscribe({
+        next: (userStations) => {
+          this.userStations = userStations;
+        },
+        error: (error: unknown) => {
+          this.errorService.displayError(
+            "Something went wrong on our end and we're looking into it. Please try again in a little while.",
+            error
+          );
+        },
+      });
   }
 }
