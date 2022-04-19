8a3871f5300029578766e6176280dee332bb156c
DarwinMMC <darwin.cerpa1@gmail.com>
2022-04-19 10:29:24 -0500

    RTM-2984-RTM-2985 show loading and message error

--- src/app/dashboard/widgets/container-pre-built-widget/container-pre-built-widget.component.spec.ts
+++ src/app/dashboard/widgets/container-pre-built-widget/container-pre-built-widget.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+import { throwError } from 'rxjs';
+import { DocumentService } from 'src/app/core/document.service';
+import { ErrorService } from 'src/app/core/error.service';
+import { MockErrorService, MockDocumentService } from 'src/mocks';
+import { LoadingWidgetComponent } from 'src/app/dashboard/widgets/loading-widget/loading-widget.component';
 
 import { ContainerPreBuiltWidgetComponent } from './container-pre-built-widget.component';
+import { ErrorWidgetComponent } from 'src/app/dashboard/widgets/error-widget/error-widget.component';
 
 describe('ContainerPreBuiltWidgetComponent', () => {
   let component: ContainerPreBuiltWidgetComponent;
@@ -8,7 +15,15 @@ describe('ContainerPreBuiltWidgetComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ContainerPreBuiltWidgetComponent],
+      declarations: [
+        ContainerPreBuiltWidgetComponent,
+        MockComponent(LoadingWidgetComponent),
+        MockComponent(ErrorWidgetComponent),
+      ],
+      providers: [
+        { provide: ErrorService, useClass: MockErrorService },
+        { provide: DocumentService, useClass: MockDocumentService },
+      ],
     }).compileComponents();
   });
 
@@ -20,5 +35,65 @@ describe('ContainerPreBuiltWidgetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getContainerWidgetPreBuilt', () => {
+    const spyGetContainerWidgetPreBuilt = spyOn(
+      TestBed.inject(DocumentService),
+      'getContainerWidgetPreBuilt'
+    ).and.callThrough();
+    component.ngOnInit();
+    expect(spyGetContainerWidgetPreBuilt).toHaveBeenCalled();
+  });
+
+  it('should catch an error if the request getContainerWidgetPreBuilt fails', () => {
+    const spyError = spyOn(
+      TestBed.inject(DocumentService),
+      'getContainerWidgetPreBuilt'
+    ).and.returnValue(
+      throwError(() => {
+        throw new Error();
+      })
+    );
+    component.ngOnInit();
+    expect(spyError).toHaveBeenCalled();
+  });
+
+  it('should show loading while get containers', () => {
+    const spyMethod = spyOn(
+      component,
+      'getContainerWidgetPreBuilt'
+    ).and.callThrough();
+
+    component.ngOnInit();
+    expect(spyMethod).toHaveBeenCalled();
+
+    const loading = fixture.nativeElement.querySelector(
+      '#app-loading-indicator'
+    );
+    expect(loading).toBeTruthy();
+  });
+
+  it('should show message error and try again', () => {
+    const spyMethod = spyOn(
+      component,
+      'getContainerWidgetPreBuilt'
+    ).and.callThrough();
+
+    const spyError = spyOn(
+      TestBed.inject(DocumentService),
+      'getContainerWidgetPreBuilt'
+    ).and.returnValue(
+      throwError(() => {
+        throw new Error();
+      })
+    );
+    component.ngOnInit();
+    fixture.detectChanges();
+    const errorComponent =
+      fixture.nativeElement.querySelector('#error-load-widget');
+    expect(errorComponent).toBeTruthy();
+    expect(spyError).toHaveBeenCalled();
+    expect(spyMethod).toHaveBeenCalled();
   });
 });
