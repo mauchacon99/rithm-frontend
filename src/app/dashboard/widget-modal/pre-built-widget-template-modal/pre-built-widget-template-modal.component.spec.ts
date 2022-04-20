365bbd026b53d08c179d0c1bba293b611d289fc9
DarwinMMC <darwin.cerpa1@gmail.com>
2022-04-20 09:21:35 -0500

    RTM-2967 render template stations

--- src/app/dashboard/widget-modal/pre-built-widget-template-modal/pre-built-widget-template-modal.component.spec.ts
+++ src/app/dashboard/widget-modal/pre-built-widget-template-modal/pre-built-widget-template-modal.component.spec.ts
@@ -1,24 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DashboardService } from 'src/app/dashboard/dashboard.service';
+import { MockDashboardService } from 'src/mocks';
+import { WidgetType } from 'src/models';
 
 import { PreBuiltWidgetTemplateModalComponent } from './pre-built-widget-template-modal.component';
 
 describe('PreBuiltWidgetTemplateModalComponent', () => {
   let component: PreBuiltWidgetTemplateModalComponent;
   let fixture: ComponentFixture<PreBuiltWidgetTemplateModalComponent>;
+  const widgetType = WidgetType.Station;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PreBuiltWidgetTemplateModalComponent],
+      providers: [
+        { provide: DashboardService, useClass: MockDashboardService },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PreBuiltWidgetTemplateModalComponent);
     component = fixture.componentInstance;
+    component.widgetType = widgetType;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set data template', () => {
+    const expectedDataTemplate =
+      TestBed.inject(DashboardService).dataTemplatePreviewWidgetModal;
+    expect(component.dataTemplate).toEqual(expectedDataTemplate);
+    expect(component.dataTemplate[component.widgetType]).toEqual(
+      expectedDataTemplate[widgetType]
+    );
   });
 });
--- src/app/dashboard/widget-modal/pre-built-widget-template-modal/pre-built-widget-template-modal.component.ts
+++ src/app/dashboard/widget-modal/pre-built-widget-template-modal/pre-built-widget-template-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WidgetType } from 'src/models';
+import { DashboardService } from 'src/app/dashboard/dashboard.service';
 /**
  * The component for pre built widget modal.
  */
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './pre-built-widget-template-modal.component.html',
   styleUrls: ['./pre-built-widget-template-modal.component.scss'],
 })
-export class PreBuiltWidgetTemplateModalComponent {}
+export class PreBuiltWidgetTemplateModalComponent {
+  /** Type of widget to show. */
+  @Input() widgetType: WidgetType = WidgetType.PreBuiltContainer;
+
+  /** Widget type for validation. */
+  enumWidgetType = WidgetType;
+
+  /** Data static for each template by widgetType. */
+  dataTemplate;
+
+  constructor(private dashboardService: DashboardService) {
+    this.dataTemplate = dashboardService.dataTemplatePreviewWidgetModal;
+  }
+}
