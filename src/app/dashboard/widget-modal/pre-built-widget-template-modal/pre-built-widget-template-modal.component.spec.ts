52e562d95b26012f364d8b03c9c2438a34001da5
maalexdev <mfigueroa9406@gmail.com>
2022-04-20 07:42:43 -0600

    Apply suggestions

--- src/app/dashboard/widget-modal/pre-built-widget-template-modal/pre-built-widget-template-modal.component.spec.ts
+++ src/app/dashboard/widget-modal/pre-built-widget-template-modal/pre-built-widget-template-modal.component.spec.ts
@@ -2,12 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PreBuiltWidgetTemplateModalComponent } from './pre-built-widget-template-modal.component';
 
-import { DashboardService } from '../../dashboard.service';
-import { MockDashboardService } from '../../../../../src/mocks';
+import { DashboardService } from 'src/app/dashboard/dashboard.service';
+import { MockDashboardService } from 'src/mocks';
+import { WidgetType } from 'src/models';
 
 describe('PreBuiltWidgetTemplateModalComponent', () => {
   let component: PreBuiltWidgetTemplateModalComponent;
   let fixture: ComponentFixture<PreBuiltWidgetTemplateModalComponent>;
+  const widgetType = WidgetType.Station;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,10 +23,20 @@ describe('PreBuiltWidgetTemplateModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PreBuiltWidgetTemplateModalComponent);
     component = fixture.componentInstance;
+    component.widgetType = widgetType;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set data template', () => {
+    const expectedDataTemplate =
+      TestBed.inject(DashboardService).dataTemplatePreviewWidgetModal;
+    expect(component.dataTemplate).toEqual(expectedDataTemplate);
+    expect(component.dataTemplate[component.widgetType]).toEqual(
+      expectedDataTemplate[widgetType]
+    );
   });
 });
