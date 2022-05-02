ca7106866d78cafa4626305e7d8f839846467332
JoharaP <jpabon@travelsupport.info>
2022-05-02 18:52:35 -0400

    add unit testing

--- src/app/document/folder/location-modal/location-modal.component.spec.ts
+++ src/app/document/folder/location-modal/location-modal.component.spec.ts
@@ -1,9 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { throwError } from 'rxjs';
 import { DocumentService } from 'src/app/core/document.service';
 import { ErrorService } from 'src/app/core/error.service';
+import { SidenavDrawerService } from 'src/app/core/sidenav-drawer.service';
 import { MockDocumentService, MockErrorService } from 'src/mocks';
+import { DocumentCurrentStation } from 'src/models';
 
 import { LocationModalComponent } from './location-modal.component';
 
@@ -15,12 +23,15 @@ describe('LocationModalComponent', () => {
   let component: LocationModalComponent;
   let fixture: ComponentFixture<LocationModalComponent>;
   const stationId = 'ED6148C9-ABB7-408E-A210-9242B2735B1C';
+  const documentId = 'E204F369-386F-4E41';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LocationModalComponent],
+      imports: [RouterTestingModule, MatDialogModule],
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: DATA_TEST },
+        { provide: MatDialogRef, useValue: { close } },
         { provide: ErrorService, useClass: MockErrorService },
         { provide: DocumentService, useClass: MockDocumentService },
       ],
@@ -69,5 +80,45 @@ describe('LocationModalComponent', () => {
       component.documentRithmId
     );
     expect(spyError).toHaveBeenCalled();
+  });
+
+  it('should call the method that redirect to the same container in other station', () => {
+    component.eventsLoading = false;
+    component.eventDocumentsError = false;
+
+    const eventStation: DocumentCurrentStation[] = [
+      {
+        name: 'Testy',
+        rithmId: '123',
+        flowedTimeUTC: '2022-04-18T20:34:24.118Z',
+      },
+    ];
+    component.currentStations = eventStation;
+    fixture.detectChanges();
+
+    const index = 0;
+    const btnContainer = fixture.debugElement.nativeElement.querySelector(
+      `#view-station-${index}`
+    );
+    expect(btnContainer).toBeTruthy();
+
+    const navigateSpy = spyOn(component, 'goToContainer').and.callThrough();
+    const routerSpy = spyOn(TestBed.inject(Router), 'navigate');
+    spyOn(
+      TestBed.inject(SidenavDrawerService),
+      'closeDrawer'
+    );
+    spyOn(TestBed.inject(MatDialogRef), 'close');
+
+    btnContainer.click(component.currentStations[0].rithmId);
+    component.goToContainer(component.currentStations[0].rithmId);
+
+    expect(navigateSpy).toHaveBeenCalledWith(component.currentStations[0].rithmId);
+    expect(routerSpy).toHaveBeenCalledWith([`/document/${component.documentRithmId}`], {
+      queryParams: {
+        documentId: component.documentRithmId,
+        stationId: component.currentStations[0].rithmId,
+      },
+    });
   });
 });
