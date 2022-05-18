5d26122f0d3d0685281a015c8395d7e5a19ad1ec
maalexdev <mfigueroa9406@gmail.com>
2022-05-18 09:30:19 -0600

    Apply suggesyions

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, Observable, of, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import {
   WorkerDashboardHeader,
