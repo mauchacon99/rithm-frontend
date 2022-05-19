01d0a170f82a8ed29a0eb640d08ddda39f3bb0c0
SHIRLEY  MUELLE CAMARGO <62040229+SJmuelle@users.noreply.github.com>
2022-05-19 04:34:37 -0500

    RTM-3314-create-a-modal-in-shared-folder-to-download-documents

---
+++ src/app/shared/file-download-modal/file-download-modal.component.spec.ts
@@ -0,0 +1,39 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { FileDownloadModalComponent } from './file-download-modal.component';
+
+describe('FileDownloadModalComponent', () => {
+  let component: FileDownloadModalComponent;
+  let fixture: ComponentFixture<FileDownloadModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FileDownloadModalComponent ],
+      providers: [
+        { provide: MatDialogRef, useValue: { close } }
+      ],
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FileDownloadModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should close the modal when close-modal-btn clicked', () => {
+    const spyMatDialogRef = spyOn(TestBed.inject(MatDialogRef), 'close');
+    const spyMethod = spyOn(component, 'closeModal').and.callThrough();
+    const btnClose = fixture.nativeElement.querySelector('#close-modal-btn');
+    expect(btnClose).toBeTruthy();
+    btnClose.click();
+    expect(spyMethod).toHaveBeenCalled();
+    expect(spyMatDialogRef).toHaveBeenCalled();
+  });
+});
---
+++ src/app/shared/file-download-modal/file-download-modal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileDownloadModalComponent } from './file-download-modal.component';
+
+
+
+@NgModule({
+  declarations: [FileDownloadModalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FileDownloadModalModule { }
